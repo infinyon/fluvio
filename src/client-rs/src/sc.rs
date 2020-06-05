668ddb4f376e956e81fdec8d8233c0a7e94475e8
Sehyo Chang <sehyo@fluvio.io>
2020-06-05 15:11:42 -0700

    issue #21: show proper error message when topic creation fails

--- src/client-rs/src/sc.rs
+++ src/client-rs/src/sc.rs
@@ -143,6 +143,36 @@ impl ScClient {
     }
 }
 
+
+
+macro_rules! topic_error {
+    ($topic:expr,$topic_response:expr,$msg:expr) => {
+        {
+            use crate::ClientError;
+            use std::io::Error as IoError;
+
+            if let Some(msg) = &($topic_response.error_message) {
+                ClientError::IoError(IoError::new(
+                    ErrorKind::Other,
+                    msg.to_owned()
+                ))
+        
+            } else {
+                ClientError::IoError(IoError::new(
+                    ErrorKind::Other,
+                    format!(
+                        $msg,
+                        $topic,
+                        $topic_response.error_code.to_sentence()
+                    ),
+                ))
+            }
+        }
+        
+    };
+}
+
+
 #[async_trait]
 impl SpuController for ScClient {
     type Leader = SpuReplicaLeader;
@@ -181,6 +211,7 @@ impl SpuController for ScClient {
             if topic_resp.error_code == FlvErrorCode::TopicNotFound {
                 return Err(ClientError::TopicNotFound(topic.to_owned()));
             } else {
+                
                 return Err(ClientError::IoError(IoError::new(
                     ErrorKind::InvalidData,
                     format!(
@@ -210,6 +241,7 @@ impl SpuController for ScClient {
                     if spu_resp.spu_id == leader_id {
                         // check for errors
                         if spu_resp.error_code != FlvErrorCode::None {
+
                             return Err(ClientError::IoError(IoError::new(
                                 ErrorKind::InvalidData,
                                 format!(
@@ -250,14 +282,7 @@ impl SpuController for ScClient {
                 if topic_response.error_code.is_ok() {
                     return Ok(topic_response.name);
                 } else {
-                    return Err(ClientError::IoError(IoError::new(
-                        ErrorKind::Other,
-                        format!(
-                            "topic error '{}' {}",
-                            topic,
-                            topic_response.error_code.to_sentence()
-                        ),
-                    )));
+                    return  Err(topic_error!(topic, topic_response,"topic deletion error '{}' {}"))
                 }
             }
         }
@@ -299,14 +324,7 @@ impl SpuController for ScClient {
                 if topic_response.error_code.is_ok() {
                     return Ok(topic);
                 } else {
-                    return Err(ClientError::IoError(IoError::new(
-                        ErrorKind::Other,
-                        format!(
-                            "topic error '{}' {}",
-                            topic,
-                            topic_response.error_code.to_sentence()
-                        ),
-                    )));
+                   return  Err(topic_error!(topic, topic_response,"topic creation error '{}' {}"))
                 }
             }
         }
@@ -332,3 +350,5 @@ impl SpuController for ScClient {
         Ok(topics)
     }
 }
+
+
