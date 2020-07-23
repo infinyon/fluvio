ebbdfe56612701786c175273b7d422ac00a39eac
Sehyo Chang <sehyo@fluvio.io>
2020-07-23 15:39:11 -0700

    cargo fmt

--- src/cli/src/cluster/install/local.rs
+++ src/cli/src/cluster/install/local.rs
@@ -75,7 +75,8 @@ fn launch_sc(option: &InstallCommand, log_dir: &str) {
     }
     binary.print();
 
-    binary.stdout(Stdio::from(outputs))
+    binary
+        .stdout(Stdio::from(outputs))
         .stderr(Stdio::from(errors))
         .spawn()
         .expect("sc server failed to start");
