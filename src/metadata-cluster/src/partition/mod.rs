b1e8ed1e6f8d97468b6cbc1103f2964fd826ef90
Sehyo Chang <sehyo@fluvio.io>
2020-08-03 13:06:05 -0700

    update k8 dispatcher to skip incoming k8 objects (#132)
    * update k8 dispatcher to skip incoming k8 objects
    make K8EnhancedSpec to make it more flexible for conversion
    
    * apply cargo fmt

--- src/metadata-cluster/src/partition/mod.rs
+++ src/metadata-cluster/src/partition/mod.rs
@@ -34,12 +34,25 @@ mod metadata {
     #[cfg(feature = "k8")]
     mod extended {
 
-        use super::metadata::PartitionSpec;
         use crate::store::k8::K8ExtendedSpec;
+        use crate::store::k8::K8ConvertError;
+        use crate::store::k8::K8MetaItem;
+        use crate::store::MetadataStoreObject;
+        use crate::k8::metadata::K8Obj;
+        use crate::store::k8::default_convert_from_k8;
+
+        use super::metadata::PartitionSpec;
 
         impl K8ExtendedSpec for PartitionSpec {
             type K8Spec = Self;
             type K8Status = Self::Status;
+
+            fn convert_from_k8(
+                k8_obj: K8Obj<Self::K8Spec>,
+            ) -> Result<MetadataStoreObject<Self, K8MetaItem>, K8ConvertError<Self::K8Spec>>
+            {
+                default_convert_from_k8(k8_obj)
+            }
         }
     }
 }
--- src/metadata-cluster/src/topic/mod.rs
+++ src/metadata-cluster/src/topic/mod.rs
@@ -35,12 +35,25 @@ mod metadata {
     #[cfg(feature = "k8")]
     mod extended {
 
-        use super::TopicSpec;
         use crate::store::k8::K8ExtendedSpec;
+        use crate::store::k8::K8ConvertError;
+        use crate::store::k8::K8MetaItem;
+        use crate::store::MetadataStoreObject;
+        use crate::k8::metadata::K8Obj;
+        use crate::store::k8::default_convert_from_k8;
+
+        use super::TopicSpec;
 
         impl K8ExtendedSpec for TopicSpec {
             type K8Spec = Self;
             type K8Status = Self::Status;
+
+            fn convert_from_k8(
+                k8_obj: K8Obj<Self::K8Spec>,
+            ) -> Result<MetadataStoreObject<Self, K8MetaItem>, K8ConvertError<Self::K8Spec>>
+            {
+                default_convert_from_k8(k8_obj)
+            }
         }
     }
 }
