2119eb8571586614ae2ee004ca8365e60f9a61c1
Nick Mosher <nicholastmosher@gmail.com>
2020-08-12 15:37:43 -0400

    Bump dependency of flv-util to 0.3.0

--- src/sc-k8/src/main.rs
+++ src/sc-k8/src/main.rs
@@ -1,8 +1,7 @@
 use structopt::StructOpt;
 
 fn main() {
-    // flv_util::init_logger();
-    flv_util::init_tracer();
+    flv_util::init_tracer(None);
 
     let opt = flv_sc_k8::ScOpt::from_args();
     flv_sc_k8::main_loop(opt);
