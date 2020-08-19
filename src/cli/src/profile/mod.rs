7363bc12056c534c4ee35fb10923748c21022b7c
Nick Mosher <nicholastmosher@gmail.com>
2020-08-19 18:21:24 -0400

    Download and save Fluvio Cloud profile (#167)
    * Download and save profile from Fluvio Cloud
    
    * Apply cargo fmt

--- src/cli/src/profile/mod.rs
+++ src/cli/src/profile/mod.rs
@@ -11,6 +11,7 @@ mod context;
 use sync::SyncCommand;
 use structopt::StructOpt;
 pub use k8::set_k8_context;
+pub use k8::discover_fluvio_addr;
 pub use context::set_local_context;
 pub use sync::CloudError;
 pub use sync::LocalOpt;
@@ -72,47 +73,65 @@ where
     match profile_command {
         ProfileCommand::View => view_profile(out.clone()),
         ProfileCommand::DisplayCurrent => display_current_profile(out.clone()),
-        ProfileCommand::Switch(profile) => match ConfigFile::load(None) {
-            Ok(mut config_file) => {
-                if !config_file
-                    .mut_config()
-                    .set_current_profile(&profile.profile_name)
-                {
-                    t_println!(out, "profile {} not founded", profile.profile_name);
-                } else {
-                    if let Err(err) = config_file.save() {
-                        t_println!(out, "unable to save profile: {}", err);
-                    }
-                }
-            }
-            Err(_) => t_print_cli_err!(out, "no profile can be founded"),
-        },
-        ProfileCommand::Delete(profile) => match ConfigFile::load(None) {
-            Ok(mut config_file) => {
-                if !config_file
-                    .mut_config()
-                    .delete_profile(&profile.profile_name)
-                {
-                    t_println!(out, "profile {} not founded", profile.profile_name);
-                } else {
-                    if let Err(err) = config_file.save() {
-                        t_println!(out, "unable to save profile: {}", err);
-                    } else {
-                        t_println!(out, "profile {} deleted", profile.profile_name);
-                        if config_file.config().current_profile_name().is_none() {
-                            t_println!(out,"warning: this removed your current profile, use 'config switch-profile to select a different one");
-                        } else {
-                            t_println!(out, "profile deleted");
-                        }
-                    }
-                }
-            }
-            Err(_) => t_print_cli_err!(out, "no profile can be founded"),
-        },
+        ProfileCommand::Switch(profile) => {
+            process_switch(out, profile)?;
+        }
+        ProfileCommand::Delete(profile) => {
+            process_delete(out, profile)?;
+        }
         ProfileCommand::Sync(profile) => {
             process_sync(out, profile).await?;
         }
     }
 
     Ok("".to_owned())
+}
+
+pub fn process_switch<O>(out: std::sync::Arc<O>, opt: SwitchOpt) -> Result<String, CliError>
+where
+    O: Terminal,
+{
+    let profile_name = opt.profile_name;
+    match ConfigFile::load(None) {
+        Ok(mut config_file) => {
+            if !config_file.mut_config().set_current_profile(&profile_name) {
+                t_println!(out, "profile {} not found", &profile_name);
+            } else {
+                if let Err(err) = config_file.save() {
+                    t_println!(out, "unable to save profile: {}", err);
+                }
+            }
+        }
+        Err(_) => t_print_cli_err!(out, "no profile can be found"),
+    }
+
+    Ok("".to_string())
+}
+
+pub fn process_delete<O>(out: std::sync::Arc<O>, opt: DeleteOpt) -> Result<String, CliError>
+where
+    O: Terminal,
+{
+    let profile_name = opt.profile_name;
+    match ConfigFile::load(None) {
+        Ok(mut config_file) => {
+            if !config_file.mut_config().delete_profile(&profile_name) {
+                t_println!(out, "profile {} not found", &profile_name);
+            } else {
+                if let Err(err) = config_file.save() {
+                    t_println!(out, "unable to save profile: {}", err);
+                } else {
+                    t_println!(out, "profile {} deleted", &profile_name);
+                    if config_file.config().current_profile_name().is_none() {
+                        t_println!(out,"warning: this removed your current profile, use 'config switch-profile to select a different one");
+                    } else {
+                        t_println!(out, "profile deleted");
+                    }
+                }
+            }
+        }
+        Err(_) => t_print_cli_err!(out, "no profile can be found"),
+    }
+
+    Ok("".to_string())
 }
