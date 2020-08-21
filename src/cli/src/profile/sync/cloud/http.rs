3335710b8f0ccb0c9705143d9eb57fc2a35b0e85
Nick Mosher <nicholastmosher@gmail.com>
2020-08-21 12:47:29 -0400

    Add better error flow to cloud login (#178)
    * Add better error flow to cloud login
    
    * Apply cargo fmt
    
    * Remove dependency on surf
    
    * Remove async-tls dependency

---
+++ src/cli/src/profile/sync/cloud/http.rs
@@ -0,0 +1,20 @@
+use async_h1::client;
+use http_types::{Request, Response, Error, StatusCode};
+use tracing::debug;
+
+pub async fn execute(req: Request) -> Result<Response, Error> {
+    debug!("executing request: {:#?}", req);
+
+    let addr = req
+        .url()
+        .socket_addrs(|| Some(443))?
+        .into_iter()
+        .next()
+        .ok_or_else(|| Error::from_str(StatusCode::BadRequest, "missing valid address"))?;
+
+    let raw_stream = flv_future_aio::net::TcpStream::connect(addr).await?;
+    let result = client::connect(raw_stream, req).await;
+
+    debug!("http result: {:#?}", result);
+    result
+}
