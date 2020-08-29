7e2dcee6faa889d7109f9ef00c75365c3ca44af4
Sehyo Chang <sehyo@fluvio.io>
2020-08-29 02:08:28 +0000

    disable clippy warning for generate_message

--- tests/runner/src/tests/smoke/message.rs
+++ tests/runner/src/tests/smoke/message.rs
@@ -3,6 +3,7 @@ use crate::TestOption;
 const VALUE: u8 = 65;
 
 /// generate test data based on iteration and option
+#[allow(clippy::all)]
 pub fn generate_message(_index: u16, option: &TestOption) -> Vec<u8> {
     let mut bytes = Vec::with_capacity(option.produce.record_size);
 
