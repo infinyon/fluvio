8bc4d2fae6c3d9b6cbde96094ff4c3eecace8010
Sehyo Chang <sehyo@fluvio.io>
2020-09-13 02:42:47 +0000

    add enum test

---
+++ src/kf-socket/tests/enum.rs
@@ -0,0 +1,27 @@
+use std::convert::TryInto;
+
+use kf_protocol::derive::Decode;
+use kf_protocol::derive::Encode;
+
+
+#[derive(Encode, Decode, PartialEq, Debug, Clone, Copy)]
+#[repr(u16)]
+pub enum TestKafkaApiEnum {
+    Echo = 1000,
+    Status = 1001,
+}
+
+impl Default for TestKafkaApiEnum {
+    fn default() -> TestKafkaApiEnum {
+        TestKafkaApiEnum::Echo
+    }
+}
+
+
+#[test]
+fn test_conversion() {
+    let key: u16 =  1000;
+    let key_enum: TestKafkaApiEnum = key.try_into().expect("conversion");
+    assert_eq!(key_enum,TestKafkaApiEnum::Echo);
+
+}
