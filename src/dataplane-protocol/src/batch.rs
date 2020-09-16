a8c838ab83290d4ed91428650619f684586d4871
Sehyo Chang <sehyo@fluvio.io>
2020-09-16 20:07:34 +0000

    more clippy and fmt update

--- src/dataplane-protocol/src/batch.rs
+++ src/dataplane-protocol/src/batch.rs
@@ -235,9 +235,8 @@ mod test {
         let decoded_record = batch.records.get(0).unwrap();
         println!("record crc: {}", batch.header.crc);
         assert_eq!(batch.header.crc, 1514417201);
-        let b =  decoded_record.value.inner_value_ref().as_deref().unwrap();
-        assert_eq!(b,"test".as_bytes());
-        
+        let b = decoded_record.value.inner_value_ref().as_deref().unwrap();
+        assert_eq!(b, b"test");
 
         Ok(())
     }
