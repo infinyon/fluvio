1836c5ca02678f6ad1aac68f5a4a0a2581fbe9ab
Nick Mosher <nicholastmosher@gmail.com>
2020-09-21 17:21:15 -0400

    Update top-level module doc example

--- src/client/src/lib.rs
+++ src/client/src/lib.rs
@@ -35,15 +35,55 @@
 //! topic "echo" created
 //! ```
 //!
-//! To produce some records, you'll need to initialize the Fluvio client
+//! There are convenience methods that let you get up-and-started quickly using default
+//! configurations. Later if you want to customize your setup, you can directly use the
+//! [`Fluvio`] client object.
 //!
 //! ```no_run
-//! # use fluvio::{Fluvio, FluvioError};
-//! # async fn do_init_fluvio() -> Result<(), FluvioError> {
-//! let fluvio = Fluvio::connect().await?;
-//! # Ok(())
-//! # }
+//! use std::time::Duration;
+//! use fluvio::{Fluvio, FluvioError};
+//! use fluvio::params::{FetchOffset, FetchLogOption};
+//!
+//! fn main() {
+//!     async_std::task::spawn(produce_records());
+//!     if let Err(e) = async_std::task::block_on(consume_records()) {
+//!         println!("Error: {}", e);
+//!     }
+//! }
+//!
+//! async fn produce_records() -> Result<(), FluvioError> {
+//!     let producer = fluvio::producer("echo").await?;
+//!     for i in 0..10u8 {
+//!         producer.send_record(format!("Hello, Fluvio {}!", i), 0).await?;
+//!         async_std::task::sleep(Duration::from_secs(1)).await;
+//!     }
+//!     Ok(())
+//! }
+//!
+//! async fn consume_records() -> Result<(), FluvioError> {
+//!     let consumer = fluvio::consumer("echo", 0).await?;
+//!     let offset = FetchOffset::Earliest(None);
+//!     let fetch_config = FetchLogOption::default();
+//!     let mut stream = consumer.stream(offset, fetch_config).await?;
+//!
+//!     while let Ok(event) = stream.next().await {
+//!         for batch in event.partition.records.batches {
+//!             for record in batch.records {
+//!                 if let Some(record) = record.value.inner_value() {
+//!                     let string = String::from_utf8(record)
+//!                         .expect("record should be a string");
+//!                     println!("Got record: {}", string);
+//!                 }
+//!             }
+//!         }
+//!     }
+//!     Ok(())
+//! }
+//!
 //! ```
+//!
+//! [Fluvio CLI]: https://nightly.fluvio.io/docs/cli/
+//! [`Fluvio`]: ./struct.Fluvio.html
 #![cfg_attr(
     feature = "nightly",
     doc(include = "../../../website/kubernetes/INSTALL.md")
