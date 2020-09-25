5108525d7eb98d436b2d9926d399627be600e643
Sehyo <sehyo@fluvio.io>
2020-09-25 04:25:43 +0000

    update the test consumer to ensure exit loop correctly

--- tests/runner/src/tests/smoke/consume.rs
+++ tests/runner/src/tests/smoke/consume.rs
@@ -54,11 +54,12 @@ fn validate_consume_message_cli(option: &TestOption,offsets: Offsets) {
 async fn validate_consume_message_api(offsets: Offsets,option: &TestOption) {
     let client = Fluvio::connect().await.expect("should connect");
     let replication = option.replication();
+    let iteration = option.produce.produce_iteration;
 
     for i in 0..replication {
         let topic_name = option.topic_name(i);
         let base_offset = offsets.get(&topic_name).expect("offsets");
-        println!("starting fetch stream for: {} at: {}",topic_name,base_offset);
+        println!("starting fetch stream for: {} at: {}, expected new records: {}",topic_name,base_offset,iteration);
         
         let consumer = client
             .partition_consumer(topic_name.clone(), 0)
@@ -68,7 +69,7 @@ async fn validate_consume_message_api(offsets: Offsets,option: &TestOption) {
         let mut stream = consumer.stream(Offset::absolute(*base_offset).unwrap()).await.expect("start from beginning");
 
         let mut total_records: u16 = 0;
-        while let Ok(event) = stream.next().await {
+        'outer: while let Ok(event) = stream.next().await {
             let batches = event.partition.records.batches;
             println!("consumer: received batches {}",batches.len() );
             for batch in batches {
@@ -78,10 +79,11 @@ async fn validate_consume_message_api(offsets: Offsets,option: &TestOption) {
                        validate_message(offset,&topic_name,option, &bytes); 
                        offset += 1;   
                        total_records += 1;
-                       println!("consumer: records: {}, validated offset: {}",total_records,offset); 
+                       println!("   consumer: total records: {}, validated offset: {}",total_records,offset); 
                        assert_eq!(offset, base_offset + total_records as i64);  
-                       if total_records  == option.produce.produce_iteration {
-                           break;
+                       if total_records  == iteration {
+                           println!("<<consume test done for: {} >>>>",topic_name);
+                           break 'outer;
                        }             
                     }
                 }
