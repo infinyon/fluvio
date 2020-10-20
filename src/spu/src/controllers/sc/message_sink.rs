86627ceb4c6bd0a3d747dd1e3adcc75b0736a5cc
Sehyo <sehyo@fluvio.io>
2020-10-20 16:47:12 +0000

    interim check-in

--- src/spu/src/controllers/sc/message_sink.rs
+++ src/spu/src/controllers/sc/message_sink.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use async_mutex::Mutex;
+use event_listener::{Event, EventListener};
 use tracing::error;
 use tracing::trace;
 
@@ -15,24 +16,39 @@ use dataplane::api::RequestMessage;
 pub type SharedSinkMessageChannel = Arc<ScSinkMessageChannel>;
 
 /// channel used to send message to sc
-pub struct ScSinkMessageChannel(Mutex<HashSet<LrsRequest>>);
+pub struct ScSinkMessageChannel {
+    requests: Mutex<HashSet<LrsRequest>>,
+    event: Event,
+}
 
 impl ScSinkMessageChannel {
     pub fn new() -> Self {
-        Self(Mutex::new(HashSet::new()))
+        Self {
+            requests: Mutex::new(HashSet::new()),
+            event: Event::new(),
+        }
     }
 
     /// send lrs request sc
     /// newer entry will overwrite previous if it has not been cleared
     pub async fn send(&self, request: LrsRequest) {
-        let mut lock = self.0.lock().await;
+        let mut lock = self.requests.lock().await;
         lock.insert(request);
+        self.event.notify(usize::MAX);
     }
 
-    pub async fn remove_all(&self) -> Vec<LrsRequest> {
-        let mut lock = self.0.lock().await;
+    async fn remove_all(&self) -> Vec<LrsRequest> {
+        let mut lock = self.requests.lock().await;
 
         lock.drain().collect()
+    }
+
+    async fn len(&self) -> usize {
+        self.requests.lock().await.len()
+    }
+
+    fn listen(&self) -> EventListener {
+        self.event.listen()
     }
 }
 
@@ -56,8 +72,19 @@ impl ScMessageSinkDispatcher {
         const WAIT_TIME: Duration = Duration::from_millis(1);
 
         use fluvio_future::timer::sleep;
+        use tokio::select;
 
+        let mut delay = 0;
+        self.send_batch_status().await;
         loop {
+            let listener = self.channel.listen();
+            listener.await;
+            if delay < 0 {
+                self.send_batch_status().await;
+            } else {
+            }
+            // await for next
+
             // we are going to rate limit
             sleep(Duration::from_millis(1)).await;
             self.send_batch_status().await;
@@ -81,14 +108,13 @@ impl ScMessageSinkDispatcher {
 #[cfg(test)]
 mod test {
 
-    use std::time::Instant;
     use std::time::Duration;
     use std::sync::Arc;
     use std::sync::atomic::AtomicU64;
     use std::sync::atomic::Ordering;
 
     use log::debug;
-    use futures_util::future::join;
+    use futures_util::future::join3;
     use event_listener::Event;
 
     use fluvio_future::timer::sleep;
@@ -100,27 +126,44 @@ mod test {
         let counter = Arc::new(AtomicU64::new(0));
         //  let event2 = event.clone();
 
-        let (a, b) = join(
+        let (_a, b, c) = join3(
             async {
-                let t1 = Instant::now();
+                sleep(Duration::from_millis(20)).await;
                 counter.fetch_add(1, Ordering::SeqCst);
                 event.notify(usize::MAX);
-                t1
+                false
             },
             async {
+                // start before notification so listener will catch notification
                 sleep(Duration::from_millis(10)).await;
-                // we still 0 then wait
+                // will trigger listen
                 if counter.load(Ordering::SeqCst) == 0 {
                     event.listen().await;
                     debug!("waiting");
+                    true
+                } else {
+                    false
                 }
-
-                Instant::now()
+            },
+            async {
+                // event listener comes after notify
+                // which we misses. so we need to check counter
+                sleep(Duration::from_millis(30)).await;
+                // we still 0 then wait
+                if counter.load(Ordering::SeqCst) == 0 {
+                    // this will not occur
+                    event.listen().await;
+                    debug!("waiting");
+                    false
+                } else {
+                    true
+                }
             },
         )
         .await;
 
-        assert!(b > a);
+        assert!(b);
+        assert!(c);
 
         Ok(())
     }
