2e6257a23397466ca157d9fc4a460d970ff1a90e
Nick Mosher <nicholastmosher@gmail.com>
2020-10-26 16:11:16 -0400

    Add package index crate

---
+++ src/package-index/src/lib.rs
@@ -0,0 +1,397 @@
+//! Data structures and domain logic for reading the Fluvio Package Index.
+//!
+//! This crate is used by the plugin installer and the self-updater. It
+//! is capable of reading the index file in the Fluvio Package Registry
+//! in order to find the latest release versions of various components.
+//!
+//! The two main use-cases for this are to allow the CLI to install plugins,
+//! e.g. via `fluvio install fluvio-cloud`, and to give the CLI visibility
+//! of new releases for itself and plugins.
+
+use std::collections::BTreeMap;
+use serde::{Serialize, Deserialize};
+
+mod package_id;
+pub use package_id::{PackageId, GroupName, PackageName, PackageIdError, Registry};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub const INDEX_CLIENT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Failed to parse PackageId")]
+    PackageIdParseError(#[from] PackageIdError),
+    #[error("Failed to lookup package: group {0} does not exist")]
+    MissingGroup(GroupName),
+    #[error("Failed to lookup package: package {0} does not exist")]
+    MissingPackage(PackageName),
+    #[error("Failed to lookup package: release version {0} does not exist")]
+    MissingRelease(semver::Version),
+    #[error("Failed to create new package {0}: it already exists")]
+    PackageAlreadyExists(String),
+    #[error("Failed to add release: release version {0} already exists")]
+    ReleaseAlreadyExists(semver::Version),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FluvioIndex {
+    /// Metadata about the Fluvio Index itself
+    #[serde(rename = "index")]
+    pub metadata: IndexMetadata,
+    /// Packages are organized by group, where a group is like a particular
+    /// user which publishes packages. For the initial index, there will only
+    /// be an official Fluvio group, but in the future there may be more.
+    /// This will prevent clashes in the package namespace.
+    pub groups: BTreeMap<GroupName, GroupPackages>,
+}
+
+impl FluvioIndex {
+    pub fn find_package(&self, id: &str) -> Result<&Package> {
+        let id = id.parse::<PackageId>()?;
+        let group = self.group(&id.group)?;
+        let package = group.package(&id.name)?;
+        Ok(package)
+    }
+
+    pub fn find_release(&self, id: &str) -> Result<&Release> {
+        let id = id.parse::<PackageId>()?;
+        let version = id.version.ok_or(PackageIdError::MissingVersion)?;
+        let group = self.group(&id.group)?;
+        let package = group.package(&id.name)?;
+        let release = package.find_release(&version)?;
+        Ok(release)
+    }
+
+    pub fn add_package(&mut self, package: Package) -> Result<()> {
+        let group = self.groups.entry(package.group.clone())
+            .or_insert_with(|| GroupPackages::empty(package.group.clone()));
+        group.add_package(package.name.clone(), package)?;
+        Ok(())
+    }
+
+    pub fn add_release(&mut self, id: &str, release: Release) -> Result<()> {
+        let id = id.parse::<PackageId>()?;
+        let group = self.group_mut(&id.group)?;
+        let package = group.package_mut(&id.name)?;
+        package.add_release(release)?;
+        Ok(())
+    }
+
+    fn empty(metadata: IndexMetadata) -> FluvioIndex {
+        FluvioIndex {
+            metadata,
+            groups: BTreeMap::new(),
+        }
+    }
+
+    fn group(&self, group: &GroupName) -> Result<&GroupPackages> {
+        self.groups.get(group).ok_or_else(|| Error::MissingGroup(group.clone()))
+    }
+
+    fn group_mut(&mut self, group: &GroupName) -> Result<&mut GroupPackages> {
+        self.groups.get_mut(group).ok_or_else(|| Error::MissingGroup(group.clone()))
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct IndexMetadata {
+    /// The index itself is given a version so that installers and updaters
+    /// can print upgrade prompts to users. If an installer is expecting an
+    /// index version `1.0.0` and finds an index with a version such as
+    /// `2.0.0`, the installer will know to prompt the user to self-update
+    /// the installer. The original index is expected to continue to be available
+    /// forever, and, critically, new versions of the installer must always
+    /// be published to the original index. New versions of the installer may
+    /// then point to future versions of the Fluvio Index, which may exhibit
+    /// breaking changes.
+    pub version: semver::Version,
+    /// The location of this registry as a URL
+    pub registry: Registry,
+    /// The minimum version of a client which must be used in order
+    /// to properly access the index. If a client finds itself with a lower
+    /// version than this minimum, it must prompt the user for an update before
+    /// it can proceed.
+    pub minimum_client_version: semver::Version,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GroupPackages {
+    group: GroupName,
+    packages: BTreeMap<PackageName, Package>,
+}
+
+impl GroupPackages {
+    pub fn empty(group: GroupName) -> Self {
+        Self {
+            group,
+            packages: BTreeMap::new(),
+        }
+    }
+
+    /// Adds the given package with the given name to this group.
+    ///
+    /// This will reject packages that already exist.
+    pub fn add_package(&mut self, name: PackageName, package: Package) -> Result<()> {
+        if self.packages.contains_key(&name) {
+            return Err(Error::PackageAlreadyExists(format!("{}/{}", self.group, name.as_str())));
+        }
+        self.packages.insert(name, package);
+        Ok(())
+    }
+
+    fn package(&self, package: &PackageName) -> Result<&Package> {
+        self.packages.get(package).ok_or_else(|| Error::MissingPackage(package.clone()))
+    }
+
+    fn package_mut(&mut self, package: &PackageName) -> Result<&mut Package> {
+        self.packages.get_mut(package).ok_or_else(|| Error::MissingPackage(package.clone()))
+    }
+}
+
+/// A `Package` represents a single published item in Fluvio's registry.
+///
+/// Each time you publish an updated version of a package, that version is
+/// known as a `Release`.
+///
+/// A package has a specified type, and all releases of that package must
+/// be the same type.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Package {
+    /// The unique name of this package
+    name: PackageName,
+    /// The ID of the group that published the package
+    group: GroupName,
+    /// The type of package this is
+    kind: PackageKind,
+    /// The author of this package
+    author: Option<String>,
+    /// The human-readable description of this package
+    description: Option<String>,
+    /// A link to the source code repository of this package
+    repository: Option<String>,
+    /// The instances of this package that have been published
+    releases: Vec<Release>,
+}
+
+impl Package {
+    pub fn new_binary<S1, S2, S3>(id: &PackageId, author: S1, desc: S2, repo: S3) -> Self
+        where S1: Into<String>,
+              S2: Into<String>,
+              S3: Into<String>,
+    {
+        let author = author.into();
+        let description = desc.into();
+        let repository = repo.into();
+        Package {
+            name: id.name.clone(),
+            group: id.group.clone(),
+            kind: PackageKind::Binary,
+            author: Some(author),
+            description: Some(description),
+            repository: Some(repository),
+            releases: vec![],
+        }
+    }
+
+    fn find_release(&self, version: &semver::Version) -> Result<&Release> {
+        self.releases.iter().find(|it| it.version == *version)
+            .ok_or_else(|| Error::MissingRelease(version.clone()))
+    }
+
+    /// Adds a new release to this package. This will reject a release if a release by the same version exists.
+    ///
+    /// Version equality is based strictly on the numeric components of a semantic
+    /// version. Therefore, if a release with version `0.1.0-alpha` exists, you
+    /// cannot add a release with version `0.1.0-beta`, since there is no way to know
+    /// which of those is more recent.
+    fn add_release(&mut self, new_release: Release) -> Result<()> {
+        // This will reject adding a release if the version numbers are the same
+        let maybe_conflict = self.releases.iter().find(|it| it.version == new_release.version);
+        if let Some(conflict) = maybe_conflict {
+            return Err(Error::ReleaseAlreadyExists(conflict.version.clone()));
+        }
+
+        self.releases.push(new_release);
+        self.releases.sort_by(|a, b| a.version.cmp(&b.version));
+        Ok(())
+    }
+}
+
+/// Packages have a `PackageKind`, which describes the contents being distributed.
+///
+/// This is used by installers and updaters to determine what the installation
+/// strategy should be for a specific type of package. For example, binaries need
+/// to be placed into the PATH, but libraries may need to be installed in a
+/// platform-specific way.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum PackageKind {
+    #[serde(rename = "bin")]
+    Binary,
+}
+
+/// A `Release` is a specific version of a published item in Fluvio's registry.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Release {
+    /// The version of the package that this release holds
+    version: semver::Version,
+    /// A permalink where this release may be downloaded
+    permalink: url::Url,
+    /// The hex-encoded SHA2 hash of the package contents
+    checksum: String,
+    /// If a release is yanked, no client should ever try to download it.
+    /// A yanked package may have its permalink taken down.
+    yanked: bool,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    fn test_path() -> PathBuf {
+        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src/bin/index.json");
+        println!("Path: {}", path.display());
+        path
+    }
+
+    fn sample_index() -> FluvioIndex {
+        let release = Release {
+            version: semver::Version::parse("1.0.0").unwrap(),
+            permalink: url::Url::parse("https://packages.fluvio.io/v1/packages/fluvio-cli/1.0.0/fluvio").unwrap(),
+            checksum: "BLAAAAAH".to_string(),
+            yanked: false,
+        };
+        let package_name = "fluvio-cli".parse::<PackageName>().unwrap();
+        let group_name = "fluvio".parse::<GroupName>().unwrap();
+        let package = Package {
+            name: package_name.clone(),
+            group: group_name.clone(),
+            kind: PackageKind::Binary,
+            author: None,
+            description: None,
+            repository: None,
+            releases: vec![release],
+        };
+        let group_packages = GroupPackages {
+            group: group_name.clone(),
+            packages: {
+                let mut packages = BTreeMap::new();
+                packages.insert(package_name, package);
+                packages
+            }
+        };
+        let metadata = IndexMetadata {
+            version: semver::Version::parse("0.1.0").unwrap(),
+            registry: "https://packages.fluvio.io/v1/".parse().unwrap(),
+            minimum_client_version: semver::Version::parse("0.6.0-alpha-1").unwrap(),
+        };
+        let index = FluvioIndex {
+            metadata,
+            groups: {
+                let mut groups = BTreeMap::new();
+                groups.insert(group_name, group_packages);
+                groups
+            }
+        };
+        index
+    }
+
+    fn write_index() {
+        let metadata = IndexMetadata {
+            version: semver::Version::parse("0.1.0").unwrap(),
+            registry: "https://packages.fluvio.io/v1/".parse().unwrap(),
+            minimum_client_version: semver::Version::parse("0.6.0-alpha-1").unwrap(),
+        };
+        let index = FluvioIndex::empty(metadata);
+        let index_string = serde_json::to_string_pretty(&index).unwrap();
+        std::fs::write(test_path(), &index_string).unwrap();
+    }
+
+    #[test]
+    fn test_create_index() {
+        let index = sample_index();
+        let index_string = serde_json::to_string_pretty(&index).unwrap();
+        std::fs::write(test_path(), &index_string).unwrap();
+    }
+
+    #[test]
+    fn test_find_release() {
+        let index = sample_index();
+        let release = index.find_release("fluvio/fluvio-cli:1.0.0").unwrap();
+        assert_eq!(release.version, semver::Version::parse("1.0.0").unwrap());
+        assert_eq!(release.permalink, url::Url::parse("https://packages.fluvio.io/v1/packages/fluvio-cli/1.0.0/fluvio").unwrap());
+    }
+
+    #[test]
+    fn test_add_package() {
+        let mut starting_index = sample_index();
+        let id = "fluvio/fluvio-cloud".parse::<PackageId>().unwrap();
+        let package = Package::new_binary(
+            &id,
+            "Bobson",
+            "The fluvio cloud plugin",
+            "https://github.com/infinyon/fluvio",
+        );
+        starting_index.add_package(package);
+    }
+
+    #[test]
+    fn test_add_release() {
+        let mut index = sample_index();
+        let new_release = Release {
+            version: semver::Version::parse("1.1.0").unwrap(),
+            permalink: url::Url::parse("https://packages.fluvio.io/v1/packages/fluvio-cli/1.1.0/fluvio").unwrap(),
+            checksum: "BLAAAAAH".to_string(),
+            yanked: false,
+        };
+        let pid = "fluvio/fluvio-cli:1.1.0";
+        index.add_release(&pid, new_release.clone()).unwrap();
+        let searched_release = index.find_release(&pid).unwrap();
+        assert_eq!(new_release, *searched_release);
+    }
+
+    #[test]
+    fn test_releases_sorted() {
+        let mut index = sample_index();
+        let release_2 = Release {
+            version: semver::Version::parse("2.0.0").unwrap(),
+            permalink: url::Url::parse("https://packages.fluvio.io/v1/packages/fluvio-cli/2.0.0/fluvio").unwrap(),
+            checksum: "BLAAAAAH".to_string(),
+            yanked: false,
+        };
+        let pid = "fluvio/fluvio-cli:2.0.0";
+        index.add_release(&pid, release_2.clone()).unwrap();
+
+        let release_1_1 = Release {
+            version: semver::Version::parse("1.1.0").unwrap(),
+            permalink: url::Url::parse("https://packages.fluvio.io/v1/packages/fluvio-cli/1.1.0/fluvio").unwrap(),
+            checksum: "BLAAAAAH".to_string(),
+            yanked: false,
+        };
+        let id = "fluvio/fluvio-cli:1.1.0".parse::<PackageId>().unwrap();
+        index.add_release(&id.to_string(), release_1_1.clone()).unwrap();
+
+        let group_packages = index.group(&id.group).unwrap();
+        let package = group_packages.package(&id.name).unwrap();
+
+        let releases = package.releases.clone();
+        for windows in releases.windows(2) {
+            match &windows {
+                &[a, b] => {
+                    println!("Is {} < {}?", a.version, b.version);
+                    assert!(a.version < b.version);
+                }
+                _ => panic!("Should have 2 windows"),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/package-index/src/package_id.rs
@@ -0,0 +1,323 @@
+use std::fmt;
+use serde::{Serialize, Deserialize, Deserializer};
+use url::Url;
+
+#[derive(thiserror::Error, Debug)]
+pub enum PackageIdError {
+    #[error("PackageIds must have at least one `/` separator: <group>/<name>:<version>")]
+    TooFewSlashes,
+    #[error("PackageIds must have zero or one `:` separator: <name>(:<version>)?")]
+    InvalidNameVersionSegment,
+    #[error("Invalid semver")]
+    InvalidSemver(#[from] semver::SemVerError),
+    #[error("Invalid package name: {0}")]
+    InvalidPackageName(String),
+    #[error("Invalid group name: {0}")]
+    InvalidGroupName(String),
+    #[error("Version number is required here")]
+    MissingVersion,
+    #[error("Failed to parse registry segment of PackageId")]
+    FailedToParseRegistry(#[from] url::ParseError),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct Registry(url::Url);
+
+lazy_static::lazy_static! {
+    static ref DEFAULT_REGISTRY: Registry = {
+        let url = url::Url::parse("https://packages.fluvio.io/v1/").unwrap();
+        Registry::from(url)
+    };
+}
+
+impl fmt::Display for Registry {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Default for Registry {
+    fn default() -> Self {
+        DEFAULT_REGISTRY.clone()
+    }
+}
+
+impl std::str::FromStr for Registry {
+    type Err = url::ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let url = url::Url::parse(s)?;
+        Ok(Self(url))
+    }
+}
+
+impl From<url::Url> for Registry {
+    fn from(url: url::Url) -> Self {
+        Self(url)
+    }
+}
+
+impl AsRef<url::Url> for Registry {
+    fn as_ref(&self) -> &Url {
+        &self.0
+    }
+}
+
+/// Certain newtypes are just Strings but cannot contain a '/' character.
+///
+/// This macro is used to derive the deserialization rules for those types.
+macro_rules! deserialize_no_slash_string {
+    ($mod:ident, $id:ident, $err:expr, $string_name:expr $(,)?) => {
+        mod $mod {
+            use super::$id;
+            use super::PackageIdError;
+            impl std::str::FromStr for $id {
+                type Err = PackageIdError;
+
+                fn from_str(s: &str) -> Result<Self, Self::Err> {
+                    if s.find('/').is_some() {
+                        return Err($err("cannot contain '/'".to_string()));
+                    }
+                    if s.find(':').is_some() {
+                        return Err($err("cannot contain ':'".to_string()));
+                    }
+                    return Ok(Self(s.to_string()))
+                }
+            }
+
+            impl $id {
+                pub fn as_str(&self) -> &str {
+                    &self.0
+                }
+            }
+
+            impl<'de> serde::Deserialize<'de> for $id {
+                fn deserialize<D>(deserializer: D) -> Result<Self, <D as serde::Deserializer<'de>>::Error> where
+                    D: serde::Deserializer<'de>
+                {
+                    let string = String::deserialize(deserializer)?;
+
+                    // Reject strings that have `/` in them
+                    if let Err(e) = string.parse::<$id>() {
+                        use serde::de::{Unexpected, Error as DeserializeError};
+                        return Err(DeserializeError::invalid_value(
+                            Unexpected::Other(&e.to_string()),
+                            &&*format!("valid {}", $string_name)
+                        ));
+                    }
+
+                    Ok(Self(string))
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Serialize)]
+#[serde(transparent)]
+pub struct GroupName(String);
+
+impl fmt::Display for GroupName {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+deserialize_no_slash_string!(group, GroupName, PackageIdError::InvalidGroupName, "index group");
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Serialize)]
+#[serde(transparent)]
+pub struct PackageName(String);
+
+impl fmt::Display for PackageName {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+deserialize_no_slash_string!(package_name, PackageName, PackageIdError::InvalidPackageName, "package name");
+
+/// A unique identifier for a package that describes its registry, group,
+/// name, and version.
+///
+/// A PackageId may be represented as a URL-style path, such as:
+///
+/// `<registry>/<group>/<name>:<version>`
+///
+/// Note that the `<registry>/` portion is optional, and will default to
+/// the value `https://packages.fluvio.io/v1/` if omitted. Therefore, it
+/// is common to write package IDs as:
+///
+/// `<group>/<name>:<version>`
+#[derive(Debug, Clone, PartialEq)]
+pub struct PackageId {
+    registry: Option<Registry>,
+    pub group: GroupName,
+    pub name: PackageName,
+    pub version: Option<semver::Version>,
+}
+
+impl PackageId {
+    pub fn registry(&self) -> &Registry {
+        match self.registry.as_ref() {
+            Some(registry) => registry,
+            None => &*DEFAULT_REGISTRY,
+        }
+    }
+}
+
+impl std::str::FromStr for PackageId {
+    type Err = PackageIdError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut segments: Vec<&str> = s.split('/').collect();
+        if segments.len() < 2 { return Err(PackageIdError::TooFewSlashes); }
+
+        let name_version_segment = segments.pop().unwrap();
+        let name_version_segments: Vec<&str> = name_version_segment.split(':').collect();
+        let (name_string, version_string) = match &name_version_segments[..] {
+            &[name_string] => (name_string, None),
+            &[name_string, version_string] => (name_string, Some(version_string)),
+            _ => return Err(PackageIdError::InvalidNameVersionSegment),
+        };
+
+        let version = match version_string {
+            Some(version_string) => Some(semver::Version::parse(version_string)?),
+            None => None,
+        };
+        let name: PackageName = name_string.parse()?;
+
+        let group_string = segments.pop().unwrap();
+        let group: GroupName = group_string.parse()?;
+
+        let registry = if segments.is_empty() {
+            None
+        } else {
+            let mut reconstructed: String = segments.join("/");
+            if !reconstructed.ends_with('/') {
+                reconstructed.push('/');
+            }
+            let registry_url = url::Url::parse(&reconstructed)?;
+            let registry = Registry::from(registry_url);
+            Some(registry)
+        };
+
+        let package_id = PackageId {
+            registry,
+            group,
+            name,
+            version,
+        };
+
+        Ok(package_id)
+    }
+}
+
+impl fmt::Display for PackageId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let registry = self.registry.as_ref()
+            .map(|it| it.0.as_str())
+            .unwrap_or("");
+        let version = self.version.as_ref()
+            .map(|it| format!(":{}", it))
+            .unwrap_or_else(|| "".to_string());
+        write!(f, "{registry}{group}/{name}{version}",
+            registry = registry,
+            group = self.group.as_str(),
+            name = self.name.as_str(),
+            version = version,
+        )
+    }
+}
+
+impl<'de> Deserialize<'de> for PackageId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error> where
+        D: Deserializer<'de>
+    {
+        let string = String::deserialize(deserializer)?;
+        let package_id = match string.parse::<PackageId>() {
+            Ok(pid) => pid,
+            Err(e) => {
+                use serde::de::{Unexpected, Error as DeserializeError};
+                return Err(DeserializeError::invalid_value(
+                    Unexpected::Other("Invalid PackageId"),
+                    &&*format!("A PackageId, <registry>/<group>/<name>:<version>, where <registry> is optional: {}", e),
+                ));
+            }
+        };
+        Ok(package_id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::package_id::{Registry, PackageId};
+    use semver::Version;
+
+    #[test]
+    fn test_deserialize_group() {
+        let group: GroupName = "fluvio.io".parse().unwrap();
+
+        #[derive(Debug, Deserialize, Serialize)]
+        struct JsonGroup {
+            group: GroupName,
+        }
+
+        let json_group = JsonGroup { group: group.clone() };
+        let group_string = serde_json::to_string(&json_group).unwrap();
+
+        let read_json_group: JsonGroup = serde_json::from_str(&group_string).unwrap();
+        let read_group = read_json_group.group;
+        assert_eq!(group, read_group);
+
+        assert!("fluvio/io".parse::<GroupName>().is_err());
+
+        let bad_json_group = JsonGroup {
+            group: GroupName("fluvio/io".to_string()),
+        };
+        let bad_group_string = serde_json::to_string(&bad_json_group).unwrap();
+        let read_bad_group_result = serde_json::from_str::<JsonGroup>(&bad_group_string);
+        assert!(read_bad_group_result.is_err());
+    }
+
+    #[test]
+    fn test_parse_package_id_default_registry() {
+        let package_id: PackageId = "fluvio.io/fluvio:0.6.0".parse().unwrap();
+        assert_eq!(package_id.registry(), &Registry::default());
+        assert_eq!(package_id.group.as_str(), "fluvio.io");
+        assert_eq!(package_id.name.as_str(), "fluvio");
+        assert_eq!(package_id.version, Some(Version::parse("0.6.0").unwrap()));
+    }
+
+    #[test]
+    fn test_parse_package_id_custom_registry() {
+        let registry_url = "https://other.registry.io/v2/";
+        let package_id: PackageId = format!("{}/fluvio.io/fluvio:0.6.0", registry_url).parse().unwrap();
+        assert_eq!(package_id.registry(), &registry_url.parse().unwrap());
+        assert_eq!(package_id.group.as_str(), "fluvio.io");
+        assert_eq!(package_id.name.as_str(), "fluvio");
+        assert_eq!(package_id.version, Some(Version::parse("0.6.0").unwrap()));
+    }
+
+    #[test]
+    fn test_package_id_idempotent() {
+        let package_id = PackageId {
+            registry: Some("https://some.registry.somewhere/v11/".parse().unwrap()),
+            group: "infinyon.super.secret.division".parse().unwrap(),
+            name: "project-x-secret-sauce".parse().unwrap(),
+            version: Some(Version::parse("100.0.0-special-edition").unwrap()),
+        };
+
+        let package_id_string = package_id.to_string();
+        assert_eq!(package_id_string,
+                   "https://some.registry.somewhere/v11/\
+            infinyon.super.secret.division/\
+            project-x-secret-sauce:100.0.0-special-edition"
+        );
+
+        let parsed_package_id: PackageId = package_id_string.parse().unwrap();
+        assert_eq!(package_id, parsed_package_id);
+    }
+}
