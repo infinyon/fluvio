6e4b1a079439e7b81ee9a43152a2698131367298
Nick Mosher <nicholastmosher@gmail.com>
2020-11-05 13:07:02 -0500

    Add prompts for required and available updates

--- src/cli/src/install/plugins.rs
+++ src/cli/src/install/plugins.rs
@@ -2,6 +2,7 @@ use structopt::StructOpt;
 use fluvio_index::{PackageId, Target, HttpAgent};
 use crate::CliError;
 use crate::install::{fetch_latest_version, fetch_package_file, fluvio_bin_dir, install_bin};
+use crate::install::update::{check_update_required, prompt_required_update, check_update_available, prompt_available_update};
 
 #[derive(StructOpt, Debug)]
 pub struct InstallOpt {
@@ -10,8 +11,30 @@ pub struct InstallOpt {
 
 impl InstallOpt {
     pub async fn process(self) -> Result<String, CliError> {
-        let target: Target = fluvio_index::PACKAGE_TARGET.parse()?;
         let agent = HttpAgent::default();
+
+        // Before any "install" type command, check if the CLI needs updating.
+        // This may be the case if the index schema has updated.
+        let require_update = check_update_required(&agent).await?;
+        if require_update {
+            prompt_required_update(&agent).await?;
+            return Ok("".to_string());
+        }
+
+        self.install_plugin(&agent).await?;
+
+        // After any 'install' command, check if the CLI has an available update,
+        // i.e. one that is not required, but present.
+        let update_available = check_update_available(&agent).await?;
+        if update_available {
+            prompt_available_update(&agent).await?;
+        }
+
+        Ok("".to_string())
+    }
+
+    async fn install_plugin(self, agent: &HttpAgent) -> Result<String, CliError> {
+        let target: Target = fluvio_index::PACKAGE_TARGET.parse()?;
 
         // If a version is given in the package ID, use it. Otherwise, use latest
         let id = match self.id.version.as_ref() {
@@ -22,7 +45,7 @@ impl InstallOpt {
             None => {
                 let mut id = self.id;
                 println!("🎣 Fetching latest version for package: {}...", &id);
-                let version = fetch_latest_version(&agent, &id, target).await?;
+                let version = fetch_latest_version(agent, &id, target).await?;
                 id.version = Some(version);
                 println!("⏳ Downloading package with latest version: {}...", &id);
                 id
@@ -30,12 +53,14 @@ impl InstallOpt {
         };
 
         // Download the package file from the package registry
-        let package_file = fetch_package_file(&agent, &id, target).await?;
+        let package_file = fetch_package_file(agent, &id, target).await?;
         println!("🔑 Downloaded and verified package file");
 
         // Install the package to the ~/.fluvio/bin/ dir
         let fluvio_dir = fluvio_bin_dir()?;
         install_bin(&fluvio_dir, id.name.as_str(), &package_file)?;
-        Ok(format!("✅ Successfully installed ~/.fluvio/bin/{}", &id.name))
+        println!("✅ Successfully installed ~/.fluvio/bin/{}", &id.name);
+
+        Ok("".to_string())
     }
 }
--- src/cli/src/install/update.rs
+++ src/cli/src/install/update.rs
@@ -3,6 +3,7 @@ use structopt::StructOpt;
 use crate::CliError;
 use fluvio_index::{PackageId, HttpAgent, Target};
 use crate::install::{fetch_latest_version, fetch_package_file, install_bin, fluvio_bin_dir};
+use semver::Version;
 
 const FLUVIO_PACKAGE_ID: &str = "fluvio/fluvio";
 
@@ -34,18 +35,59 @@ async fn update_self(agent: &HttpAgent) -> Result<String, CliError> {
     // Install the package to the ~/.fluvio/bin/ dir
     let fluvio_dir = fluvio_bin_dir()?;
     install_bin(&fluvio_dir, "fluvio", &package_file)?;
-    Ok(format!("✅ Successfully installed ~/.fluvio/bin/{}", &id.name))
+    println!("✅ Successfully installed ~/.fluvio/bin/{}", &id.name);
+
+    Ok("".to_string())
 }
 
 /// Check whether the index requires a more recent version of the client.
 ///
 /// If this is the case, we need to prompt the user to perform an update.
-pub async fn check_update_required() -> Result<bool, CliError> {
-    let agent = HttpAgent::default();
+pub async fn check_update_required(agent: &HttpAgent) -> Result<bool, CliError> {
     let request = agent.request_index()?;
     let response = crate::http::execute(request).await?;
     let index = agent.index_from_response(response).await?;
     Ok(index.metadata.update_required())
 }
 
-pub async fn prompt_update() {}
+/// Check whether there is any newer version of the Fluvio CLI available
+pub async fn check_update_available(agent: &HttpAgent) -> Result<bool, CliError> {
+    let target: Target = fluvio_index::PACKAGE_TARGET.parse()?;
+
+    let request = agent.request_index()?;
+    let response = crate::http::execute(request).await?;
+    let index = agent.index_from_response(response).await?;
+
+    let id: PackageId = FLUVIO_PACKAGE_ID.parse()?;
+    let package = index.find_package(&id)?;
+    let release = package.latest_release_for_target(target)?;
+    let latest_version = &release.version;
+
+    let current_version = Version::parse(crate::VERSION)
+        .expect("Fluvio CLI 'VERSION' should be a valid semver");
+
+    Ok(current_version < *latest_version)
+}
+
+/// Prompt the user about a more recent version of the Fluvio CLI
+pub async fn prompt_required_update(agent: &HttpAgent) -> Result<(), CliError> {
+    let target: Target = fluvio_index::PACKAGE_TARGET.parse()?;
+    let id: PackageId = FLUVIO_PACKAGE_ID.parse()?;
+    let latest_version = fetch_latest_version(agent, &id, target).await?;
+
+    println!("⚠️ A major update to Fluvio has been detected!");
+    println!("⚠️ You must complete this update before using any 'install' command");
+    println!("⚠️     Run 'fluvio update' to install v{} of Fluvio", &latest_version);
+    Ok(())
+}
+
+pub async fn prompt_available_update(agent: &HttpAgent) -> Result<(), CliError> {
+    let target: Target = fluvio_index::PACKAGE_TARGET.parse()?;
+    let id: PackageId = FLUVIO_PACKAGE_ID.parse()?;
+    let latest_version = fetch_latest_version(agent, &id, target).await?;
+
+    println!();
+    println!("💡 An update to Fluvio is available!");
+    println!("💡     Run 'fluvio update' to install v{} of Fluvio", &latest_version);
+    Ok(())
+}
