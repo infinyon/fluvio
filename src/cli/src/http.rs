1d26e68056aeeaec13e0e7f390b9cc0e5202523e
Nick Mosher <nicholastmosher@gmail.com>
2020-11-06 09:20:09 -0500

    Return error on non-http URLs for http client

--- src/cli/src/http.rs
+++ src/cli/src/http.rs
@@ -1,3 +1,4 @@
+use std::io::{ErrorKind, Error as IoError};
 use async_h1::client;
 use http_types::{Error, Request, Response, StatusCode};
 use tracing::debug;
@@ -5,31 +6,28 @@ use tracing::debug;
 pub async fn execute(req: Request) -> Result<Response, Error> {
     debug!("executing request: {:#?}", req);
 
+    if req.url().scheme() != "https" {
+        return Err(IoError::new(ErrorKind::InvalidInput, "Must use https").into());
+    }
+
     let host = req
         .url()
         .host_str()
         .ok_or_else(|| Error::from_str(StatusCode::BadRequest, "missing hostname"))?
         .to_string();
 
-    let is_https = req.url().scheme() == "https";
-
     let addr = req
         .url()
-        .socket_addrs(|| if is_https { Some(443) } else { Some(80) })?
+        .socket_addrs(|| Some(443))?
         .into_iter()
         .next()
         .ok_or_else(|| Error::from_str(StatusCode::BadRequest, "missing valid address"))?;
 
     let tcp_stream = fluvio_future::net::TcpStream::connect(addr).await?;
-
-    let result = if is_https {
-        let tls_connector = fluvio_future::tls::TlsConnector::default();
-        let tls_stream = tls_connector.connect(host, tcp_stream).await?;
-        client::connect(tls_stream, req).await
-    } else {
-        client::connect(tcp_stream, req).await
-    };
+    let tls_connector = fluvio_future::tls::TlsConnector::default();
+    let tls_stream = tls_connector.connect(host, tcp_stream).await?;
+    let result = client::connect(tls_stream, req).await?;
 
     debug!("http result: {:#?}", result);
-    result
+    Ok(result)
 }
