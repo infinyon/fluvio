86a6bbabb8e7eb961746d9c60724fd878b29985b
Sehyo <sehyo@fluvio.io>
2020-11-23 02:28:08 +0000

    clean up dependencies

--- src/extension-runner/src/error.rs
+++ src/extension-runner/src/error.rs
@@ -1,7 +1,5 @@
 use std::io::Error as IoError;
 
-use fluvio_extension_common::output::OutputError;
-
 pub type Result<T> = std::result::Result<T, RunnerError>;
 
 #[derive(thiserror::Error, Debug)]
@@ -11,12 +9,6 @@ pub enum RunnerError {
         #[from]
         source: IoError,
     },
-    #[error(transparent)]
-    OutputError {
-        #[from]
-        source: OutputError,
-    },
-
     #[error("Unknown error: {0}")]
     Other(String),
 }
