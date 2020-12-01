4144c3aa9f74d1a7808e476e85c1369e7addd413
simlay <simlay@users.noreply.github.com>
2020-11-30 16:24:35 -0800

    Added extensions to help section of fluvio (#528)

--- src/cli/src/bin/main.rs
+++ src/cli/src/bin/main.rs
@@ -1,6 +1,6 @@
 use structopt::StructOpt;
 use color_eyre::eyre::Result;
-use fluvio_cli::{Root, CliError};
+use fluvio_cli::{Root, CliError, HelpOpt};
 use fluvio_future::task::run_block_on;
 
 fn main() -> Result<()> {
@@ -8,8 +8,12 @@ fn main() -> Result<()> {
     color_eyre::config::HookBuilder::blank()
         .display_env_section(false)
         .install()?;
-
-    let root: Root = Root::from_args();
-    run_block_on(root.process()).map_err(CliError::into_report)?;
+    let args = std::env::args();
+    if args.len() < 2 {
+        HelpOpt {}.process()?;
+    } else {
+        let root: Root = Root::from_args();
+        run_block_on(root.process()).map_err(CliError::into_report)?;
+    }
     Ok(())
 }
