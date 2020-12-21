1c19ef35e0bb594abbd742da59e6d06daf953048
Nick Cardin <nick@cardin.email>
2020-12-21 22:54:32 +0000

    delete old files

--- k8-util/crd/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# initialize CRD
-# add CRD 
-DATA_DIR=$(dirname "$0")
-kubectl apply -f ${DATA_DIR}/crd_spu.yaml
-kubectl apply -f ${DATA_DIR}/crd_spg.yaml
-kubectl apply -f ${DATA_DIR}/crd_partition.yaml
-kubectl apply -f ${DATA_DIR}/crd_topic.yaml
--- k8-util/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mydir="$(dirname "${0}")"
-kubectl create clusterrolebinding cluster-system-anonymous --clusterrole=cluster-admin --user=system:anonymous
-# kubectl apply -f ${mydir}/crd/config/minikube-storageclass-spu.yaml
-# kubectl apply -f ${mydir}/crd/config/gp2-storageclass-spu.yaml
-${mydir}/crd/init.sh
