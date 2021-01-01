526569b227a629abe6295e32c0d5f47456e54525
Sehyo <sehyo@fluvio.io>
2021-01-01 04:59:55 +0000

    update reset-minikube

--- k8-util/minikube/reset-minikube.sh
+++ k8-util/minikube/reset-minikube.sh
@@ -3,7 +3,8 @@
 # this defaults to docker and assume you have have sudo access
 set -e
 ARG1=${1:-docker}
+sudo pkill -f "minikube tunnel"
 minikube delete
-minikube start --driver $ARG1
-sudo nohup  minikube tunnel  > /tmp/tunnel.out 2> /tmp/tunnel.out &
+minikube start --driver $ARG1 --kubernetes-version 1.19.6
+sudo nohup  minikube tunnel --alsologtostderr > /tmp/tunnel.out 2> /tmp/tunnel.out &
 fluvio cluster start --sys
