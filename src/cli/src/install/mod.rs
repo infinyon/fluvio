64548ea60d85ff09aa727cc2fa0c968b759617da
Nick Mosher <nicholastmosher@gmail.com>
2021-01-04 14:48:11 -0500

    Use fs::rename but create tmp dir adjacent to executable

--- src/cli/src/install/mod.rs
+++ src/cli/src/install/mod.rs
@@ -122,12 +122,12 @@ pub fn install_bin<P: AsRef<Path>, B: AsRef<[u8]>>(bin_path: P, bytes: B) -> Res
     let bin_path = bin_path.as_ref();
 
     // Create directories to bin_path if they do not exist
-    if let Some(parent) = bin_path.parent() {
-        std::fs::create_dir_all(&parent)?;
-    }
+    let parent = bin_path.parent()
+        .ok_or_else(|| IoError::new(ErrorKind::NotFound, "parent directory not found"))?;
+    std::fs::create_dir_all(&parent)?;
 
     // Write bin to temporary file
-    let tmp_dir = tempdir::TempDir::new("fluvio")?;
+    let tmp_dir = tempdir::TempDir::new_in(parent, "fluvio")?;
     let tmp_path = tmp_dir.path().join("fluvio");
     let mut tmp_file = File::create(&tmp_path)?;
     tmp_file.write_all(bytes.as_ref())?;
@@ -136,7 +136,7 @@ pub fn install_bin<P: AsRef<Path>, B: AsRef<[u8]>>(bin_path: P, bytes: B) -> Res
     make_executable(&mut tmp_file)?;
 
     // Rename (atomic move on unix) temp file to destination
-    std::fs::copy(&tmp_path, &bin_path)?;
+    std::fs::rename(&tmp_path, &bin_path)?;
 
     Ok(())
 }
