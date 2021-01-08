f011c08ed241cce350218d0e192e5b3939e9256c
Nick Mosher <nicholastmosher@gmail.com>
2021-01-08 10:08:14 -0500

    Treat files and stdin uniformly

--- src/extension-consumer/src/produce/mod.rs
+++ src/extension-consumer/src/produce/mod.rs
@@ -1,25 +1,11 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use structopt::StructOpt;
 
 use fluvio::{Fluvio, TopicProducer};
 use crate::common::FluvioExtensionMetadata;
 use crate::Result;
-
-/// Produce log configuration parameters
-#[derive(Debug)]
-pub struct ProduceLogConfig {
-    pub topic: String,
-    pub partition: i32,
-    pub continuous: bool,
-}
-
-#[derive(Debug)]
-pub enum FileRecord {
-    Lines(PathBuf),
-    Files(Vec<PathBuf>),
-}
 
 // -----------------------------------
 // CLI Options
@@ -40,61 +26,60 @@ pub struct ProduceLogOpt {
     )]
     pub partition: i32,
 
-    /// Send messages in an infinite loop
-    #[structopt(short = "C", long = "continuous")]
-    pub continuous: bool,
+    /// Whether to print each line of input as an individual record
+    #[structopt(short, long)]
+    pub lines: bool,
 
-    /// Send each line of the file as its own Record
-    #[structopt(
-        short = "l",
-        long = "record-per-line",
-        value_name = "filename",
-        parse(from_os_str)
-    )]
-    record_per_line: Option<PathBuf>,
-
-    /// Send an entire file as a single Record
-    #[structopt(
-        short = "r",
-        long = "record-file",
-        value_name = "filename",
-        parse(from_os_str),
-        conflicts_with = "record-per-line"
-    )]
-    record_file: Vec<PathBuf>,
+    /// One or more files to read records from
+    #[structopt(short, long)]
+    pub files: Vec<PathBuf>,
 }
 
 impl ProduceLogOpt {
     pub async fn process(self, fluvio: &Fluvio) -> Result<()> {
-        let (cfg, file_records) = self.validate()?;
-        let mut producer = fluvio.topic_producer(&cfg.topic).await?;
+        let mut producer = fluvio.topic_producer(&self.topic).await?;
 
-        if let Some(records) = file_records {
-            produce_from_files(&mut producer, cfg, records).await?;
+        if self.files.is_empty() {
+            self.produce_from_stdin(&mut producer).await?;
         } else {
-            produce_stdin(&mut producer, cfg).await?;
+            self.produce_from_files(&mut producer).await?;
         }
 
         Ok(())
     }
 
-    /// Validate cli options. Generate target-server and produce log configuration.
-    pub fn validate(self) -> Result<(ProduceLogConfig, Option<FileRecord>)> {
-        let file_records = if let Some(record_per_line) = self.record_per_line {
-            Some(FileRecord::Lines(record_per_line))
-        } else if !self.record_file.is_empty() {
-            Some(FileRecord::Files(self.record_file))
+    /// Sends each line of stdin as a record
+    async fn produce_from_stdin(&self, producer: &mut TopicProducer) -> Result<()> {
+        let mut stdin_lines = BufReader::new(std::io::stdin()).lines();
+        while let Some(Ok(line)) = stdin_lines.next() {
+            producer.send_record(&line, self.partition).await?;
+        }
+        Ok(())
+    }
+
+    /// Sends records to a Topic based on the file configuration given
+    async fn produce_from_files(&self, producer: &mut TopicProducer) -> Result<()> {
+        for path in &self.files {
+            self.produce_from_file(producer, path).await?;
+        }
+
+        Ok(())
+    }
+
+    /// Produces one or more records from a single file
+    async fn produce_from_file(&self, producer: &mut TopicProducer, path: &Path) -> Result<()> {
+        if self.lines {
+            let file = File::open(path)?;
+            let mut lines = BufReader::new(file).lines();
+            while let Some(Ok(line)) = lines.next() {
+                producer.send_record(&line, self.partition).await?;
+            }
         } else {
-            None
-        };
+            let bytes = std::fs::read(path)?;
+            producer.send_record(&bytes, self.partition).await?;
+        }
 
-        let produce_log_cfg = ProduceLogConfig {
-            topic: self.topic,
-            partition: self.partition,
-            continuous: self.continuous,
-        };
-
-        Ok((produce_log_cfg, file_records))
+        Ok(())
     }
 
     pub fn metadata() -> FluvioExtensionMetadata {
@@ -104,46 +89,4 @@ impl ProduceLogOpt {
             version: env!("CARGO_PKG_VERSION").into(),
         }
     }
-}
-
-/// Sends records to a Topic based on the file configuration given
-///
-/// This will either send the lines of a single file as individual records,
-/// or it will send the entirety of a list of files as records, where each
-/// whole file is one record.
-async fn produce_from_files(
-    producer: &mut TopicProducer,
-    cfg: ProduceLogConfig,
-    records: FileRecord,
-) -> Result<()> {
-
-    match records {
-        FileRecord::Files(paths) => {
-            for path in paths {
-                let bytes = std::fs::read(&path)?;
-                producer.send_record(&bytes, cfg.partition).await?;
-            }
-        },
-        FileRecord::Lines(path) => {
-            let file = File::open(&path)?;
-            let mut lines = BufReader::new(file).lines();
-            while let Some(Ok(line)) = lines.next() {
-                producer.send_record(&line, cfg.partition).await?;
-            }
-        }
-    }
-
-    Ok(())
-}
-
-/// Sends each line of stdin as a record
-async fn produce_stdin(
-    producer: &mut TopicProducer,
-    cfg: ProduceLogConfig,
-) -> Result<()> {
-    let mut stdin_lines = BufReader::new(std::io::stdin()).lines();
-    while let Some(Ok(line)) = stdin_lines.next() {
-        producer.send_record(&line, cfg.partition).await?;
-    }
-    Ok(())
 }
