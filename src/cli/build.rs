1a52f5caf8d7a0b78244476437ba79c3692fa9ff
Nick Mosher <nicholastmosher@gmail.com>
2021-01-29 15:37:21 -0500

    Add rebuild-if for VERSION

--- src/cli/build.rs
+++ src/cli/build.rs
@@ -3,6 +3,7 @@ use rustc_version::version_meta;
 
 fn main() {
     // Copy VERSION file. Do not fail e.g. when built via `cargo publish`
+    println!("cargo:rerun-if-changed=../../VERSION");
     let _ = std::fs::copy("../../VERSION", "./src/VERSION");
 
     // Fetch current git hash to print version output
