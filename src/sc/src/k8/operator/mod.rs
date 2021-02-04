e285fb073c101aa8f6e63861eea3c1a8bd208761
Sehyo Chang <sehyo@fluvio.io>
2021-02-03 17:03:49 -0800

    merge changes from 0.6.2 branch (#737)
    * helm: add scConfig to helm chart values
    sc: disable spu to sc sync if set
    
    * cargo fmt and clippy
    
    * fix loading of k8configmap
    
    * reduce timeout for mac examples
    
    * set smaller timeout for linux test

--- src/sc/src/k8/operator/mod.rs
+++ src/sc/src/k8/operator/mod.rs
@@ -3,32 +3,50 @@ mod conversion;
 mod spg_group;
 mod spu_k8_config;
 
-use k8_client::SharedK8Client;
+pub use spu_k8_config::ScK8Config;
+pub use k8_operator::run_k8_operators;
 
-use conversion::convert_cluster_to_statefulset;
-use conversion::generate_service;
-use spg_group::SpuGroupObj;
-use spg_group::SpuValidation;
-use spg_operator::SpgOperator;
-use spu_k8_config::SpuK8Config;
+mod k8_operator {
+    use k8_client::SharedK8Client;
 
-use crate::cli::TlsConfig;
-use crate::core::SharedContext;
-use crate::stores::StoreContext;
-use crate::dispatcher::dispatcher::K8ClusterStateDispatcher;
-use crate::k8::service::SpuServicespec;
-use crate::k8::service::SpuServiceController;
+    use tracing::error;
 
-pub fn run_k8_operators(
-    namespace: String,
-    k8_client: SharedK8Client,
-    ctx: SharedContext,
-    tls: Option<TlsConfig>,
-) {
-    SpgOperator::new(k8_client.clone(), namespace.clone(), ctx.clone(), tls).run();
+    use crate::cli::TlsConfig;
+    use crate::core::SharedContext;
+    use crate::stores::StoreContext;
+    use crate::dispatcher::dispatcher::K8ClusterStateDispatcher;
+    use crate::k8::service::SpuServicespec;
+    use crate::k8::service::SpuServiceController;
 
-    let svc_ctx: StoreContext<SpuServicespec> = StoreContext::new();
+    use super::{ScK8Config};
+    use super::spg_operator::SpgOperator;
 
-    K8ClusterStateDispatcher::<SpuServicespec, _>::start(namespace, k8_client, svc_ctx.clone());
-    SpuServiceController::start(ctx, svc_ctx);
+    pub async fn run_k8_operators(
+        namespace: String,
+        k8_client: SharedK8Client,
+        ctx: SharedContext,
+        tls: Option<TlsConfig>,
+    ) {
+        SpgOperator::new(k8_client.clone(), namespace.clone(), ctx.clone(), tls).run();
+
+        let svc_ctx: StoreContext<SpuServicespec> = StoreContext::new();
+
+        K8ClusterStateDispatcher::<SpuServicespec, _>::start(
+            namespace.clone(),
+            k8_client.clone(),
+            svc_ctx.clone(),
+        );
+
+        let disable_spu = match ScK8Config::load(&k8_client, &namespace).await {
+            Ok(config) => config.sc_config.disable_spu,
+            Err(err) => {
+                error!("error loading config: {:#?}", err);
+                false
+            }
+        };
+
+        if !disable_spu {
+            SpuServiceController::start(ctx, svc_ctx);
+        }
+    }
 }
