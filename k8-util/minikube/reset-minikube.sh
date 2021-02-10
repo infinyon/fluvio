16bc24d1a33d5adf7588cb49d921c097c2d25d29
Sehyo <sehyo@fluvio.io>
2021-02-10 05:25:22 +0000

    specify cpu=4 when creating minikube cluster

--- k8-util/minikube/reset-minikube.sh
+++ k8-util/minikube/reset-minikube.sh
@@ -5,6 +5,6 @@ set -e
 ARG1=${1:-docker}
 sudo pkill -f "minikube tunnel"
 minikube delete
-minikube start --driver $ARG1 --kubernetes-version 1.19.6
+minikube start --driver $ARG1 --kubernetes-version 1.19.6 --cpus 4
 sudo nohup  minikube tunnel --alsologtostderr > /tmp/tunnel.out 2> /tmp/tunnel.out &
 fluvio cluster start --sys
