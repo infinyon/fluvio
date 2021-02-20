39df7366de36069c1a07da0f349720a9747f7ce2
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-02-19 17:37:33 -0800

    removed build rs

--- src/sc/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // Copy VERSION file. Do not fail e.g. when built via `cargo publish`
-    println!("cargo:rerun-if-changed=../../VERSION");
-    let _ = std::fs::copy("../../VERSION", "./src/VERSION");
-}
