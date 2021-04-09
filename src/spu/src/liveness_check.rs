f75f8964e4bf4b5868aa884bebaf28e2890cf26c
Nick Cardin <nick@cardin.email>
2021-04-09 15:35:27 +0000

    fix clippy warnings

--- src/spu/src/liveness_check.rs
+++ src/spu/src/liveness_check.rs
@@ -13,9 +13,9 @@ use dataplane::versions::ApiVersionsRequest;
 pub fn probe(endpoint: SocketAddr) -> Result<()> {
     let mut stream = TcpStream::connect(endpoint)?;
     let req_msg = RequestMessage::new_request(ApiVersionsRequest::default());
-    let mut response_bytes = [0u8; 512];
-    stream.write(&req_msg.as_bytes(0)?)?;
-    let _len = stream.read(&mut response_bytes)?;
+    let mut response_bytes = Vec::new();
+    stream.write_all(&req_msg.as_bytes(0)?)?;
+    let _len = stream.read_to_end(&mut response_bytes)?;
     let mut cursor = Cursor::new(response_bytes);
     cursor.set_position(4);
     let response = req_msg.decode_response(&mut cursor, req_msg.header.api_version())?;
