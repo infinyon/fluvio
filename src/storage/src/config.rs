f43b7bf0542b034362a2a459153cf0377d8ef84b
Luis Moreno <morenol@users.noreply.github.com>
2021-05-11 15:15:06 -0400

    feat: use builder pattern in fluvio_storage::config::ConfigOption

--- src/storage/src/config.rs
+++ src/storage/src/config.rs
@@ -2,6 +2,7 @@ use std::default::Default;
 use std::path::PathBuf;
 use std::fmt;
 
+use derive_builder::Builder;
 use serde::Deserialize;
 
 use fluvio_types::defaults::{SPU_LOG_INDEX_MAX_BYTES, SPU_LOG_BASE_DIR};
@@ -16,22 +17,30 @@ pub const DEFAULT_FLUSH_IDLE_MSEC: u32 = 0;
 pub const DEFAULT_MAX_BATCH_SIZE: u32 = 1048588;
 
 // common option
-#[derive(Debug, Clone, PartialEq, Deserialize)]
+#[derive(Builder, Debug, Clone, PartialEq, Deserialize)]
 pub struct ConfigOption {
+    #[builder(default = "default_base_dir()")]
     #[serde(default = "default_base_dir")]
     pub base_dir: PathBuf,
+    #[builder(default = "default_index_max_bytes()")]
     #[serde(default = "default_index_max_bytes")]
     pub index_max_bytes: Size,
+    #[builder(default = "default_index_max_interval_bytes()")]
     #[serde(default = "default_index_max_interval_bytes")]
     pub index_max_interval_bytes: Size,
+    #[builder(default = "default_segment_max_bytes()")]
     #[serde(default = "default_segment_max_bytes")]
     pub segment_max_bytes: Size,
+    #[builder(default = "default_flush_write_count()")]
     #[serde(default = "default_flush_write_count")]
     pub flush_write_count: Size,
+    #[builder(default = "default_flush_idle_msec()")]
     #[serde(default = "default_flush_idle_msec")]
     pub flush_idle_msec: Size,
+    #[builder(default = "default_max_batch_size()")]
     #[serde(default = "default_max_batch_size")]
     pub max_batch_size: Size,
+    #[builder(default = "default_update_hw()")]
     #[serde(default = "default_update_hw")]
     pub update_hw: bool, // if true, enable hw update
 }
