a89133ab65b6cb383deb8fc62432e12d34a6878e
Nick Mosher <nicholastmosher@gmail.com>
2021-07-07 17:19:43 -0400

    Start on upload-fluvio action

---
+++ .github/actions/upload-fluvio/main.js
@@ -0,0 +1,45 @@
+const core = require("@actions/core");
+
+const runOnce = async () => {
+  // Load input and environment variables
+  const artifacts = core.getInput('artifacts', { required: true });
+  const sha = process.env.GITHUB_SHA;
+  const ref = process.env.GITHUB_REF;
+
+  core.info(`artifacts: ${artifacts}`);
+  core.info(`sha: ${sha}`);
+  core.info(`ref: ${ref}`);
+};
+
+const run = async () => {
+  const retries = 10;
+  for (let i = 0; i < retries; i++) {
+    try {
+      await runOnce();
+      break;
+    } catch (e) {
+      if (i === retries - 1)
+        throw e;
+      logError(e);
+      console.log("RETRYING after 10s");
+      await sleep(10000)
+    }
+  }
+}
+
+const sleep = async (millis) => new Promise(resolve => setTimeout(resolve, millis));
+
+const logError = (e) => {
+  console.log("ERROR: ", e.message);
+  try {
+    console.log(JSON.stringify(e, null, 2));
+  } catch (e) {
+    // We tried
+  }
+  console.log(e.stack);
+}
+
+run().catch(err => {
+  logError(err);
+  core.setFailed(err.message);
+})
