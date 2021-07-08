7845472cd95b578b029e229787133ecbff3cb14e
Nick Mosher <nicholastmosher@gmail.com>
2021-07-08 11:49:45 -0400

    Fix root upload dir

--- .github/actions/upload-fluvio/main.js
+++ .github/actions/upload-fluvio/main.js
@@ -8,39 +8,39 @@ const runOnce = async () => {
 
   // Load input and environment variables
   const artifactInput = core.getInput('artifact', { required: true }).trim();
+  const artifactNames = artifactInput.split("\n");
   const target = core.getInput('target').trim();
   const sha = process.env.GITHUB_SHA;
   const ref = process.env.GITHUB_REF;
-  const repoDir = path.normalize(`${__dirname}/../../..`);
+  const repoPath = path.normalize(`${__dirname}/../../..`);
   const releaseMode = github.context.eventName === "push" && ref === "refs/heads/staging";
 
   // Assemble path into `target/` directory for each artifact based on target and release
   const buildPrefix = (!!target) ? `target/${target}` : "target";
   const buildRelease = (releaseMode) ? "release" : "debug";
-  const artifactNames = artifactInput.split("\n");
-  const artifactPaths = artifactNames.map(artifact => `${buildPrefix}/${buildRelease}/${artifact}`);
+  const targetPath = `${buildPrefix}/${buildRelease}`;
 
   core.info(`artifact: ${artifact}`);
   core.info(`artifacts: ${JSON.stringify(artifactNames)}`);
-  core.info(`paths: ${JSON.stringify(artifactPaths)}`);
   core.info(`target: ${target}`);
+  core.info(`targetPath: ${targetPath}`);
   core.info(`sha: ${sha}`);
   core.info(`ref: ${ref}`);
   core.info(`release: ${releaseMode}`);
   core.info(`buildPrefix: ${buildPrefix}`);
   core.info(`buildRelease: ${buildRelease}`);
   core.info(`__dirname: ${__dirname}`);
-  core.info(`repoDir: ${repoDir}`);
+  core.info(`repoPath: ${repoPath}`);
 
   for (let i = 0; i < artifactNames.length; i++) {
     const artifactName = artifactNames[i];
-    const artifactPath = artifactPaths[i];
-
     // E.g. fluvio-x86_64-unknown-linux-musl
     const artifactKey = `${artifactName}-${target}`;
 
+    // E.g. /home/user/fluvio/fluvio/target/x86_64-unknown-linux-musl/release
+    const artifactPath = path.resolve(repoPath, targetPath);
     const options = { continueOnError: false };
-    await artifactClient.uploadArtifact(artifactKey, [artifactPath], repoDir, options);
+    await artifactClient.uploadArtifact(artifactKey, [artifactName], artifactPath, options);
     core.info(`Uploaded ${artifactKey} from ${artifactPath}`);
   }
 };
