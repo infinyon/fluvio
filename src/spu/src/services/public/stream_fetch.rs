d463cc557ddb9ad03d8a4187b7a5c21c80817258
Sehyo <sehyo@fluvio.io>
2021-07-12 01:15:44 +0000

    fix wasm

--- src/spu/src/services/public/stream_fetch.rs
+++ src/spu/src/services/public/stream_fetch.rs
@@ -663,7 +663,7 @@ mod test {
             .parent()
             .expect("parent")
             .join(format!(
-                "smartstream/examples/target/wasm32-unknown-unknown/release/{}.wasm",
+                "smartstream/examples/target/wasm32-unknown-unknown/debug/{}.wasm",
                 module_name
             ));
         read_filter_from_path(wasm_path)
