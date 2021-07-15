c00c1e3b9196d7ad5dd195f340af741641c75e22
Nick Mosher <nicholastmosher@gmail.com>
2021-07-15 17:44:09 -0400

    test removing

--- actions/action-install-fluvio-cluster.sh
+++ actions/action-install-fluvio-cluster.sh
@@ -17,20 +17,6 @@ CHART_VERSION="${REPO_VERSION}-${GITHUB_SHA}"
 # Install Fluvio System Charts
 #
 
-# If VERSION is equal to exactly "latest", use LATEST channel
-if [ "${VERSION}" == "latest" ]; then
-    fluvio cluster start --setup --local --sys --chart-version="${REPO_VERSION}-c963500f9d985a1a42b67380bf7bb683cdace1d7"
-#    fluvio cluster start --setup --local --sys --chart-version="${CHART_VERSION}"
-else
-    fluvio cluster start --setup --local --sys
-fi
-
-#
-# Run Fluvio Cluster Pre-Install Check
-#
-
-fluvio cluster check
-
 # Install Local Fluvio Cluster
 if [ "$CLUSTER_TYPE" = "local" ]; then
 
