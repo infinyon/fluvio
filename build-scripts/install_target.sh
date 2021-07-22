7eb1a645cccc490636fa9f74ea375ba6e1421ed9
Sehyo <sehyo@fluvio.io>
2021-07-22 00:00:59 +0000

    fix installing cross

--- build-scripts/install_target.sh
+++ build-scripts/install_target.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+if [ "$TARGET" = "armv7-unknown-linux-gnueabihf" ]; then
+    cargo install cross
+fi
+
 if [ -n "$TARGET" ]; then
     echo "loading target $TARGET"
     rustup target add $TARGET
