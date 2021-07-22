56e20e8b35f59f5f15191f353aecaa73a57d7890
simlay <sebastian.imlay@gmail.com>
2021-07-22 00:47:09 +0000

    Revert to native tls for cli (#1281)
    I've tested these changes over in #1218 and it works there as well.
    
    Should resolve #1273.

--- src/cli/src/http.rs
+++ src/cli/src/http.rs
@@ -2,13 +2,12 @@ use std::io::{ErrorKind, Error as IoError};
 use async_h1::client;
 use http_types::{Error, Request, Response, StatusCode};
 use tracing::{debug, error, instrument};
-use crate::error::CliError;
 
 #[instrument(
     skip(request),
     fields(url = %request.url())
 )]
-pub async fn execute(request: Request) -> Result<Response, CliError> {
+pub async fn execute(request: Request) -> Result<Response, Error> {
     debug!(?request, "Executing http request:");
 
     if request.url().scheme() != "https" {
@@ -26,17 +25,25 @@ pub async fn execute(request: Request) -> Result<Response, CliError> {
     let addr: (&str, u16) = (&host, request.url().port_or_known_default().unwrap_or(443));
     let tcp_stream = fluvio_future::net::TcpStream::connect(addr).await?;
     debug!("Established TCP stream");
-    let tls_connector = create_tls().await?;
+    let tls_connector = create_tls().await;
     debug!("Created TLS connector");
-    let tls_stream = tls_connector.connect(&host, tcp_stream).await?;
+    let tls_stream = tls_connector.connect(host, tcp_stream).await?;
     debug!("Opened TLS stream from TCP stream");
     let response = client::connect(tls_stream, request).await?;
 
     debug!(?response, "Http response:");
     Ok(response)
 }
+async fn create_tls() -> fluvio_future::native_tls::TlsConnector {
+    fluvio_future::native_tls::TlsConnector::default()
+}
 
-async fn create_tls(
-) -> Result<fluvio_future::openssl::TlsConnector, fluvio_future::openssl::TlsError> {
-    Ok(fluvio_future::openssl::TlsConnector::builder()?.build())
+#[fluvio_future::test]
+async fn test_web_request() {
+    use fluvio_index::HttpAgent;
+    use http_types::StatusCode;
+    let agent = HttpAgent::default();
+    let index = agent.request_index().expect("Failed to get request index");
+    let response = execute(index).await.expect("Failed to execute request");
+    assert_eq!(response.status(), StatusCode::Ok);
 }
