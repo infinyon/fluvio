231279ab456e82ed8649b28871039e5f962719ac
Nick Mosher <nicholastmosher@gmail.com>
2021-07-27 14:36:52 -0400

    Invert cross check

--- build-scripts/install_target.sh
+++ build-scripts/install_target.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$TARGET" = "armv7-unknown-linux-gnueabihf" ] || [ "$TARGET" = "arm-unknown-linux-gnueabihf" ]; then
     # Install cross if not installed
-    [[ -x "$(command -v cross)" ]] && cargo install cross
+    [[ -x "$(command -v cross)" ]] || cargo install cross
 fi
 
 if [ -n "$TARGET" ]; then
