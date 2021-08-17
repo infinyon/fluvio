916ae41ebce848aa438a5bda6d49d599f99b287c
Nick Mosher <nicholastmosher@gmail.com>
2021-08-17 11:41:50 -0400

    Clean up derive by using imports

--- src/smartstream/derive/src/generator/aggregate.rs
+++ src/smartstream/derive/src/generator/aggregate.rs
@@ -13,44 +13,51 @@ pub fn generate_aggregate_smartstream(func: &SmartStreamFn) -> TokenStream {
             #[no_mangle]
             #[allow(clippy::missing_safety_doc)]
             pub unsafe fn aggregate(ptr: &mut u8, len: usize) -> i32 {
+                use fluvio_smartstream::dataplane::smartstream::{
+                    SmartStreamAggregateInput, SmartStreamInternalError,
+                    SmartStreamRuntimeError, SmartStreamType, SmartStreamOutput,
+                };
+                use fluvio_smartstream::dataplane::core::{Encoder, Decoder};
+                use fluvio_smartstream::dataplane::record::{Record, RecordData};
+
                 extern "C" {
                     fn copy_records(putr: i32, len: i32);
                 }
 
                 let input_data = Vec::from_raw_parts(ptr, len, len);
-                let mut smartstream_input = fluvio_smartstream::dataplane::smartstream::SmartStreamAggregateInput::default();
-                if let Err(_err) = fluvio_smartstream::dataplane::core::Decoder::decode(&mut smartstream_input, &mut std::io::Cursor::new(input_data), 0) {
-                    return fluvio_smartstream::dataplane::smartstream::SmartStreamInternalError::DecodingBaseInput as i32;
+                let mut smartstream_input = SmartStreamAggregateInput::default();
+                if let Err(_err) = Decoder::decode(&mut smartstream_input, &mut std::io::Cursor::new(input_data), 0) {
+                    return SmartStreamInternalError::DecodingBaseInput as i32;
                 }
 
                 let mut accumulator = smartstream_input.accumulator;
                 let records_input = smartstream_input.base.record_data;
-                let mut records: Vec<fluvio_smartstream::dataplane::record::Record> = vec![];
-                if let Err(_err) = fluvio_smartstream::dataplane::core::Decoder::decode(&mut records, &mut std::io::Cursor::new(records_input), 0) {
-                    return fluvio_smartstream::dataplane::smartstream::SmartStreamInternalError::DecodingRecords as i32;
+                let mut records: Vec<Record> = vec![];
+                if let Err(_err) = Decoder::decode(&mut records, &mut std::io::Cursor::new(records_input), 0) {
+                    return SmartStreamInternalError::DecodingRecords as i32;
                 };
 
                 // PROCESSING
-                let mut output = fluvio_smartstream::dataplane::smartstream::SmartStreamOutput {
+                let mut output = SmartStreamOutput {
                     successes: Vec::with_capacity(records.len()),
                     error: None,
                 };
 
                 for mut record in records.into_iter() {
-                    let acc_data = fluvio_smartstream::dataplane::record::RecordData::from(accumulator);
+                    let acc_data = RecordData::from(accumulator);
                     let result = super:: #user_fn(acc_data, &record);
 
                     match result {
                         Ok(value) => {
                             accumulator = Vec::from(value.as_ref());
-                            record.value = fluvio_smartstream::dataplane::record::RecordData::from(accumulator.clone());
+                            record.value = RecordData::from(accumulator.clone());
                             output.successes.push(record);
                         }
                         Err(err) => {
-                            let error = fluvio_smartstream::dataplane::smartstream::SmartStreamRuntimeError::new(
+                            let error = SmartStreamRuntimeError::new(
                                 &record,
                                 smartstream_input.base.base_offset,
-                                fluvio_smartstream::dataplane::smartstream::SmartStreamType::Aggregate,
+                                SmartStreamType::Aggregate,
                                 err,
                             );
                             output.error = Some(error);
@@ -61,8 +68,8 @@ pub fn generate_aggregate_smartstream(func: &SmartStreamFn) -> TokenStream {
 
                 // ENCODING
                 let mut out = vec![];
-                if let Err(_) = fluvio_smartstream::dataplane::core::Encoder::encode(&mut output, &mut out, 0) {
-                    return fluvio_smartstream::dataplane::smartstream::SmartStreamInternalError::EncodingOutput as i32;
+                if let Err(_) = Encoder::encode(&mut output, &mut out, 0) {
+                    return SmartStreamInternalError::EncodingOutput as i32;
                 }
 
                 let out_len = out.len();
