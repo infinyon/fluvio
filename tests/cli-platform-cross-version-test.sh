3a669b740d73ddcc7530a633b211b374a2b7dffb
T.J. Telan <t.telan@gmail.com>
2021-08-20 21:25:15 +0000

    Slow down test for github runner env (#1482)
    The test locks up before doing anything useful bc of cpu.
    Copying what we do for the upgrade test.
    
    Adding retry, just in case of env-related failure

--- tests/cli-platform-cross-version-test.sh
+++ tests/cli-platform-cross-version-test.sh
@@ -7,6 +7,19 @@ readonly CLUSTER_VERSION=${2-latest}
 readonly TEST_TOPIC=$CLI_VERSION-x-$CLUSTER_VERSION
 readonly FLUVIO_BIN=~/.fluvio/bin/fluvio
 readonly PAYLOAD_SIZE=${PAYLOAD_SIZE:-100}
+readonly CI_SLEEP=${CI_SLEEP:-5}
+readonly CI=${CI:-}
+
+# If we're in CI, we want to slow down execution
+# to give CPU some time to rest, so we don't time out
+function ci_check() {
+    if [[ ! -z "$CI" ]];
+    then
+        echo "[CI MODE] Pausing for ${CI_SLEEP} second(s)";
+        w | head -1
+        sleep ${CI_SLEEP};
+    fi
+}
 
 function setup_cluster() {
     echo "Installing cluster @ VERSION: $CLUSTER_VERSION"
@@ -28,10 +41,16 @@ function setup_cli() {
 
 function run_test() {
     local RANDOM_DATA=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w${PAYLOAD_SIZE} | head -n1)
+    ci_check;
 
     $FLUVIO_BIN topic create $TEST_TOPIC
+    ci_check;
+
     echo $RANDOM_DATA | $FLUVIO_BIN produce $TEST_TOPIC
+    ci_check;
+
     $FLUVIO_BIN consume $TEST_TOPIC -B -d
+    ci_check;
     # TODO: Verify the test output matches
 }
 
@@ -46,10 +65,14 @@ function main() {
     if [[ -z "$CI" ]];
     then
         cleanup;
+    else
+        echo "[CI MODE] Skipping initial cleanup";
     fi
 
     setup_cluster $CLUSTER_VERSION;
+    ci_check;
     setup_cli $CLI_VERSION;
+    ci_check;
     run_test;
     cleanup;
 }
