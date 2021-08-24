22c50323f6443a6779a59b87e6de804d79df3bd0
T.J. Telan <t.telan@gmail.com>
2021-08-23 20:18:11 -0700

    Updating test timer
    For throughput measurement in test driver

--- tests/runner/src/utils/test_meta/test_timer.rs
+++ tests/runner/src/utils/test_meta/test_timer.rs
@@ -3,23 +3,52 @@ use std::fmt::Debug;
 
 #[derive(Debug, Clone)]
 pub struct TestTimer {
-    pub start_time: Instant,
+    pub start_time: Option<Instant>,
     pub duration: Option<Duration>,
 }
 
+impl Default for TestTimer {
+    fn default() -> Self {
+        TestTimer {
+            start_time: None,
+            duration: None,
+        }
+    }
+}
+
 impl TestTimer {
+    pub fn new() -> Self {
+        TestTimer::default()
+    }
+
     pub fn start() -> Self {
         TestTimer {
-            start_time: Instant::now(),
+            start_time: Some(Instant::now()),
             duration: None,
         }
     }
 
+    pub fn is_running(&self) -> bool {
+        self.start_time.is_some()
+    }
+
     pub fn stop(&mut self) {
-        self.duration = Some(self.start_time.elapsed());
+        if let Some(time) = self.start_time {
+            self.duration = Some(time.elapsed());
+        } else {
+            self.duration = None;
+        }
     }
 
     pub fn duration(&self) -> Duration {
         self.duration.expect("Timer is still running")
+    }
+
+    pub fn elapsed(&self) -> Duration {
+        if let Some(time) = self.start_time {
+            time.elapsed()
+        } else {
+            self.duration.expect("Timer hasn't been started")
+        }
     }
 }
