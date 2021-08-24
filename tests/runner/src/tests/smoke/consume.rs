ccdf8361d605c1a1e48f79d228556f8873b77dad
Sehyo <sehyo@fluvio.io>
2021-08-24 00:33:00 +0000

    update

--- tests/runner/src/tests/smoke/consume.rs
+++ tests/runner/src/tests/smoke/consume.rs
@@ -110,8 +110,9 @@ async fn validate_consume_message_api(
             let now = SystemTime::now();
             select! {
 
-                // max time for each read
-                _ = sleep(Duration::from_secs(30)) => {
+                
+                // max time for each read of 5 miniutes
+                _ = sleep(Duration::from_secs(60*5)) => {
                     println!("Timeout in read");
                     panic!("no consumer read iter: current {}",producer_iteration);
                 },
