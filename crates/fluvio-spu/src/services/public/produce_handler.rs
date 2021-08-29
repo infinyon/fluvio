5d6b406839e8adae8d025a84a436c7bddd04ce96
Nick Mosher <nicholastmosher@gmail.com>
2021-08-29 10:22:03 -0400

    Clean up produce handler

--- crates/fluvio-spu/src/services/public/produce_handler.rs
+++ crates/fluvio-spu/src/services/public/produce_handler.rs
@@ -7,9 +7,11 @@ use tracing::instrument;
 use dataplane::ErrorCode;
 use dataplane::produce::{
     DefaultProduceRequest, ProduceResponse, TopicProduceResponse, PartitionProduceResponse,
+    PartitionProduceData, TopicProduceData,
 };
 use dataplane::api::RequestMessage;
 use dataplane::api::ResponseMessage;
+use dataplane::record::RecordSet;
 use fluvio_controlplane_metadata::partition::ReplicaKey;
 
 use crate::core::DefaultSharedGlobalContext;
@@ -26,63 +28,86 @@ pub async fn handle_produce_request(
     ctx: DefaultSharedGlobalContext,
 ) -> Result<ResponseMessage<ProduceResponse>, Error> {
     let (header, produce_request) = request.get_header_request();
-    trace!("handling produce request: {:#?}", produce_request);
+    trace!("Handling ProduceRequest: {:#?}", produce_request);
 
     let mut response = ProduceResponse::default();
-
-    //let ack = produce_request.acks;
-
     for topic_request in produce_request.topics.into_iter() {
-        let topic = &topic_request.name;
-        trace!("handling produce request for topic{}", topic);
-
-        let mut topic_response = TopicProduceResponse {
-            name: topic.to_owned(),
-            ..Default::default()
-        };
-
-        for mut partition_request in topic_request.partitions.into_iter() {
-            let rep_id = ReplicaKey::new(topic.clone(), partition_request.partition_index);
-
-            trace!("handling produce request for replia: {}", rep_id);
-
-            let mut partition_response = PartitionProduceResponse {
-                partition_index: rep_id.partition,
-                ..Default::default()
-            };
-
-            if let Some(leader_state) = ctx.leaders_state().get(&rep_id) {
-                match leader_state
-                    .write_record_set(&mut partition_request.records, ctx.follower_notifier())
-                    .await
-                {
-                    Ok(_) => {
-                        partition_response.error_code = ErrorCode::None;
-                    }
-                    Err(err) => {
-                        error!("error: {:#?} writing to replica: {}", err, rep_id);
-                        match err {
-                            StorageError::BatchTooBig(_) => {
-                                partition_response.error_code = ErrorCode::MessageTooLarge
-                            }
-                            _ => {
-                                partition_response.error_code = ErrorCode::StorageError;
-                            }
-                        }
-                    }
-                }
-            } else {
-                debug!(%rep_id,"no replica found");
-                partition_response.error_code = ErrorCode::NotLeaderForPartition;
-            }
-
-            topic_response.partitions.push(partition_response);
-        }
-
+        let topic_response = handle_produce_topic(&ctx, topic_request).await?;
         response.responses.push(topic_response);
     }
 
-    trace!("produce request completed");
-
+    trace!("Returning ProduceResponse: {:#?}", &response);
     Ok(RequestMessage::<DefaultProduceRequest>::response_with_header(&header, response))
+}
+
+#[instrument(
+    skip(ctx, topic_request),
+    fields(topic = %topic_request.name),
+)]
+async fn handle_produce_topic(
+    ctx: &DefaultSharedGlobalContext,
+    topic_request: TopicProduceData<RecordSet>,
+) -> Result<TopicProduceResponse, Error> {
+    trace!("Handling produce request for topic:");
+    let topic = &topic_request.name;
+
+    let mut topic_response = TopicProduceResponse {
+        name: topic.to_owned(),
+        ..Default::default()
+    };
+
+    for partition_request in topic_request.partitions.into_iter() {
+        let replica_id = ReplicaKey::new(topic.to_string(), partition_request.partition_index);
+        let partition_response =
+            handle_produce_partition(&ctx, replica_id, partition_request).await?;
+        topic_response.partitions.push(partition_response);
+    }
+
+    Ok(topic_response)
+}
+
+#[instrument(
+    skip(ctx, replica_id, partition_request),
+    fields(%replica_id),
+)]
+async fn handle_produce_partition(
+    ctx: &DefaultSharedGlobalContext,
+    replica_id: ReplicaKey,
+    mut partition_request: PartitionProduceData<RecordSet>,
+) -> Result<PartitionProduceResponse, Error> {
+    trace!("Handling produce request for partition:");
+
+    let mut partition_response = PartitionProduceResponse {
+        partition_index: replica_id.partition,
+        ..Default::default()
+    };
+
+    let leader_state = match ctx.leaders_state().get(&replica_id) {
+        Some(leader_state) => leader_state,
+        None => {
+            debug!(%replica_id, "Replica not found");
+            partition_response.error_code = ErrorCode::NotLeaderForPartition;
+            return Ok(partition_response);
+        }
+    };
+
+    let write_result = leader_state
+        .write_record_set(&mut partition_request.records, ctx.follower_notifier())
+        .await;
+
+    match write_result {
+        Ok(_) => {
+            partition_response.error_code = ErrorCode::None;
+        }
+        Err(err @ StorageError::BatchTooBig(_)) => {
+            error!(%replica_id, "Batch is too big: {:#?}", err);
+            partition_response.error_code = ErrorCode::MessageTooLarge
+        }
+        Err(err) => {
+            error!(%replica_id, "Error writing to replica: {:#?}", err);
+            partition_response.error_code = ErrorCode::StorageError;
+        }
+    }
+
+    Ok(partition_response)
 }
