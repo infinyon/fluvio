ea5fa82dd069b17422f023707f0a87371c1a043d
T.J. Telan <t.telan@gmail.com>
2021-09-02 14:37:26 -0700

    expose payload size and producer rate for override

--- tests/longevity-producer.sh
+++ tests/longevity-producer.sh
@@ -16,10 +16,10 @@ readonly TEN_MIN_IN_SECONDS=600
 
 # This var controls the expected test duration
 readonly TOTAL_TEST_TIME=${TOTAL_TEST_TIME:-$TEN_MIN_IN_SECONDS}
-readonly PAYLOAD_SIZE=1000
+readonly PAYLOAD_SIZE=${PAYLOAD_SIZE:-1000}
 readonly NEW_TOPIC_NAME=longevity-new
 readonly EXISTING_TOPIC_NAME=longevity-existing
-readonly PRODUCER_RATE=20 # 20 msg/s => 72K msg/hour
+readonly PRODUCER_RATE=${PRODUCER_RATE:-20} # 20 msg/s => 72K msg/hour
 
 # On Mac, use 'greadlink' instead of 'readlink'
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -50,7 +50,7 @@ function setup() {
 
 
     # Start a cluster
-    $FLUVIO_BIN cluster start --image-version latest
+    #$FLUVIO_BIN cluster start --image-version latest --local
 
     # Create a topic to delete at the end
     #$FLUVIO_BIN topic create $NEW_TOPIC_NAME || true
