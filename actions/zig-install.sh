556c5623929e987e5de24b523ab2cbed3e0c5762
Sehyo <sehyo@fluvio.io>
2021-09-07 15:00:24 +0000

    don't perform brew update

--- actions/zig-install.sh
+++ actions/zig-install.sh
@@ -13,7 +13,7 @@ fi
 
 if [[ "$MATRIX_OS" == "macos-latest" ]]; then
     echo "installing zig on mac"
-    brew update
+ #   brew update
     brew install zig && \
     brew install llvm@11 && \
     echo "FLUVIO_BUILD_LLD=/usr/local/opt/llvm@11/bin/lld" | tee -a $GITHUB_ENV
