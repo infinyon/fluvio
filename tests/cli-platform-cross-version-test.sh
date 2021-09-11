infinyon/fluvio

commit 0459c0518c21d0f49b70ee2ee9c492b7bced61ac

Test CLI backwards compatibility in CI (#1607)
Test the latest CLI against the current stable version of cluster in CI

Modified `cli-platform-cross-version-test` from CD_Dev so it can run in CI w/o setting up a cluster w/o setup, bc we do that outside the script.

.github/workflows/ci.yml
@@ -615,6 +615,41 @@ jobs:
           name: k8_${{ matrix.run }}_log
           path: /tmp/k8_*.log
 
+  k8_cli_backward_compat:
+    name: Latest CLI vs Stable Cluster
+    needs: build_binaries
+    if: false
+    runs-on: ${{ matrix.os }}
+    strategy:
+      fail-fast: false
+      matrix:
+        os: [ubuntu-latest]
+        cluster_version: [stable]
+        cli_version: [latest]
+    steps:
+      - uses: actions/checkout@v2
+      - name: Install K3d
+        run: |
+          curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+          ./k8-util/cluster/reset-k3d.sh
+      - name: Install stable CLI and start Fluvio cluster
+        run: |
+          curl -fsS https://packages.fluvio.io/v1/install.sh | bash
+          ~/.fluvio/bin/fluvio cluster start
+
+      - name: Download artifact - fluvio
+        uses: actions/download-artifact@v2
+        with:
+          name: fluvio-x86_64-unknown-linux-musl
+          path: .
+
+      - name: Print artifacts and mark executable
+        run: ls -la . && chmod +x ./fluvio && ./fluvio version
+
+      - name: Test ${{ matrix.cli_version }} CLI vs ${{ matrix.cluster_version }} cluster
+        run: |
+          make SKIP_SETUP=true CLI_VERSION=${{ matrix.cli_version }} CLUSTER_VERSION=${{ matrix.cluster_version }} FLUVIO_BIN=./fluvio cli-platform-cross-version-test
+
   # Ensure all checks, tests are perform and all binaries are built
   # After this, we are committed for release
   docker_push:

tests/cli-platform-cross-version-test.sh
@@ -5,10 +5,12 @@ set -e
 readonly CLI_VERSION=${1-stable}
 readonly CLUSTER_VERSION=${2-latest}
 readonly TEST_TOPIC=$CLI_VERSION-x-$CLUSTER_VERSION
-readonly FLUVIO_BIN=~/.fluvio/bin/fluvio
+readonly FLUVIO_BIN=${FLUVIO_BIN:-~/.fluvio/bin/fluvio}
 readonly PAYLOAD_SIZE=${PAYLOAD_SIZE:-100}
 readonly CI_SLEEP=${CI_SLEEP:-5}
 readonly CI=${CI:-}
+readonly SKIP_SETUP=${SKIP_SETUP:-}
+readonly SKIP_CLEANUP=${SKIP_CLEANUP:-}
 
 # If we're in CI, we want to slow down execution
 # to give CPU some time to rest, so we don't time out
@@ -35,13 +37,13 @@ function setup_cli() {
 }
 
 function run_test() {
-    local RANDOM_DATA=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w${PAYLOAD_SIZE} | head -n1)
+    local TEST_DATA=$(shuf -zer -n${PAYLOAD_SIZE}  {A..Z} {a..z} {0..9} | tr -d '\0')
     ci_check;
 
-    $FLUVIO_BIN topic create $TEST_TOPIC
+    $FLUVIO_BIN topic create $TEST_TOPIC || true
     ci_check;
 
-    echo $RANDOM_DATA | $FLUVIO_BIN produce $TEST_TOPIC
+    echo $TEST_DATA | $FLUVIO_BIN produce $TEST_TOPIC
     ci_check;
 
     $FLUVIO_BIN consume $TEST_TOPIC -B -d
@@ -64,12 +66,25 @@ function main() {
         echo "[CI MODE] Skipping initial cleanup";
     fi
 
-    setup_cluster $CLUSTER_VERSION;
-    ci_check;
-    setup_cli $CLI_VERSION;
-    ci_check;
+    if [[ -z "$SKIP_SETUP" ]];
+    then
+        setup_cluster $CLUSTER_VERSION;
+        ci_check;
+        setup_cli $CLI_VERSION;
+        ci_check;
+    else
+        echo "Skipping setup"
+    fi
+
     run_test;
-    cleanup;
+
+    if [[ -z "$SKIP_CLEANUP" ]];
+    then
+        cleanup;
+    else
+        echo "Skipping cleanup"
+    fi
+
 }
 
 main;

commit 10733661e02f618bd000e954a1838815c1dd2595

Testing fix for cd_dev
Broken by #1607

.github/workflows/ci.yml
@@ -790,3 +790,25 @@ jobs:
     steps:
       - name: Done
         run: echo "Done!"
+
+  cli-platform-cross-version-test:
+    name: CLI (${{ matrix.cli_version }}) x Platform (${{ matrix.cluster_version }}) version test 
+    #if: false
+    runs-on: ${{ matrix.os }}
+    strategy:
+      fail-fast: false
+      matrix:
+        os: [ubuntu-latest]
+        cluster_version: [stable, latest]
+        cli_version: [stable, latest]
+    steps:
+      - uses: actions/checkout@v2
+      - name: Setup K3d
+        run: curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+      - name: Create K3d cluster
+        run: |
+          ./k8-util/cluster/reset-k3d.sh
+      - name: CLI ${{ matrix.cli_version }} x Cluster ${{ matrix.cluster_version }}
+        run: |
+          make CLI_VERSION=${{ matrix.cli_version }} CLUSTER_VERSION=${{ matrix.cluster_version }} cli-platform-cross-version-test
+

tests/cli-platform-cross-version-test.sh
@@ -5,12 +5,19 @@ set -e
 readonly CLI_VERSION=${1-stable}
 readonly CLUSTER_VERSION=${2-latest}
 readonly TEST_TOPIC=$CLI_VERSION-x-$CLUSTER_VERSION
-readonly FLUVIO_BIN=${FLUVIO_BIN:-~/.fluvio/bin/fluvio}
 readonly PAYLOAD_SIZE=${PAYLOAD_SIZE:-100}
 readonly CI_SLEEP=${CI_SLEEP:-5}
 readonly CI=${CI:-}
 readonly SKIP_SETUP=${SKIP_SETUP:-}
 readonly SKIP_CLEANUP=${SKIP_CLEANUP:-}
+
+if [[ -z "$SKIP_SETUP" ]];
+then
+    readonly FLUVIO_BIN=${FLUVIO_BIN?Set FLUVIO_BIN if skipping setup}
+else
+    # We want to avoid letting Makefile set bin path to target dir
+    readonly FLUVIO_BIN=~/.fluvio/bin/fluvio
+fi
 
 # If we're in CI, we want to slow down execution
 # to give CPU some time to rest, so we don't time out