97df132bb003b44393fe9f5e1efbc41e220ffed0
T.J. Telan <t.telan@gmail.com>
2021-09-11 19:48:47 +0000

    Fix for cd_dev (#1612)
    Fix cli cross compat test in CD_Dev Broken by #1607

--- tests/cli-platform-cross-version-test.sh
+++ tests/cli-platform-cross-version-test.sh
@@ -5,18 +5,20 @@ set -e
 readonly CLI_VERSION=${1-stable}
 readonly CLUSTER_VERSION=${2-latest}
 readonly TEST_TOPIC=$CLI_VERSION-x-$CLUSTER_VERSION
-readonly FLUVIO_BIN=${FLUVIO_BIN:-~/.fluvio/bin/fluvio}
 readonly PAYLOAD_SIZE=${PAYLOAD_SIZE:-100}
 readonly CI_SLEEP=${CI_SLEEP:-5}
 readonly CI=${CI:-}
 readonly SKIP_SETUP=${SKIP_SETUP:-}
 readonly SKIP_CLEANUP=${SKIP_CLEANUP:-}
 
-# If we're in CI, we want to slow down execution
-# to give CPU some time to rest, so we don't time out
-function ci_check() {
-    :
-}
+if [[ -z "$SKIP_SETUP" ]];
+then
+    # This will default to the Makefile value if not provided
+    readonly FLUVIO_BIN=${FLUVIO_BIN?Set FLUVIO_BIN if skipping setup}
+else
+    # We want to avoid letting Makefile set bin path to target dir
+    readonly FLUVIO_BIN=~/.fluvio/bin/fluvio
+fi
 
 function setup_cluster() {
     echo "Installing cluster @ VERSION: $CLUSTER_VERSION"
@@ -38,16 +40,12 @@ function setup_cli() {
 
 function run_test() {
     local TEST_DATA=$(shuf -zer -n${PAYLOAD_SIZE}  {A..Z} {a..z} {0..9} | tr -d '\0')
-    ci_check;
 
     $FLUVIO_BIN topic create $TEST_TOPIC || true
-    ci_check;
 
     echo $TEST_DATA | $FLUVIO_BIN produce $TEST_TOPIC
-    ci_check;
 
     $FLUVIO_BIN consume $TEST_TOPIC -B -d
-    ci_check;
     # TODO: Verify the test output matches
 }
 
@@ -69,9 +67,7 @@ function main() {
     if [[ -z "$SKIP_SETUP" ]];
     then
         setup_cluster $CLUSTER_VERSION;
-        ci_check;
         setup_cli $CLI_VERSION;
-        ci_check;
     else
         echo "Skipping setup"
     fi
