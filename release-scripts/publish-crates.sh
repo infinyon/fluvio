494d0a4d3d2bdcee6ea8201eee2fe32dbbbd270a
T.J. Telan <t.telan@gmail.com>
2021-09-15 16:23:11 -0700

    Ready PR for publishing crates in release workflow
    Crates publish is last step

--- release-scripts/publish-crates.sh
+++ release-scripts/publish-crates.sh
@@ -6,20 +6,20 @@ PUBLISH_CRATES=(
     fluvio-protocol-core
     fluvio-smartstream-derive
     fluvio-types
- #   fluvio-protocol-derive
- #   fluvio-protocol-codec
- #   fluvio-protocol
- #   fluvio-dataplane-protocol
- #   fluvio-socket
- #   fluvio-stream-model
- #   fluvio-controlplane-metadata
- #   fluvio-spu-schema
- #   fluvio-sc-schema
- #   fluvio-smartstream
- #   fluvio
- #   #fluvio-stream-dispatcher # This was actually updated, so let's remove during testing
- #   fluvio-package-index
- #   fluvio-extension-common
+    fluvio-protocol-derive
+    fluvio-protocol-codec
+    fluvio-protocol
+    fluvio-dataplane-protocol
+    fluvio-socket
+    fluvio-stream-model
+    fluvio-controlplane-metadata
+    fluvio-spu-schema
+    fluvio-sc-schema
+    fluvio-smartstream
+    fluvio
+    fluvio-stream-dispatcher
+    fluvio-package-index
+    fluvio-extension-common
 )
 
 LOOP_AGAIN=false
