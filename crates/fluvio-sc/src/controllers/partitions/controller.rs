1d324e272d681624f2f713e592b73f5f5d141e3e
Sehyo <sehyo@fluvio.io>
2021-09-19 05:01:16 +0000

    update test

--- crates/fluvio-sc/src/controllers/partitions/controller.rs
+++ crates/fluvio-sc/src/controllers/partitions/controller.rs
@@ -50,7 +50,10 @@ where
         use tokio::select;
 
         let mut spu_status_listener = self.spus.change_listener();
+        let _ = spu_status_listener.wait_for_initial_sync().await;
+
         let mut partition_listener = self.partitions.change_listener();
+        let _ = partition_listener.wait_for_initial_sync().await;
 
         loop {
             self.sync_spu_changes(&mut spu_status_listener).await;
@@ -137,18 +140,81 @@ where
     }
 }
 
-/*
+
 #[cfg(test)]
 mod test {
 
 
+    use super::*;
+
+    mod meta {
+        use fluvio_controlplane_metadata::core::{MetadataItem, MetadataRevExtension};
+
+
+
+        /// simple memory representation of meta
+        #[derive(Debug, Default, PartialEq, Clone)]
+        pub struct MemoryMeta {
+            pub rev: u32,
+        }
+
+        impl MetadataItem for MemoryMeta {
+            type UId = u32;
+
+            fn uid(&self) -> &Self::UId {
+                &self.rev
+            }
+
+            fn is_newer(&self, another: &Self) -> bool {
+                self.rev >= another.rev
+            }
+        }
+
+        impl MetadataRevExtension for MemoryMeta {
+            fn next_rev(&self) -> Self {
+                Self {
+                    rev: self.rev + 1,
+                }
+            }
+        }
+
+        impl MemoryMeta {
+            pub fn new(rev: u32) -> Self {
+                Self {
+                    rev,
+                }
+            }
+        }
+
+        /* 
+        impl From<u32> for MetadataContext<MemoryMeta> {
+            fn from(val: u32) -> MetadataContext<MemoryMeta> {
+                MemoryMeta::new(val).into()
+            }
+        }
+        */
+
+    }
+
+    use fluvio_controlplane_metadata::store::MetadataStoreObject;
+    use meta::*;
+
+    type MemPartition = MetadataStoreObject<PartitionSpec,MemoryMeta>;
+    type MemSpu = MetadataStoreObject<SpuSpec,MemoryMeta>;
 
     #[fluvio_future::test(ignore)]
-
     async fn test_election() {
+
+        let partitions: StoreContext<PartitionSpec,MemoryMeta> =  StoreContext::new();
+        let spus: StoreContext<SpuSpec,MemoryMeta> = StoreContext::new();
+
+        let controller = PartitionController::start(partitions.clone(), spus.clone());
+
+        // add partitions
+        spus.store().sync_all(vec![]).await;
 
 
     }
 
 }
-*/
+
