2ae3b9ae7164a8a26ae6f2a083041dbe452fd6a5
T.J. Telan <t.telan@gmail.com>
2021-09-24 10:49:58 -0700

    Clean up script
    passes shellcheck lints

--- release-scripts/test-crate-version.sh
+++ release-scripts/test-crate-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -u
+set -eu
 
 PUBLISH_CRATES=(
     fluvio-protocol-core
@@ -27,20 +27,27 @@ CHECK_CRATES=()
 readonly VERBOSE=${VERBOSE:-false}
 
 # Check if we have cargo-download in path
-# Attempt to download it
+# If not, attempt to download it
 function cargo_download_check() {
-    :
+    if which cargo-download;
+    then
+        echo "🔧 cargo-download found"
+    else
+        echo "cargo-download not found"
+        echo "Attempting to download"
+        cargo install cargo-download
+    fi
 }
 
 function download_crate() {
     CRATE_NAME=$1
-    mkdir -p ./crates_io/$CRATE_NAME
+    mkdir -p ./crates_io/"$CRATE_NAME"
 
     if [[ $VERBOSE == true ]];
     then
-        cargo download -x  $CRATE_NAME -o ./crates_io/$CRATE_NAME --verbose
+        cargo download -x "$CRATE_NAME" -o ./crates_io/"$CRATE_NAME" --verbose
     else
-        cargo download -x  $CRATE_NAME -o ./crates_io/$CRATE_NAME --quiet
+        cargo download -x "$CRATE_NAME" -o ./crates_io/"$CRATE_NAME" --quiet
     fi
     
 }
@@ -51,9 +58,10 @@ function compare_crates_src() {
 
     if [[ $VERBOSE == true ]];
     then
-        diff -bur ./crates/$CRATE_NAME/src ./crates_io/$CRATE_NAME/src;
+        diff -bur ./crates/"$CRATE_NAME"/src ./crates_io/"$CRATE_NAME"/src;
     else
-        diff -burq ./crates/$CRATE_NAME/src ./crates_io/$CRATE_NAME/src;
+        # Don't print the diff
+        diff -burq ./crates/"$CRATE_NAME"/src ./crates_io/"$CRATE_NAME"/src;
     fi
     
 }
@@ -61,10 +69,10 @@ function compare_crates_src() {
 function compare_crates_version() {
     CRATE_NAME=$1
 
-    CRATESIO_VERSION=$(cat ./crates_io/$CRATE_NAME/Cargo.toml | grep -e "^version" | head -1)
-    REPO_VERSION=$(cat ./crates/$CRATE_NAME/Cargo.toml | grep -e "^version" | head -1)
+    CRATESIO_VERSION=$(grep -e "^version" ./crates_io/"$CRATE_NAME"/Cargo.toml | head -1)
+    REPO_VERSION=$(grep -e "^version" ./crates/"$CRATE_NAME"/Cargo.toml | head -1)
 
-    if [[ $CRATESIO_VERSION == $REPO_VERSION ]];
+    if [[ "$CRATESIO_VERSION" == "$REPO_VERSION" ]];
     then
         return 0
     else
@@ -76,36 +84,41 @@ function compare_crates_version() {
 # ✅ If src does not match and version does not match
 # ❌ If src matches and version does not match (This is highly unlikely)
 # ❌ If src does not match and version does match (This is the most common fail case)
-function does_repo_crate_version_need_bump() {
+function check_crate() {
     SRC_MATCH=$1
     VERSION_MATCH=$2
     CRATE_NAME=$3
 
+    # No changes between repo and crates.io
     if [[ "$SRC_MATCH" == true && "$VERSION_MATCH" == true ]];
     then
-        echo "✅ Crate has not changed"
+        echo "🟢 Repo code does not differ from crates.io"
     fi
 
+    # Code changes found, but versions don't match
+    # It's assumed that the repo is bumped appropriately
     if [[ "$SRC_MATCH" == false && "$VERSION_MATCH" == false ]];
     then
-        echo "✅ Crate has already been bumped"
+        echo "🟢 Repo code differs, but version has been updated too"
     fi
 
+    # Code changes found, however versions match
+    # It's assumed that the repo has increased version number
     if [[ "$SRC_MATCH" == false && "$VERSION_MATCH" == true ]];
     then
-        echo "❌ Code has changed but version needs to be bumped"
+        echo "⛔ Code has changed but version needs to be bumped"
         CHECK_CRATES+=("$CRATE_NAME")
         ALL_CRATE_CHECK_PASS=false
     fi
 
+    # No code changes found, but the versions are different
+    # This is unneeded change w/o code modifications
     if [[ "$SRC_MATCH" == true && "$VERSION_MATCH" == false ]];
     then
-        echo "❌ Code has NOT changed, but versions don't match. Something is weird."
+        echo "🔴 Code has NOT changed, but versions don't match. Something is weird."
         CHECK_CRATES+=("$CRATE_NAME")
         ALL_CRATE_CHECK_PASS=false
     fi
-
-
 }
 
 function main() {
@@ -127,28 +140,20 @@ function main() {
         SRC_MATCH=false        
         VERSION_MATCH=false
 
-        download_crate $crate;
+        download_crate "$crate";
 
-
-        compare_crates_src $crate;
-        if [[ "$?" == 0 ]];
+        if compare_crates_src "$crate";
         then
-            #echo "Crate src match"
             SRC_MATCH=true
-        #else
-        #    #echo "Crate src DOES NOT match"
         fi 
         
-        compare_crates_version $crate;
-        if [[ "$?" == 0 ]];
+        
+        if compare_crates_version "$crate";
         then
-            #echo "Crate version match"
             VERSION_MATCH=true
-        #else
-        #    #echo "Crate version DOES NOT match"
         fi 
 
-        does_repo_crate_version_need_bump $SRC_MATCH $VERSION_MATCH $crate;
+        check_crate "$SRC_MATCH" "$VERSION_MATCH" "$crate";
     done
 
     echo
