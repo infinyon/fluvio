510ed858c69e542b7ff5bcc091208e89f78bab0b
Sehyo <sehyo@fluvio.io>
2021-09-27 00:40:55 +0000

    add test cluster to test driver

--- crates/fluvio-test-util/setup/environment/mod.rs
+++ crates/fluvio-test-util/setup/environment/mod.rs
@@ -5,13 +5,18 @@ pub use common::*;
 
 mod common {
     use async_trait::async_trait;
-
-    use fluvio_cluster::{StartStatus, runtime::spu::SpuClusterManager};
     use serde::{Serialize, Deserialize};
 
-    /// Environment driver for test
+    use fluvio_cluster::{StartStatus, runtime::spu::SpuClusterManager};
+
+    #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+    pub enum EnvironmentType {
+        K8,
+        Local,
+    }
+
     #[async_trait]
-    pub trait TestEnvironmentDriver {
+    pub trait EnvironmentDriver {
         /// remove cluster
         async fn remove_cluster(&self);
 
@@ -21,22 +26,47 @@ mod common {
         fn create_cluster_manager(&self) -> Box<dyn SpuClusterManager>;
     }
 
-    #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
-    pub enum EnvironmentType {
-        K8,
-        Local,
+    #[derive(Clone)]
+    pub enum TestEnvironmentDriver {
+        K8(K8EnvironmentDriver),
+        Local(LocalEnvDriver),
+    }
+
+    impl TestEnvironmentDriver {
+        /// remove cluster
+        pub async fn remove_cluster(&self) {
+            match self {
+                Self::K8(k8) => k8.remove_cluster().await,
+                Self::Local(local) => local.remove_cluster().await,
+            }
+        }
+
+        /// install cluster
+        pub async fn start_cluster(&self) -> StartStatus {
+            match self {
+                Self::K8(k8) => k8.start_cluster().await,
+                Self::Local(local) => local.start_cluster().await,
+            }
+        }
+
+        pub fn create_cluster_manager(&self) -> Box<dyn SpuClusterManager> {
+            match self {
+                Self::K8(k8) => k8.create_cluster_manager(),
+                Self::Local(local) => local.create_cluster_manager(),
+            }
+        }
     }
 
     use crate::{setup::environment::k8::K8EnvironmentDriver, test_meta::environment::EnvironmentSetup};
     use crate::setup::environment::local::LocalEnvDriver;
 
-    pub fn create_driver(option: EnvironmentSetup) -> Box<dyn TestEnvironmentDriver> {
+    pub fn create_driver(option: EnvironmentSetup) -> TestEnvironmentDriver {
         if option.local {
             //println!("using local environment driver");
-            Box::new(LocalEnvDriver::new(option)) as Box<dyn TestEnvironmentDriver>
+            TestEnvironmentDriver::Local(LocalEnvDriver::new(option))
         } else {
             //println!("using k8 environment driver");
-            Box::new(K8EnvironmentDriver::new(option)) as Box<dyn TestEnvironmentDriver>
+            TestEnvironmentDriver::K8(K8EnvironmentDriver::new(option))
         }
     }
 }
