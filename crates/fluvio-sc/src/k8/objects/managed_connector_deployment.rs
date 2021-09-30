05dc77ad27be0f542c27094b937a0fc44ea6308a
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-09-30 14:02:44 -0400

    cargo fmt

--- crates/fluvio-sc/src/k8/objects/managed_connector_deployment.rs
+++ crates/fluvio-sc/src/k8/objects/managed_connector_deployment.rs
@@ -82,9 +82,12 @@ mod extended {
         fn convert_from_k8(
             k8_obj: K8Obj<Self::K8Spec>,
         ) -> Result<MetadataStoreObject<Self, K8MetaItem>, K8ConvertError<Self::K8Spec>> {
-            if let Some(_) = k8_obj.metadata.owner_references.iter().find(|v| {
-                v.kind == K8ManagedConnectorSpec::metadata().names.kind
-            })  {
+            if let Some(_) = k8_obj
+                .metadata
+                .owner_references
+                .iter()
+                .find(|v| v.kind == K8ManagedConnectorSpec::metadata().names.kind)
+            {
                 trace!("converting k8 managed connector: {:#?}", k8_obj);
                 default_convert_from_k8(k8_obj)
             } else {
