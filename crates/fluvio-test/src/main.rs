92eb1777547e955ed6df0b4a5d4e57b3c76d11d8
T.J. Telan <t.telan@gmail.com>
2021-10-01 18:28:38 -0700

    re-enable panic handler

--- crates/fluvio-test/src/main.rs
+++ crates/fluvio-test/src/main.rs
@@ -61,8 +61,7 @@ fn main() {
         exit(-1);
     }
 
-    let _panic_timer = TestTimer::start();
-    /*
+    let panic_timer = TestTimer::start();
     std::panic::set_hook(Box::new(move |panic_info| {
         let mut panic_timer = panic_timer.clone();
         panic_timer.stop();
@@ -76,7 +75,6 @@ fn main() {
         eprintln!("Test panicked: {:#?}",panic_info);
         eprintln!("{}", test_result);
     }));
-    */
 
     let test_result = run_test(option.environment.clone(), test_opt, test_meta);
     cluster_cleanup(option.environment);
