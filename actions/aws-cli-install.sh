c83a4cb9cf0ebce012adedf3b5bef218ed8ee0d3
Nick Cardin <nick@cardin.email>
2021-10-04 23:23:53 +0000

    WIP

--- actions/aws-cli-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-which aws 2> /dev/null > /dev/null || {
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip";
-    unzip awscliv2.zip;
-    sudo ./aws/install;
-}
