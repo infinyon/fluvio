fcd54c9e8614565057ad0e689b315a28f67aefcd
Sehyo <sehyo@fluvio.io>
2021-10-10 23:25:45 +0000

    try2

--- k8-util/cluster/start-minikube-mac.sh
+++ k8-util/cluster/start-minikube-mac.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
+brew install hyperkit
 sudo mv /var/db/dhcpd_leases /var/db/dhcpd_leases.old
 sudo touch /var/db/dhcpd_leases
-brew install hyperkit
 brew install minikube
 minikube config set memory 16384
 minikube start --driver hyperkit --kubernetes-version=1.21.2
