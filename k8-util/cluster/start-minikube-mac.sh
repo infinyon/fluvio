600ad4fca69e23a443c3db00349469f4222ea87a
Sehyo <sehyo@fluvio.io>
2021-10-10 23:55:15 +0000

    revert back to virtualbox

--- k8-util/cluster/start-minikube-mac.sh
+++ k8-util/cluster/start-minikube-mac.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -x
-brew install hyperkit
-sudo mv /var/db/dhcpd_leases /var/db/dhcpd_leases.old
-sudo touch /var/db/dhcpd_leases
 brew install minikube
 minikube config set memory 16384
-minikube start --driver hyperkit --kubernetes-version=1.21.2
+minikube start --driver virtualbox --kubernetes-version=1.21.2
 kubectl get nodes
