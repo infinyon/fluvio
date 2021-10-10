1bde0ad1d5ee0654fce8b3872ba0be03d9b3dd45
Sehyo <sehyo@fluvio.io>
2021-10-10 23:12:53 +0000

    fix

--- k8-util/cluster/start-minikube-mac.sh
+++ k8-util/cluster/start-minikube-mac.sh
@@ -2,7 +2,6 @@
 set -x
 brew install hyperkit
 brew install minikube
-brew install helm
 minikube config set memory 16384
 minikube start --driver hyperkit --kubernetes-version=1.21.2
 kubectl get nodes
