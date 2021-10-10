cfef28db41ba37f2ead4e1e28e181b51603fc776
Sehyo <sehyo@fluvio.io>
2021-10-10 22:04:56 +0000

    re-order dependencies

--- k8-util/cluster/start-minikube-mac.sh
+++ k8-util/cluster/start-minikube-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
-brew install minikube
 brew install hyperkit
+brew install minikube
 minikube config set memory 16384
 minikube start --driver hyperkit --kubernetes-version=1.21.2
 kubectl get nodes
