9b5b25803e8364c8e9f974f516e18d6e7e7e9ec7
Sehyo <sehyo@fluvio.io>
2021-10-21 00:52:44 +0000

    add missing files

--- crates/fluvio-sc-schema/src/objects/watch.rs
+++ crates/fluvio-sc-schema/src/objects/watch.rs
@@ -85,3 +85,263 @@ where
         }
     }
 }
+<<<<<<< HEAD
+=======
+
+// later this can be written using procedure macro
+mod encoding {
+
+    use std::io::Error;
+    use std::io::ErrorKind;
+
+    use fluvio_controlplane_metadata::smartstream::SmartStreamSpec;
+    use tracing::trace;
+
+    use dataplane::core::Encoder;
+    use dataplane::core::Decoder;
+    use dataplane::core::Version;
+    use dataplane::bytes::{Buf, BufMut};
+
+    use super::*;
+
+    impl WatchRequest {
+        /// type represent as string
+        fn type_string(&self) -> &'static str {
+            match self {
+                Self::Topic(_) => TopicSpec::LABEL,
+                Self::Spu(_) => SpuSpec::LABEL,
+                Self::SpuGroup(_) => SpuGroupSpec::LABEL,
+                Self::Partition(_) => PartitionSpec::LABEL,
+                Self::ManagedConnector(_) => ManagedConnectorSpec::LABEL,
+                Self::SmartModule(_) => SmartModuleSpec::LABEL,
+                Self::Table(_) => TableSpec::LABEL,
+                Self::SmartStream(_) => SmartStreamSpec::LABEL,
+            }
+        }
+    }
+
+    impl Encoder for WatchRequest {
+        fn write_size(&self, version: Version) -> usize {
+            let type_size = self.type_string().to_owned().write_size(version);
+
+            type_size
+                + match self {
+                    Self::Topic(s) => s.write_size(version),
+                    Self::SpuGroup(s) => s.write_size(version),
+                    Self::Spu(s) => s.write_size(version),
+                    Self::Partition(s) => s.write_size(version),
+                    Self::ManagedConnector(s) => s.write_size(version),
+                    Self::SmartModule(s) => s.write_size(version),
+                    Self::Table(s) => s.write_size(version),
+                    Self::SmartStream(s) => s.write_size(version),
+                    
+                }
+        }
+
+        // encode match
+        fn encode<T>(&self, dest: &mut T, version: Version) -> Result<(), Error>
+        where
+            T: BufMut,
+        {
+            self.type_string().to_owned().encode(dest, version)?;
+
+            match self {
+                Self::Topic(s) => s.encode(dest, version)?,
+                Self::SpuGroup(s) => s.encode(dest, version)?,
+                Self::Spu(s) => s.encode(dest, version)?,
+                Self::Partition(s) => s.encode(dest, version)?,
+                Self::ManagedConnector(s) => s.encode(dest, version)?,
+                Self::SmartModule(s) => s.encode(dest, version)?,
+                Self::Table(s) => s.encode(dest, version)?,
+                Self::SmartStream(s) => s.encode(dest, version)?,
+            }
+
+            Ok(())
+        }
+    }
+
+    impl Decoder for WatchRequest {
+        fn decode<T>(&mut self, src: &mut T, version: Version) -> Result<(), Error>
+        where
+            T: Buf,
+        {
+            let mut typ = "".to_owned();
+            typ.decode(src, version)?;
+            trace!("decoded type: {}", typ);
+
+            match typ.as_ref() {
+                TopicSpec::LABEL => {
+                    let mut response: Epoch = Epoch::default();
+                    response.decode(src, version)?;
+                    *self = Self::Topic(response);
+                    Ok(())
+                }
+
+                SpuGroupSpec::LABEL => {
+                    let mut response: Epoch = Epoch::default();
+                    response.decode(src, version)?;
+                    *self = Self::SpuGroup(response);
+                    Ok(())
+                }
+
+                SpuSpec::LABEL => {
+                    let mut response: Epoch = Epoch::default();
+                    response.decode(src, version)?;
+                    *self = Self::Spu(response);
+                    Ok(())
+                }
+
+                PartitionSpec::LABEL => {
+                    let mut response: Epoch = Epoch::default();
+                    response.decode(src, version)?;
+                    *self = Self::Partition(response);
+                    Ok(())
+                }
+
+                TableSpec::LABEL => {
+                    let mut response: Epoch = Epoch::default();
+                    response.decode(src, version)?;
+                    *self = Self::Table(response);
+                    Ok(())
+                }
+
+                SmartStreamSpec::LABEL => {
+                    let mut response: Epoch = Epoch::default();
+                    response.decode(src, version)?;
+                    *self = Self::SmartStream(response);
+                    Ok(())
+                }
+
+                // Unexpected type
+                _ => Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!("invalid request type {}", typ),
+                )),
+            }
+        }
+    }
+
+    impl WatchResponse {
+        /// type represent as string
+        fn type_string(&self) -> &'static str {
+            match self {
+                Self::Topic(_) => TopicSpec::LABEL,
+                Self::Spu(_) => SpuSpec::LABEL,
+                Self::SpuGroup(_) => SpuGroupSpec::LABEL,
+                Self::Partition(_) => PartitionSpec::LABEL,
+                Self::ManagedConnector(_) => ManagedConnectorSpec::LABEL,
+                Self::SmartModule(_) => SmartModuleSpec::LABEL,
+                Self::Table(_) => TableSpec::LABEL,
+                Self::SmartStream(_) => SmartStreamSpec::LABEL,
+            }
+        }
+    }
+
+    impl Encoder for WatchResponse {
+        fn write_size(&self, version: Version) -> usize {
+            let type_size = self.type_string().to_owned().write_size(version);
+
+            type_size
+                + match self {
+                    Self::Topic(s) => s.write_size(version),
+                    Self::SpuGroup(s) => s.write_size(version),
+                    Self::Spu(s) => s.write_size(version),
+                    Self::Partition(s) => s.write_size(version),
+                    Self::ManagedConnector(s) => s.write_size(version),
+                    Self::SmartModule(s) => s.write_size(version),
+                    Self::Table(s) => s.write_size(version),
+                    Self::SmartStream(s) => s.write_size(version),
+                }
+        }
+
+        // encode match
+        fn encode<T>(&self, dest: &mut T, version: Version) -> Result<(), Error>
+        where
+            T: BufMut,
+        {
+            self.type_string().to_owned().encode(dest, version)?;
+
+            match self {
+                Self::Topic(s) => s.encode(dest, version)?,
+                Self::SpuGroup(s) => s.encode(dest, version)?,
+                Self::Spu(s) => s.encode(dest, version)?,
+                Self::Partition(s) => s.encode(dest, version)?,
+                Self::ManagedConnector(s) => s.encode(dest, version)?,
+                Self::SmartModule(s) => s.encode(dest, version)?,
+                Self::Table(s) => s.encode(dest, version)?,
+                Self::SmartStream(s) => s.encode(dest, version)?,
+            }
+
+            Ok(())
+        }
+    }
+
+    impl Decoder for WatchResponse {
+        fn decode<T>(&mut self, src: &mut T, version: Version) -> Result<(), Error>
+        where
+            T: Buf,
+        {
+            let mut typ = "".to_owned();
+            typ.decode(src, version)?;
+            trace!("decoded type: {}", typ);
+
+            match typ.as_ref() {
+                TopicSpec::LABEL => {
+                    let mut response = MetadataUpdate::default();
+                    response.decode(src, version)?;
+                    *self = Self::Topic(response);
+                    Ok(())
+                }
+
+                SpuGroupSpec::LABEL => {
+                    let mut response = MetadataUpdate::default();
+                    response.decode(src, version)?;
+                    *self = Self::SpuGroup(response);
+                    Ok(())
+                }
+
+                SpuSpec::LABEL => {
+                    let mut response = MetadataUpdate::default();
+                    response.decode(src, version)?;
+                    *self = Self::Spu(response);
+                    Ok(())
+                }
+
+                PartitionSpec::LABEL => {
+                    let mut response = MetadataUpdate::default();
+                    response.decode(src, version)?;
+                    *self = Self::Partition(response);
+                    Ok(())
+                }
+
+                SmartModuleSpec::LABEL => {
+                    let mut response = MetadataUpdate::default();
+                    response.decode(src, version)?;
+                    *self = Self::SmartModule(response);
+                    Ok(())
+                }
+
+                TableSpec::LABEL => {
+                    let mut response = MetadataUpdate::default();
+                    response.decode(src, version)?;
+                    *self = Self::Table(response);
+                    Ok(())
+                }
+
+                SmartStreamSpec::LABEL => {
+                    let mut response = MetadataUpdate::default();
+                    response.decode(src, version)?;
+                    *self = Self::SmartStream(response);
+                    Ok(())
+                }
+
+                // Unexpected type
+                _ => Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!("invalid spec type {}", typ),
+                )),
+            }
+        }
+    }
+}
+>>>>>>> add missing files
---
+++ crates/fluvio-sc-schema/src/smartstream/mod.rs
@@ -0,0 +1,63 @@
+pub use fluvio_controlplane_metadata::smartstream::*;
+
+mod convert {
+
+    use std::io::Error;
+    use std::io::ErrorKind;
+    use std::convert::TryInto;
+
+    use crate::objects::*;
+    use super::*;
+
+    impl From<SmartStreamSpec> for AllCreatableSpec {
+        fn from(spec: SmartStreamSpec) -> Self {
+            Self::SmartStream(spec)
+        }
+    }
+
+    impl DeleteSpec for SmartStreamSpec {
+        fn into_request<K>(key: K) -> DeleteRequest
+        where
+            K: Into<Self::DeleteKey>,
+        {
+            DeleteRequest::SmartStream(key.into())
+        }
+    }
+
+    impl ListSpec for SmartStreamSpec {
+        type Filter = NameFilter;
+
+        fn into_list_request(filters: Vec<Self::Filter>) -> ListRequest {
+            ListRequest::SmartStream(filters)
+        }
+    }
+
+    impl TryInto<Vec<Metadata<SmartStreamSpec>>> for ListResponse {
+        type Error = Error;
+
+        fn try_into(self) -> Result<Vec<Metadata<SmartStreamSpec>>, Self::Error> {
+            match self {
+                ListResponse::SmartStream(s) => Ok(s),
+                _ => Err(Error::new(ErrorKind::Other, "not smartmodule")),
+            }
+        }
+    }
+
+    impl From<MetadataUpdate<SmartStreamSpec>> for WatchResponse {
+        fn from(update: MetadataUpdate<SmartStreamSpec>) -> Self {
+            Self::SmartStream(update)
+        }
+    }
+
+    impl TryInto<MetadataUpdate<SmartStreamSpec>> for WatchResponse {
+        type Error = Error;
+
+        fn try_into(self) -> Result<MetadataUpdate<SmartStreamSpec>, Self::Error> {
+            match self {
+                WatchResponse::SmartModule(m) => Ok(m),
+                _ => Err(Error::new(ErrorKind::Other, "not smartmodule")),
+            }
+        }
+    }
+
+}
