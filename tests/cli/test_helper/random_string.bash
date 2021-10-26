da1be9a5af97542104eb9ea17a496eb2371b7c05
T.J. Telan <t.telan@gmail.com>
2021-10-26 16:04:30 -0700

    Fix random string

--- tests/cli/test_helper/random_string.bash
+++ tests/cli/test_helper/random_string.bash
@@ -1,14 +1,14 @@
 # Note: We should only generate strings w/ *lowercase* alphanum or '-'
 function random_string() {
     DEFAULT_LEN=7
-    STRING_LEN=${1:$DEFAULT_LEN}
+    STRING_LEN=${1:-$DEFAULT_LEN}
 
     #RANDOM_STRING=$(shuf -zer -n"$STRING_LEN" {A..Z} {a..z} {0..9})
-    #RANDOM_STRING=$(shuf -zer -n"$STRING_LEN" {a..z} {0..9})
-    RANDOM_STRING=$(shuf -zer -n7 {a..z} {0..9})
+    RANDOM_STRING=$(shuf -zer -n"$STRING_LEN" {a..z} {0..9})
+    #RANDOM_STRING=$(shuf -zer -n7 {a..z} {0..9})
 
     if [[ -n $DEBUG ]]; then
-        echo "# DEBUG: Random str: $RANDOM_STRING" >&3
+        echo "# DEBUG: Random str (len: $STRING_LEN): $RANDOM_STRING" >&3
     fi
 
     export RANDOM_STRING
