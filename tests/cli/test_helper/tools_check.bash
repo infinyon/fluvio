eae8f1b12d53f51c05a8fa3f3104ee301e7522da
T.J. Telan <t.telan@gmail.com>
2021-10-25 18:28:58 -0700

    Search for fluvio binary in tools_check

--- tests/cli/test_helper/tools_check.bash
+++ tests/cli/test_helper/tools_check.bash
@@ -1,9 +1,29 @@
-# TODO: Use FLUVIO_BIN env var instead of expecting `fluvio` in PATH.
-# Search order: $FLUVIO_BIN, $PATH, $(pwd)/fluvio, $HOME/.fluvio/bin/fluvio
+# Resolve path to `fluvio` binary instead of expecting it in PATH
+# Search order: $FLUVIO_BIN, in PATH, current directory, home directory
 
-setup() {
-    FLUVIO_BIN=$(which fluvio)
+function set_bin_path_then_exit() {
+    #echo "# Fluvio binary path: $FLUVIO_BIN" >&3
     export FLUVIO_BIN
-    # Print out `fluvio` path used in test
-    echo "# Fluvio binary path: $FLUVIO_BIN" >&3
 }
+
+main() {
+    if [[ -n $FLUVIO_BIN ]]; then
+        #echo "# FLUVIO_BIN was defined" >&3
+        FLUVIO_BIN="$FLUVIO_BIN"
+        set_bin_path_then_exit;
+    elif which fluvio; then
+        #echo "# fluvio in PATH" >&3
+        FLUVIO_BIN="$(which fluvio)"
+        set_bin_path_then_exit;
+    elif test -f "$(pwd)/fluvio"; then
+        #echo "# fluvio in current directory" >&3
+        FLUVIO_BIN="$(pwd)/fluvio"
+        set_bin_path_then_exit;
+    elif test -f "$HOME/.fluvio/bin/fluvio"; then
+        #echo "# fluvio in home directory" >&3
+        FLUVIO_BIN="$HOME/.fluvio/bin/fluvio"
+        set_bin_path_then_exit;
+    fi
+}
+
+main;
