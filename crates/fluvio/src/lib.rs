a4c9d59bc37228f4941c435527de7a347d7207d6
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-10-29 12:16:42 -0400

    Added SmartStreamConfig to public export in fluvio client

--- crates/fluvio/src/lib.rs
+++ crates/fluvio/src/lib.rs
@@ -96,7 +96,7 @@ pub mod config;
 use tracing::instrument;
 pub use error::FluvioError;
 pub use config::FluvioConfig;
-pub use producer::{TopicProducer, RecordKey};
+pub use producer::{TopicProducer, RecordKey, SmartStreamConfig};
 pub use consumer::{
     PartitionConsumer, ConsumerConfig, MultiplePartitionConsumer, PartitionSelectionStrategy,
 };
