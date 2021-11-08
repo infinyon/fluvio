7f875779fa9fcc2bace2a1c736a41adf20dc3466
Sehyo <sehyo@fluvio.io>
2021-11-08 04:46:22 +0000

    fix ui-test

--- crates/fluvio-smartstream/ui-tests/pass_join.rs
+++ crates/fluvio-smartstream/ui-tests/pass_join.rs
@@ -1,7 +1,7 @@
 use fluvio_smartstream::{smartstream, Record, RecordData, Result};
 
 #[smartstream(join)]
-pub fn my_join(_record: &Record, _record1: Record) -> Result<(Option<RecordData>, RecordData)> {
+pub fn my_join(_record: &Record, _record1: &Record) -> Result<(Option<RecordData>, RecordData)> {
     unimplemented!()
 }
 
