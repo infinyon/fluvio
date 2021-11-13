4180208044dcbb0eecb33a53eec998917f6ac85d
Sehyo Chang <sehyo@fluvio.io>
2021-11-12 19:16:34 -0800

    cargo fmt

--- crates/fluvio-cluster/build.rs
+++ crates/fluvio-cluster/build.rs
@@ -1,8 +1,6 @@
 use std::process::Command;
 
-
 fn main() {
-
     // force it re-run everytime
     println!("cargo:rerun-if-changed=../../k8-util/helm/pkg_app/fluvio-app-none.tgz");
     let _uname_output = Command::new("make")
