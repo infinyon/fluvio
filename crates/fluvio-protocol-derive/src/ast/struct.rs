f23be7ca8808cb1183a0a4a8c4cafb0ac0f3fd80
Luis Moreno <morenol@users.noreply.github.com>
2021-11-15 12:21:56 -0400

    Add support for tuple structs in fluvio protocol

--- crates/fluvio-protocol-derive/src/ast/struct.rs
+++ crates/fluvio-protocol-derive/src/ast/struct.rs
@@ -1,7 +1,12 @@
-use crate::ast::prop::NamedProp;
+use crate::ast::prop::{NamedProp, UnnamedProp};
 use syn::{Fields, Generics, Ident, ItemStruct};
 
-pub(crate) struct FluvioStruct {
+pub(crate) enum FluvioStruct {
+    Named(FluvioNamedStruct),
+    Tuple(FluvioTupleStruct),
+}
+
+pub(crate) struct FluvioNamedStruct {
     pub struct_ident: Ident,
     pub props: Vec<NamedProp>,
     pub generics: Generics,
@@ -10,21 +15,72 @@ pub(crate) struct FluvioStruct {
 impl FluvioStruct {
     pub fn from_ast(item: &ItemStruct) -> syn::Result<Self> {
         let struct_ident = item.ident.clone();
-        let props: Vec<NamedProp> = if let Fields::Named(fields) = &item.fields {
-            let mut prp = vec![];
-            for field in fields.named.iter() {
-                prp.push(NamedProp::from_ast(field)?);
-            }
-            prp
-        } else {
-            vec![]
-        };
         let generics = item.generics.clone();
 
-        Ok(FluvioStruct {
-            struct_ident,
-            props,
-            generics,
-        })
+        let fluvio_struct = match &item.fields {
+            Fields::Named(fields) => {
+                let mut props = vec![];
+                for field in fields.named.iter() {
+                    props.push(NamedProp::from_ast(field)?);
+                }
+
+                FluvioStruct::Named(FluvioNamedStruct {
+                    struct_ident,
+                    props,
+                    generics,
+                })
+            }
+            Fields::Unnamed(fields) => {
+                let mut props = vec![];
+                for field in fields.unnamed.iter() {
+                    props.push(UnnamedProp::from_ast(field)?);
+                }
+                FluvioStruct::Tuple(FluvioTupleStruct {
+                    struct_ident,
+                    props,
+                    generics,
+                })
+            }
+
+            Fields::Unit => FluvioStruct::Tuple(FluvioTupleStruct {
+                struct_ident,
+                props: vec![],
+                generics,
+            }),
+        };
+
+        Ok(fluvio_struct)
     }
+
+    pub fn struct_ident(&self) -> &Ident {
+        match self {
+            FluvioStruct::Named(inner) => &inner.struct_ident,
+            FluvioStruct::Tuple(inner) => &inner.struct_ident,
+        }
+    }
+
+    pub fn generics(&self) -> &Generics {
+        match self {
+            FluvioStruct::Named(inner) => &inner.generics,
+            FluvioStruct::Tuple(inner) => &inner.generics,
+        }
+    }
+
+    pub fn props(&self) -> FluvioStructProps {
+        match self {
+            FluvioStruct::Named(inner) => FluvioStructProps::Named(inner.props.clone()),
+            FluvioStruct::Tuple(inner) => FluvioStructProps::Unnamed(inner.props.clone()),
+        }
+    }
+}
+
+pub(crate) enum FluvioStructProps {
+    Named(Vec<NamedProp>),
+    Unnamed(Vec<UnnamedProp>),
+}
+
+pub(crate) struct FluvioTupleStruct {
+    pub struct_ident: Ident,
+    pub props: Vec<UnnamedProp>,
+    pub generics: Generics,
 }
---
+++ crates/fluvio-protocol/tests/tuple_struct.rs
@@ -0,0 +1,24 @@
+use std::io::Cursor;
+use std::io::Error;
+
+use fluvio_protocol::{Decoder, Encoder};
+
+#[derive(Encoder, Default, Decoder, Debug)]
+pub struct TupleStruct(String, i32);
+
+#[test]
+fn test_encode_tuple_struct() -> Result<(), Error> {
+    let mut v1 = TupleStruct::default();
+    v1.0 = format!("Struct without named fields");
+    v1.1 = 42;
+    let mut src = vec![];
+    v1.encode(&mut src, 0)?;
+
+    // 29 bytes (String) + 4 bytes (i32)
+    assert_eq!(src.len(), 33);
+    let v2 = TupleStruct::decode_from(&mut Cursor::new(src), 0)?;
+    assert_eq!(v2.0, "Struct without named fields");
+    assert_eq!(v2.1, 42);
+
+    Ok(())
+}
