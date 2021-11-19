6e7e91419f9f2e4fcb61d10c8dcb47e2d992154a
Luis Moreno <morenol@users.noreply.github.com>
2021-11-18 22:53:52 -0400

    Use latest version in aggregate

--- crates/fluvio-smartmodule-derive/src/generator/aggregate.rs
+++ crates/fluvio-smartmodule-derive/src/generator/aggregate.rs
@@ -51,7 +51,7 @@ pub fn generate_aggregate_smartmodule(func: &SmartModuleFn, has_params: bool) ->
                 let input_data = Vec::from_raw_parts(ptr, len, len);
                 let mut smartmodule_input = SmartModuleAggregateInput::default();
                 // 13 is version for aggregate
-                if let Err(_err) = Decoder::decode(&mut smartmodule_input, &mut std::io::Cursor::new(input_data), fluvio_smartmodule::api_versions::AGGREGATOR_API) {
+                if let Err(_err) = Decoder::decode(&mut smartmodule_input, &mut std::io::Cursor::new(input_data), fluvio_smartmodule::api_versions::SMART_MODULE_API) {
                     return SmartModuleInternalError::DecodingBaseInput as i32;
                 }
 
@@ -61,7 +61,7 @@ pub fn generate_aggregate_smartmodule(func: &SmartModuleFn, has_params: bool) ->
 
                 let records_input = smartmodule_input.base.record_data;
                 let mut records: Vec<Record> = vec![];
-                if let Err(_err) = Decoder::decode(&mut records, &mut std::io::Cursor::new(records_input), fluvio_smartmodule::api_versions::AGGREGATOR_API) {
+                if let Err(_err) = Decoder::decode(&mut records, &mut std::io::Cursor::new(records_input), fluvio_smartmodule::api_versions::SMART_MODULE_API) {
                     return SmartModuleInternalError::DecodingRecords as i32;
                 };
 
@@ -102,7 +102,7 @@ pub fn generate_aggregate_smartmodule(func: &SmartModuleFn, has_params: bool) ->
 
                 // ENCODING
                 let mut out = vec![];
-                if let Err(_) = Encoder::encode(&mut output, &mut out, fluvio_smartmodule::api_versions::AGGREGATOR_API) {
+                if let Err(_) = Encoder::encode(&mut output, &mut out, fluvio_smartmodule::api_versions::SMART_MODULE_API) {
                     return SmartModuleInternalError::EncodingOutput as i32;
                 }
 
