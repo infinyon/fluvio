f726afb5080eb175d05f38dacb6ba50aa619d925
chris <bohlmannchris1@gmail.com>
2021-11-25 02:17:53 +0000

    using {n} to format lines appropriately; make check-fmt passed

--- crates/fluvio-cli/src/lib.rs
+++ crates/fluvio-cli/src/lib.rs
@@ -138,13 +138,10 @@ mod root {
         Version(VersionOpt),
 
         /// Generate command-line completions for Fluvio
-        /// 
-        /// Run the following two commands to enable fluvio command completions.
+        /// {n}{n}Run the following two commands to enable fluvio command completions.
         /// Open a new terminal for the changes to take effect.
-        /// 
-        /// $ fluvio completions bash > ~/fluvio_completions.sh
-        /// 
-        /// $ echo "source ~/fluvio_completions.sh" >> ~/.bashrc 
+        /// {n}{n}$ fluvio completions bash > ~/fluvio_completions.sh
+        /// {n}$ echo "source ~/fluvio_completions.sh" >> ~/.bashrc
         #[structopt(name = "completions")]
         Completions(CompletionCmd),
 
