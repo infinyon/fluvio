f810a79bbb8403284446f40ae3fb70bdee9bb383
Sehyo Chang <sehyo@fluvio.io>
2021-11-27 23:53:57 +0000

    make cd-dev workflow more reliable by using docker with kind (#1943)
    - clean up workflow by separating jobs
    - add fluvio to bin path

--- k8-util/cluster/start-minikube-mac.sh
+++ k8-util/cluster/start-minikube-mac.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#
+# Hyperkit doesn't work for M1
+# It also is not reliable in Github Actions
+# Only reliable way to run is use of kind in Docker
 set -x
 brew install minikube
-minikube config set memory 16384
-minikube start --driver virtualbox --kubernetes-version=1.21.2
+brew install hyperkit
+minikube config set memory 1024
+minikube start --driver=hyperkit
 kubectl get nodes
