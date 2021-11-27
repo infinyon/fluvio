c9524337b3148a1b8b59811193f879b76c96e35f
Sehyo Chang <sehyo@fluvio.io>
2021-11-27 10:50:02 -0800

    use docker with kind

--- k8-util/cluster/start-minikube-mac.sh
+++ k8-util/cluster/start-minikube-mac.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#
+# Hyperkit doesn't work for M1
+# It also is not reliable in Github Actions
+# Only reliable way to run is use of kind in Docker
 set -x
 brew install minikube
+brew install hyperkit
 minikube config set memory 1024
-minikube start --driver virtualbox
+minikube start --driver=hyperkit
 kubectl get nodes
