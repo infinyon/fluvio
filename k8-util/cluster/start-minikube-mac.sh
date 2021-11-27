9ed26a1da3ca8a089afd295d3da9436628479baf
Sehyo Chang <sehyo@fluvio.io>
2021-11-27 10:40:36 -0800

    use smaller memory

--- k8-util/cluster/start-minikube-mac.sh
+++ k8-util/cluster/start-minikube-mac.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -x
 brew install minikube
-brew install hyperkit
 minikube config set memory 1024
-ls -la $(which docker-machine-driver-hyperkit)
-mkdir -p /var/db
-touch /var/db/dhcpd.leases
-minikube start --driver=hyperkit
+minikube start --driver virtualbox
 kubectl get nodes
