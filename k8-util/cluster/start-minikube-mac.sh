66d136eb206b59e59521e56b3f72ba6276cb962c
Sehyo Chang <sehyo@fluvio.io>
2021-11-27 10:39:35 -0800

    make it hyperkit more reliable?

--- k8-util/cluster/start-minikube-mac.sh
+++ k8-util/cluster/start-minikube-mac.sh
@@ -2,5 +2,9 @@
 set -x
 brew install minikube
 brew install hyperkit
+minikube config set memory 1024
+ls -la $(which docker-machine-driver-hyperkit)
+mkdir -p /var/db
+touch /var/db/dhcpd.leases
 minikube start --driver=hyperkit
 kubectl get nodes
