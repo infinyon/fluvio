1fcd98a81fc23642450f47812a6d0883a52f542d
Luis Moreno <morenol@users.noreply.github.com>
2021-12-02 14:24:45 -0400

    clippy

--- crates/fluvio-cluster/src/start/k8.rs
+++ crates/fluvio-cluster/src/start/k8.rs
@@ -248,26 +248,10 @@ pub struct ClusterConfig {
     /// # Ok(())
     /// # }
     /// ```
+    #[allow(dead_code)]
     #[builder(default = "true")]
     install_sys: bool,
-    /// Whether to update the `kubectl` context to match the Fluvio installation. Defaults to `true`.
-    ///
-    /// # Example
-    ///
-    /// If you do not want your Kubernetes contexts to be updated, you can do this
-    ///
-    /// ```
-    /// # use fluvio_cluster::{ClusterConfig, ClusterConfigBuilder, ClusterError};
-    /// # fn example(builder: &mut ClusterConfigBuilder) -> Result<(), ClusterError> {
-    /// let config = builder
-    ///     .update_context(false)
-    ///     .build()?;
-    /// # Ok(())
-    /// # }
-    /// ```
-    #[builder(default = "false")]
-    update_context: bool,
-    /// Whether to upgrade an existing installation
+
     #[builder(default = "false")]
     upgrade: bool,
     /// Whether to skip pre-install checks before installation. Defaults to `false`.
@@ -613,10 +597,8 @@ impl ClusterInstaller {
     /// for more details:
     ///
     /// - [`system_chart`]
-    /// - [`update_context`]
     ///
     /// [`system_chart`]: ./struct.ClusterInstaller.html#method.system_chart
-    /// [`update_context`]: ./struct.ClusterInstaller.html#method.update_context
     #[instrument(skip(self))]
     pub async fn setup(&self) -> CheckResults {
         const DISPATCHER_WAIT: &str = "FLV_DISPATCHER_WAIT";
