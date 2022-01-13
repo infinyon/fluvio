0f27d5b1646a5d46e0046ec9ca722f7af8d7e268
T.J. Telan <t.telan@gmail.com>
2022-01-12 17:14:50 -0800

    Revert "Update min and max platform versions"
    This reverts commit e7dec999a94278df3102d5c49d08d052f7d357f3.

--- crates/fluvio/src/lib.rs
+++ crates/fluvio/src/lib.rs
@@ -111,10 +111,10 @@ pub(crate) mod built_info {
 }
 
 /// The minimum VERSION of the Fluvio Platform that this client is compatible with.
-const MINIMUM_PLATFORM_VERSION: &str = "0.9.17";
+const MINIMUM_PLATFORM_VERSION: &str = "0.9.0";
 
 /// The maximum VERSION of the Fluvio Platform that this client is compatible with.
-const MAXIMUM_PLATFORM_VERSION: &str = "0.11.0";
+const MAXIMUM_PLATFORM_VERSION: &str = "0.10.0";
 
 /// Creates a producer that sends records to the named topic
 ///
