44f558505f9eba902cf9e2e77d93a4b66a77e666
carlos.canon <carlos.canon@geniussports.com>
2022-03-24 21:07:11 -0500

    create a example to use your own cloud configuration on https://infinyon.cloud/

---
+++ examples/05-produce-cloud/src/main.rs
@@ -0,0 +1,47 @@
+//! A minimal example showing how to produce messages on Fluvio
+//!
+//! Before running this example, make sure you have created a topic
+//! named `simple` with the following command:
+//!
+//! ```text
+//! $ fluvio topic create simple
+//! ```
+//!
+//! Run this example using the following:
+//!
+//! ```text
+//! $ cargo run --bin produce-cloud
+//! Sent simple record: Hello, Fluvio!
+//! ```
+//!
+//! After running this example, you can see the messages that have
+//! been sent to the topic using the following command:
+//!
+//! ```text
+//! $ fluvio consume simple -B -d
+//! Hello, Fluvio!
+//! ```
+
+use fluvio::config::ConfigFile;
+use fluvio::RecordKey;
+
+#[async_std::main]
+async fn main() {
+    if let Err(e) = produce().await {
+        println!("Produce error: {:?}", e);
+    }
+}
+
+async fn produce() -> Result<(), fluvio::FluvioError> {
+    let config = ConfigFile::load(None).unwrap();
+    let fluvio_config = config.config().cluster.get("cloud").unwrap();
+    let fluvio_connection = fluvio::Fluvio::connect_with_config(fluvio_config);
+    let producer = fluvio_connection.await?.topic_producer("simple").await?;
+
+    let value = "Hello, Fluvio!";
+    producer.send(RecordKey::NULL, value).await?;
+    producer.flush().await?;
+    println!("{}", value);
+
+    Ok(())
+}
---
+++ examples/06-consume-cloud/src/main.rs
@@ -0,0 +1,60 @@
+//! A minimal example showing how to produce messages on Fluvio
+//!
+//! This consumer will run for 3 seconds and print all of the messages
+//! that it reads during that time.
+//!
+//! Before running this example, make sure you have created a topic
+//! named `simple` with the following command:
+//!
+//! ```text
+//! $ fluvio topic create simple
+//! ```
+//!
+//! You will also need to send some messages to the topic. You can
+//! either run the `06-produce-cloud` example to send some messages,
+//! or you can use the following command:
+//!
+//! ```text
+//! $ echo "Hello, Fluvio" | fluvio produce simple
+//! ```
+
+use std::time::Duration;
+use async_std::future::timeout;
+use fluvio::config::ConfigFile;
+use fluvio::PartitionSelectionStrategy;
+
+const TIMEOUT_MS: u64 = 3_000;
+
+#[async_std::main]
+async fn main() {
+    // The consumer will run forever if we let it, so we set a timeout
+    let result = timeout(Duration::from_millis(TIMEOUT_MS), consume()).await;
+
+    match result {
+        // Success case: timeout is up, we are done consuming
+        Err(_timeout) => (),
+        // We encountered an error before having a chance to time out
+        Ok(Err(e)) => {
+            eprintln!("Consume error: {:?}", e);
+        }
+        // The consumer should run forever except for the timeout above
+        _ => unreachable!("Consumer should last forever"),
+    }
+}
+
+async fn consume() -> Result<(), fluvio::FluvioError> {
+    use futures_lite::StreamExt;
+
+    let config = ConfigFile::load(None).unwrap();
+    let fluvio_config = config.config().cluster.get("cloud").unwrap();
+    let fluvio_connection = fluvio::Fluvio::connect_with_config(fluvio_config);
+
+    let consumer = fluvio_connection.await?.consumer(PartitionSelectionStrategy::All("simple".to_string())).await?;
+    let mut stream = consumer.stream(fluvio::Offset::beginning()).await?;
+
+    while let Some(Ok(record)) = stream.next().await {
+        let string = String::from_utf8_lossy(record.value());
+        println!("{}", string);
+    }
+    Ok(())
+}
