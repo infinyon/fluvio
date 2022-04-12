5178c74e4c7831ae83526c18683a4ff10ea4428b
Tejas Ravishankar <xtremedevx@gmail.com>
2022-04-12 22:10:17 +0400

    Update crates/fluvio-cli/src/topic/list.rs
    Co-authored-by: morenol <22335041+morenol@users.noreply.github.com>

--- crates/fluvio-cli/src/topic/list.rs
+++ crates/fluvio-cli/src/topic/list.rs
@@ -107,7 +107,7 @@ mod display {
                     Row::from([
                         Cell::new(metadata.name.to_string()),
                         Cell::new(topic.type_label()),
-                        Cell::new(topic.partitions_display()),
+                        Cell::new(topic.partitions_display()).set_alignment(CellAlignment::Left);,
                         Cell::new(topic.replication_factor_display()),
                         Cell::new(format_duration(Duration::from_secs(
                             topic.retention_secs() as u64
