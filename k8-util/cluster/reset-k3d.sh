39e5930ce6db758dd0ad4e10169a76921f0980c0
Sehyo Chang <sehyo@fluvio.io>
2022-04-17 18:39:21 +0000

    update k3d to use v5.4.1 for Linux (#2315)
    Also remove other options.
    Hopefully this will increase reliability of cluster creation

--- k8-util/cluster/reset-k3d.sh
+++ k8-util/cluster/reset-k3d.sh
@@ -3,4 +3,4 @@
 # this defaults to docker and assume you have have sudo access
 set -e
 k3d cluster delete fluvio
-k3d cluster create fluvio  --no-lb --no-hostip --no-rollback
+k3d cluster create fluvio
