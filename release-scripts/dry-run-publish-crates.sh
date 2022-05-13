2e49cb2e6087166d5da2b239d5eb746db410f3e1
T.J. Telan <t.telan@gmail.com>
2022-05-12 18:54:18 -0700

    Add cargo publish dry run to ci
    Resolves #2333

---
+++ release-scripts/dry-run-publish-crates.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# Read in PUBLISH_CRATES var
+source $(dirname -- ${BASH_SOURCE[0]})/publish-list
+
+CRATES_CHECKED=0
+readonly VERBOSE=${VERBOSE:-false}
+
+function cargo_publish_dry_run_all() {
+
+    for crate in "${PUBLISH_CRATES[@]}" ; do
+        echo "$crate";
+        pushd crates/"$crate";
+
+        #cargo check
+        cargo publish --dry-run
+        result="$?";
+
+        # cargo publish exit codes:
+        if [[ "$result" != 0 ]];
+        then
+            echo "❌ $crate check failed"
+            exit 1
+        else
+            CRATES_CHECKED=$((CRATES_CHECKED+1));
+        fi
+
+        popd
+    done
+
+}
+
+function main() {
+
+    if [[ $VERBOSE != false ]];
+    then
+        echo "VERBOSE MODE ON"
+        set -x
+    fi
+
+    cargo_publish_dry_run_all;
+    
+    echo "✅ # $CRATES_CHECKED crates checked"
+}
+
+main;
