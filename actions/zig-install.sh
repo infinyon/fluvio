98d7635fbce2b4039a12ec2b4464893dbc032675
Sehyo Chang <sehyo@fluvio.io>
2022-05-18 16:16:09 +0200

    update zig_installer

--- actions/zig-install.sh
+++ actions/zig-install.sh
@@ -2,11 +2,16 @@
 # set up sccache
 set -e
 MATRIX_OS=${1}
+ZIG_VER=0.9.1
 echo "installing zig matrix.os=$MATRIX_OS"
 
 if [[ "$MATRIX_OS" == "ubuntu-latest" ]]; then
     echo "installing zig on ubuntu"
-    sudo snap install --beta --classic zig && \
+    cd /tmp
+    wget https://ziglang.org/download/$ZIG_VER/zig-linux-x86_64-$ZIG_VER.tar.xz
+    sudo mv zig-linux-x86_64-$ZIG_VER /usr/local
+    cd /usr/local/bin
+    sudo ln -s ../zig-linux-x86_64-$ZIG_VER/zig .
     sudo ${0%/*}/llvm.sh 13 && \
     echo "FLUVIO_BUILD_LLD=lld-13" | tee -a $GITHUB_ENV
 fi
@@ -19,4 +24,4 @@ if [[ "$MATRIX_OS" == "macos-11" ]]; then
 fi
 
 
-          
+        
