30f98955c94c085c321af9b73593ade33b9d1e81
Sehyo <sehyo@fluvio.io>
2022-05-18 14:53:07 +0000

    update installer

--- actions/zig-install.sh
+++ actions/zig-install.sh
@@ -3,17 +3,28 @@
 set -e
 MATRIX_OS=${1}
 ZIG_VER=0.9.1
-echo "installing zig matrix.os=$MATRIX_OS"
+LLVM_VER=13.0.1
+ARCH=x86_64
+echo "installing zig matrix.os=$MATRIX_OS version=$ZIG_VER"
 
 if [[ "$MATRIX_OS" == "ubuntu-latest" ]]; then
     echo "installing zig on ubuntu"
-    cd /tmp
-    wget https://ziglang.org/download/$ZIG_VER/zig-linux-x86_64-$ZIG_VER.tar.xz
-    sudo mv zig-linux-x86_64-$ZIG_VER /usr/local
-    cd /usr/local/bin
-    sudo ln -s ../zig-linux-x86_64-$ZIG_VER/zig .
-    sudo ${0%/*}/llvm.sh 13 && \
+    wget https://ziglang.org/download/$ZIG_VER/zig-linux-$ARCH-$ZIG_VER.tar.xz && \
+    tar -xf zig-linux-$ARCH-$ZIG_VER.tar.xz && \
+    sudo mv zig-linux-$ARCH-$ZIG_VER /usr/local && \
+    pushd /usr/local/bin && \
+    sudo ln -s ../zig-linux-$ARCH-$ZIG_VER/zig . && \
+    popd && \
+    rm zig-linux-x86_64-0.9.1.tar.* && \
+    sudo ${0%/*}/llvm.sh $LLVM_VER && \
     echo "FLUVIO_BUILD_LLD=lld-13" | tee -a $GITHUB_ENV
+fi
+
+# remove zig
+if [[ "$MATRIX_OS" == "ubuntu-cleanup" ]]; then
+    echo "removing zig"
+    sudo rm -rf /usr/local/zig-linux-$ARCH-$ZIG_VER && \
+    sudo rm -rf /usr/local/bin/zig
 fi
 
 if [[ "$MATRIX_OS" == "macos-11" ]]; then
