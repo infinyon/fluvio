92b49236483543109acb74cdcd3776e4874b1144
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-06-20 12:27:16 -0400

    Initial stuff for consumer and producer config updates to connector yamls

--- crates/fluvio-cli/src/connector/mod.rs
+++ crates/fluvio-cli/src/connector/mod.rs
@@ -1,13 +1,21 @@
 use std::sync::Arc;
 use clap::Parser;
 
-use serde::Deserialize;
+use serde::{
+    Deserializer,
+    Deserialize,
+};
+use serde::de::{self, Visitor, SeqAccess};
+use std::fmt;
 use std::collections::BTreeMap;
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::Read;
 
-use fluvio::Fluvio;
+use fluvio::{
+    Fluvio,
+    Compression,
+};
 use fluvio::metadata::connector::{ManagedConnectorSpec, SecretString};
 use fluvio_extension_common::Terminal;
 use fluvio_extension_common::COMMAND_TEMPLATE;
@@ -101,8 +109,91 @@ pub struct ConnectorConfig {
 
     #[serde(default)]
     secrets: BTreeMap<String, SecretString>,
+
+    #[serde(default)]
+    producer: Option<ProducerParameters>,
+
+    #[serde(default)]
+    consumer: Option<ConsumerParameters>,
 }
-use serde::{Deserializer};
+
+#[test]
+fn config_test() {
+    let connector_cfg = ConnectorConfig::from_file("test-data/test-config.yaml")
+        .expect("Failed to load test config");
+    println!("{:?}", connector_cfg);
+    let out: ManagedConnectorSpec = connector_cfg.into();
+    println!("{:?}", out);
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ConsumerParameters {
+    #[serde(default)]
+    partition: Option<i32>
+}
+use std::time::Duration;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ProducerParameters {
+    #[serde(with = "humantime_serde")]
+    linger: Option<Duration>,
+
+    compression: Option<Compression>,
+
+    batch_size: Option<usize>,
+}
+
+
+impl ConnectorConfig {
+    pub fn from_file<P: Into<PathBuf>>(path: P) -> Result<Self, CliError> {
+        let mut file = File::open(path.into())?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        let connector_config: Self = serde_yaml::from_str(&contents)?;
+        Ok(connector_config)
+    }
+}
+
+impl From<ConnectorConfig> for ManagedConnectorSpec {
+    fn from(config: ConnectorConfig) -> ManagedConnectorSpec {
+        let mut parameters = BTreeMap::new();
+        for (key, value) in config.parameters.iter() {
+            parameters.insert(key.clone(), value.context.clone());
+        }
+
+        // Producer arguments are prefixed with `producer`
+        if let Some(producer) = config.producer {
+            if let Some(linger) = producer.linger {
+                let linger = humantime::format_duration(linger).to_string();
+                parameters.insert("producer-linger".to_string(), vec![linger]);
+            }
+            if let Some(compression) = producer.compression {
+                let compression = format!("{:?}", compression);
+                parameters.insert("producer-compression".to_string(), vec![compression]);
+            }
+            if let Some(batch_size) = producer.batch_size {
+                let batch_size = format!("{}", batch_size);
+                parameters.insert("producer-batch-size".to_string(), vec![batch_size]);
+            }
+        }
+
+        // Consumer arguments are prefixed with `consumer`.
+        if let Some(consumer) = config.consumer {
+            if let Some(partition) = consumer.partition {
+                let partition = format!("{}", partition);
+                parameters.insert("consumer-partition".to_string(), vec![partition]);
+            }
+        }
+        ManagedConnectorSpec {
+            name: config.name,
+            type_: config.type_,
+            topic: config.topic,
+            parameters,
+            secrets: config.secrets,
+            version: config.version,
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct YamlParameter {
@@ -113,14 +204,12 @@ impl<'de> Deserialize<'de> for YamlParameter {
     where
         D: Deserializer<'de>,
     {
-		deserializer.deserialize_any(YamlParameterVisitor)
+        deserializer.deserialize_any(YamlParameterVisitor)
     }
 }
-use serde::de::{self, Visitor, SeqAccess};
 
 struct YamlParameterVisitor;
 
-    use std::fmt;
 impl<'de> Visitor<'de> for YamlParameterVisitor
 {
     type Value = YamlParameter;
@@ -139,53 +228,9 @@ impl<'de> Visitor<'de> for YamlParameterVisitor
     fn visit_seq<A>(self, seq: A) -> Result<YamlParameter, A::Error>
         where A: SeqAccess<'de>,
     {
-        Deserialize::deserialize(de::value::SeqAccessDeserializer::new(seq))
+        Ok(YamlParameter{ context: vec![] })
+
+       //Deserialize::deserialize(de::value::SeqAccessDeserializer::new(seq))
     }
 
-}
-
-use std::str::FromStr;
-impl FromStr for YamlParameter {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(YamlParameter {
-            context: vec![s.to_string()],
-        })
-    }
-}
-
-impl ConnectorConfig {
-    pub fn from_file<P: Into<PathBuf>>(path: P) -> Result<Self, CliError> {
-        let mut file = File::open(path.into())?;
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)?;
-        let connector_config: Self = serde_yaml::from_str(&contents)?;
-        Ok(connector_config)
-    }
-}
-
-impl From<ConnectorConfig> for ManagedConnectorSpec {
-    fn from(config: ConnectorConfig) -> ManagedConnectorSpec {
-        let mut parameters = BTreeMap::new();
-        for (key, value) in config.parameters.iter() {
-            parameters.insert(key.clone(), value.context.clone());
-        }
-        ManagedConnectorSpec {
-            name: config.name,
-            type_: config.type_,
-            topic: config.topic,
-            parameters,
-            secrets: config.secrets,
-            version: config.version,
-        }
-    }
-}
-
-#[test]
-fn config_test() {
-    let out: ManagedConnectorSpec = ConnectorConfig::from_file("test-data/test-config.yaml")
-        .expect("Failed to load test config")
-        .into();
-    println!("{:?}", out);
 }
