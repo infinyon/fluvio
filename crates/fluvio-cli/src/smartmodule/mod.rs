0729d8b80f6772c27b5c0c5041939d326c84c83d
Sehyo Chang <sehyo@fluvio.io>
2022-06-23 07:57:15 -0700

    remove changes related to generate

--- crates/fluvio-cli/src/smartmodule/mod.rs
+++ crates/fluvio-cli/src/smartmodule/mod.rs
@@ -7,22 +7,18 @@ use fluvio::Fluvio;
 mod create;
 mod list;
 mod delete;
-mod generate;
 mod test;
 
 use self::create::CreateSmartModuleOpt;
 use self::list::ListSmartModuleOpt;
 use self::delete::DeleteSmartModuleOpt;
-use self::generate::GenerateSmartModuleOpt;
 use self::test::TestSmartModuleOpt;
-use self::local_modules::*;
 
 #[derive(Debug, Parser)]
 pub enum SmartModuleCmd {
     Create(CreateSmartModuleOpt),
     List(ListSmartModuleOpt),
     Delete(DeleteSmartModuleOpt),
-    Generate(GenerateSmartModuleOpt),
     Test(TestSmartModuleOpt),
 }
 
@@ -38,34 +34,10 @@ impl SmartModuleCmd {
             Self::Delete(opt) => {
                 opt.process(fluvio).await?;
             }
-            Self::Generate(opt) => {
-                opt.process(fluvio).await?;
-            }
             Self::Test(opt) => {
                 opt.process(fluvio).await?;
             }
         }
         Ok(())
-    }
-}
-
-mod local_modules {
-
-    use std::path::PathBuf;
-
-    use tracing::info;
-
-    use fluvio_cli_common::install::fluvio_base_dir;
-
-    use crate::Result;
-
-    pub fn fluvio_smart_dir() -> Result<PathBuf> {
-        let base_dir = fluvio_base_dir()?;
-        let path = base_dir.join("sm");
-        if !path.exists() {
-            info!(log = ?path,"creating smart module directory");
-            std::fs::create_dir(&path)?;
-        }
-        Ok(path)
     }
 }
--- crates/fluvio-cli/src/smartmodule/test.rs
+++ crates/fluvio-cli/src/smartmodule/test.rs
@@ -4,7 +4,6 @@ use fluvio_smartengine::SmartEngine;
 use fluvio_spu_schema::server::stream_fetch::{LegacySmartModulePayload, SmartModuleWasmCompressed};
 use tracing::debug;
 use clap::Parser;
-use duct::{cmd};
 
 use fluvio::{
     Fluvio,
@@ -18,7 +17,7 @@ use fluvio::{
 };
 
 use crate::Result;
-use super::fluvio_smart_dir;
+
 
 /// Create a new SmartModule with a given name
 #[derive(Debug, Parser)]
@@ -32,33 +31,9 @@ pub struct TestSmartModuleOpt {
 
 impl TestSmartModuleOpt {
     pub async fn process(self, _fluvio: &Fluvio) -> Result<()> {
-        let sm_base_dir = fluvio_smart_dir()?;
-        let sm_dir = sm_base_dir.join(&self.name);
-        if !sm_dir.exists() {
-            println!("SmartModule {} does not exist", self.name);
-            return Ok(());
-        }
-
-        // generate a simple template
-        cmd!(
-            "cargo",
-            "build",
-            "--release",
-            "--target",
-            "wasm32-unknown-unknown"
-        )
-        .dir(&sm_dir)
-        .run()?;
-
-        // get wasm file directory
-        let wasm_file = sm_dir
-            .join("target")
-            .join("wasm32-unknown-unknown")
-            .join("release")
-            .join(format!("{}.wasm", self.name));
-        debug!(?wasm_file, "wasm file");
-        let raw = std::fs::read(wasm_file)?;
-        println!("loading {} wasm bytes", raw.len());
+        
+        // todo load from files
+        let raw = vec![];
 
         let payload = LegacySmartModulePayload {
             wasm: SmartModuleWasmCompressed::Raw(raw),
