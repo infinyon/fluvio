ef2b4eef8e8e319ef2ebd1caee1aae50a6d187d1
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-06-28 16:23:21 -0400

    Re-added producer config to connector yaml

--- crates/fluvio-cli/src/connector/mod.rs
+++ crates/fluvio-cli/src/connector/mod.rs
@@ -6,8 +6,10 @@ use std::collections::BTreeMap;
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::Read;
+use std::time::Duration;
+use bytesize::ByteSize;
 
-use fluvio::Fluvio;
+use fluvio::{Fluvio, Compression};
 use fluvio::metadata::connector::{ManagedConnectorSpec, SecretString};
 use fluvio_extension_common::Terminal;
 use fluvio_extension_common::COMMAND_TEMPLATE;
@@ -92,12 +94,46 @@ pub struct ConnectorConfig {
     name: String,
     #[serde(rename = "type")]
     type_: String,
+
     pub(crate) topic: String,
     pub(crate) version: Option<String>,
+
     #[serde(default)]
     parameters: BTreeMap<String, String>,
+
     #[serde(default)]
     secrets: BTreeMap<String, SecretString>,
+
+    #[serde(default)]
+    producer: Option<ProducerParameters>,
+
+    #[serde(default)]
+    consumer: Option<ConsumerParameters>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ConsumerParameters {
+    #[serde(default)]
+    partition: Option<i32>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ProducerParameters {
+    #[serde(with = "humantime_serde")]
+    #[serde(default)]
+    linger: Option<Duration>,
+
+    #[serde(default)]
+    compression: Option<Compression>,
+
+    // This is needed because `ByteSize` serde deserializes as bytes. We need to use the parse
+    // feature to populate `batch_size`.
+    #[serde(rename = "batch-size")]
+    #[serde(default)]
+    batch_size_string: Option<String>,
+
+    #[serde(skip)]
+    batch_size: Option<ByteSize>,
 }
 
 impl ConnectorConfig {
@@ -105,18 +141,54 @@ impl ConnectorConfig {
         let mut file = File::open(path.into())?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
-        let connector_config: Self = serde_yaml::from_str(&contents)?;
+        let mut connector_config: Self = serde_yaml::from_str(&contents)?;
+
+        // This is needed because we want to use a human readable version of `BatchSize` but the
+        // serde support for BatchSize serializes and deserializes as bytes.
+        if let Some(ref mut producer) = &mut connector_config.producer {
+            if let Some(batch_size_string) = &producer.batch_size_string {
+                let batch_size = batch_size_string
+                    .parse::<ByteSize>()
+                    .map_err(CliError::Other)?;
+                producer.batch_size = Some(batch_size);
+            }
+        }
         Ok(connector_config)
     }
 }
 
 impl From<ConnectorConfig> for ManagedConnectorSpec {
     fn from(config: ConnectorConfig) -> ManagedConnectorSpec {
+        let mut parameters = config.parameters;
+
+        // Producer arguments are prefixed with `producer`
+        if let Some(producer) = config.producer {
+            if let Some(linger) = producer.linger {
+                let linger = humantime::format_duration(linger).to_string();
+                parameters.insert("producer-linger".to_string(), linger);
+            }
+            if let Some(compression) = producer.compression {
+                let compression = format!("{:?}", compression);
+                parameters.insert("producer-compression".to_string(), compression);
+            }
+            if let Some(batch_size) = producer.batch_size {
+                let batch_size = format!("{}", batch_size);
+                parameters.insert("producer-batch-size".to_string(), batch_size);
+            }
+        }
+
+        // Consumer arguments are prefixed with `consumer`.
+        if let Some(consumer) = config.consumer {
+            if let Some(partition) = consumer.partition {
+                let partition = format!("{}", partition);
+                parameters.insert("consumer-partition".to_string(), partition);
+            }
+        }
         ManagedConnectorSpec {
             name: config.name,
             type_: config.type_,
             topic: config.topic,
-            parameters: config.parameters,
+            parameters,
             secrets: config.secrets,
             version: config.version,
         }
@@ -124,8 +196,44 @@ impl From<ConnectorConfig> for ManagedConnectorSpec {
 }
 
 #[test]
-fn config_test() {
-    let _: ManagedConnectorSpec = ConnectorConfig::from_file("test-data/test-config.yaml")
-        .expect("Failed to load test config")
-        .into();
+fn full_yaml_test() {
+    let connector_cfg = ConnectorConfig::from_file("test-data/connectors/full-config.yaml")
+        .expect("Failed to load test config");
+    let expected_params = BTreeMap::from([("param_1".to_string(), "mqtt.hsl.fi".to_string())]);
+    assert_eq!(connector_cfg.parameters, expected_params);
+    let out: ManagedConnectorSpec = connector_cfg.into();
+    let expected_params = BTreeMap::from([
+        ("consumer-partition".to_string(), "10".to_string()),
+        ("param_1".to_string(), "mqtt.hsl.fi".to_string()),
+        ("producer-batch-size".to_string(), "44.0 MB".to_string()),
+        ("producer-compression".to_string(), "Gzip".to_string()),
+        ("producer-linger".to_string(), "1ms".to_string()),
+    ]);
+    assert_eq!(out.parameters, expected_params);
+}
+
+#[test]
+fn simple_yaml_test() {
+    let connector_cfg = ConnectorConfig::from_file("test-data/connectors/simple.yaml")
+        .expect("Failed to load test config");
+    let out: ManagedConnectorSpec = connector_cfg.into();
+    let expected_params = BTreeMap::new();
+    assert_eq!(out.parameters, expected_params);
+}
+
+#[test]
+fn error_yaml_tests() {
+    let connector_cfg = ConnectorConfig::from_file("test-data/connectors/error-linger.yaml")
+        .expect_err("This yaml should error");
+    #[cfg(unix)]
+    assert_eq!("ConnectorConfig(Message(\"invalid value: string \\\"1\\\", expected a duration\", Some(Pos { marker: Marker { index: 118, line: 8, col: 10 }, path: \"producer.linger\" })))", format!("{:?}", connector_cfg));
+    let connector_cfg = ConnectorConfig::from_file("test-data/connectors/error-compression.yaml")
+        .expect_err("This yaml should error");
+    #[cfg(unix)]
+    assert_eq!("ConnectorConfig(Message(\"unknown variant `gzipaoeu`, expected one of `none`, `gzip`, `snappy`, `lz4`\", Some(Pos { marker: Marker { index: 123, line: 8, col: 15 }, path: \"producer.compression\" })))", format!("{:?}", connector_cfg));
+
+    let connector_cfg = ConnectorConfig::from_file("test-data/connectors/error-batchsize.yaml")
+        .expect_err("This yaml should error");
+    #[cfg(unix)]
+    assert_eq!("Other(\"couldn't parse \\\"aoeu\\\" into a known SI unit, couldn't parse unit of \\\"aoeu\\\"\")", format!("{:?}", connector_cfg));
 }
