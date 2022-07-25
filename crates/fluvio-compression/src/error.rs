8e019a47f6cd6ec5b90b2b01894967dc4f570e24
Alexander Galibey <alexander.galibey@infinyon.com>
2022-07-25 18:11:57 +0000

    Make produce request cloneable (#2501)
    This is needed for being able to retry produce requests if an error occurs.
    
    Related #2481

--- crates/fluvio-compression/src/error.rs
+++ crates/fluvio-compression/src/error.rs
@@ -1,3 +1,5 @@
+use bytes::buf::Writer;
+use bytes::BytesMut;
 use snap::write::{IntoInnerError, FrameEncoder};
 
 #[derive(thiserror::Error, Debug)]
@@ -7,7 +9,7 @@ pub enum CompressionError {
     #[error("unknown compression format: {0}")]
     UnknownCompressionFormat(String),
     #[error("error flushing Snap encoder: {0}")]
-    SnapError(#[from] Box<IntoInnerError<FrameEncoder<Vec<u8>>>>),
+    SnapError(#[from] Box<IntoInnerError<FrameEncoder<Writer<BytesMut>>>>),
     #[error("error flushing Snap encoder: {0}")]
     Lz4Error(#[from] lz4_flex::frame::Error),
     #[error("Unreachable error")]
