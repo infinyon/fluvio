9dd1a7edc1b9fc3a43e6aca471e07d6a7c0f3f09
Sehyo Chang <sehyo@fluvio.io>
2022-08-01 16:42:56 -0700

    update derive macro for init

--- crates/fluvio-smartmodule-derive/src/generator/init.rs
+++ crates/fluvio-smartmodule-derive/src/generator/init.rs
@@ -4,29 +4,30 @@ use crate::SmartModuleFn;
 
 // generate init smartmodule
 pub fn generate_init_smartmodule(func: &SmartModuleFn) -> TokenStream {
-    // let user_fn = &func.name;
     let user_code = func.func;
 
     quote! {
         #user_code
 
-        mod __system {
+        mod _system {
+
             #[no_mangle]
             #[allow(clippy::missing_safety_doc)]
             pub unsafe fn init(ptr: *mut u8, len: usize, version: i16) -> i32 {
-                use fluvio_smartmodule::dataplane::smartmodule::{SmartModuleInternalError, SmartModuleExtraParams};
+                use fluvio_smartmodule::dataplane::smartmodule::{
+                    SmartModuleInternalError, SmartModuleExtraParams,
+                };
                 use fluvio_smartmodule::dataplane::core::{Decoder};
-
 
                 let input_data = Vec::from_raw_parts(ptr, len, len);
                 let mut params = SmartModuleExtraParams::default();
-                if let Err(_err) = Decoder::decode(&mut params, &mut std::io::Cursor::new(input_data), version)
+                if let Err(_err) =
+                    Decoder::decode(&mut params, &mut std::io::Cursor::new(input_data), version)
                 {
                     return SmartModuleInternalError::ParsingExtraParams as i32;
                 }
 
-
-
+                super::init(params)
             }
         }
     }
--- examples/sm-regex/src/lib.rs
+++ examples/sm-regex/src/lib.rs
@@ -1,51 +1,20 @@
 use regex::Regex;
 use once_cell::sync::OnceCell;
 
-use fluvio_smartmodule::{smartmodule, Record, Result, dataplane::smartmodule::SmartModuleExtraParams};
+use fluvio_smartmodule::{
+    smartmodule, Record, Result,
+    dataplane::smartmodule::{SmartModuleExtraParams, SmartModuleInternalError},
+};
 
 static REGEX: OnceCell<Regex> = OnceCell::new();
 
-// r"^\d{4}-\d{2}-\d{2}$"
-//#[smartmodule(config)]
-/*
-pub fn init(config: SmartModuleConfig) {
-    REGEX.set(Regex::new(config).unwrap());
-}
-*/
-
-//#[smartmodule(init)]
-//pub fn init() {
-//    REGEX.set(Regex::new("[0-9]{3}-[0-9]{3}-[0-9]{4}").unwrap()).unwrap();
-//}
-
+#[smartmodule(init)]
 fn init(params: SmartModuleExtraParams) -> i32 {
     if let Some(regex) = params.get("regex") {
         REGEX.set(Regex::new(regex).unwrap()).unwrap();
         0
     } else {
-        -1
-    }
-}
-
-mod _system {
-
-    #[no_mangle]
-    #[allow(clippy::missing_safety_doc)]
-    pub unsafe fn init(ptr: *mut u8, len: usize, version: i16) -> i32 {
-        use fluvio_smartmodule::dataplane::smartmodule::{
-            SmartModuleInternalError, SmartModuleExtraParams,
-        };
-        use fluvio_smartmodule::dataplane::core::{Decoder};
-
-        let input_data = Vec::from_raw_parts(ptr, len, len);
-        let mut params = SmartModuleExtraParams::default();
-        if let Err(_err) =
-            Decoder::decode(&mut params, &mut std::io::Cursor::new(input_data), version)
-        {
-            return SmartModuleInternalError::ParsingExtraParams as i32;
-        }
-
-        super::init(params)
+        SmartModuleInternalError::InitParamsNotFound as i32
     }
 }
 
