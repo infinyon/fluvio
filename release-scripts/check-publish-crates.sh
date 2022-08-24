af6228e69c73d47d031186164ae9165eb154e345
xenago <xenago@users.noreply.github.com>
2022-08-24 17:40:42 -0400

    Add check for pinned versions

--- release-scripts/check-publish-crates.sh
+++ release-scripts/check-publish-crates.sh
@@ -13,6 +13,24 @@ function cargo_publish_dry_run_all() {
     for crate in "${PUBLISH_CRATES[@]}" ; do
         echo "$crate";
         pushd crates/"$crate";
+        
+        # Validate that dependencies with multiple locations defined have
+        # a specific version pinned, to avoid failures at publish time (#2550)
+        deps_header=0
+        while read -r line; do
+            if [[ $line =~ ^\[dependencies\]$ ]]; then
+                # In the dependencies section of Cargo.toml
+                deps_header=1
+            elif [[ $deps_header -eq 1 ]] && [[ $line =~ ^\[.*\]$ ]]; then
+                # Done checking the dependencies section of Cargo.toml
+                break
+            elif [[ $deps_header -eq 1 ]] && [[ $line =~ ^.*\{.*\}.*$ ]] && \
+                 [[ ! $line =~ ^.*version.*$ ]]; then
+                # Dependency has multiple locations defined but no version
+                echo "❌ $crate has no version pinned for a dependency: $line"
+                exit 1
+            fi
+        done < Cargo.toml
 
         # Using `cargo check` instead of `cargo publish --dry-run`
         # because dry-run will only utilize published dependencies.
