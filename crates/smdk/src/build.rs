bc637ba4cdffeca7b1d40215ef40704bc17ac874
Esteban Borai <estebanborai@gmail.com>
2022-09-30 18:47:10 -0300

    fix: pr review comments

--- crates/smdk/src/build.rs
+++ crates/smdk/src/build.rs
@@ -1,11 +1,19 @@
-use anyhow::{Error, Result};
-use clap::Parser;
 use std::process::Command;
 use std::fmt::Debug;
 
+use anyhow::{Error, Result};
+use clap::Parser;
+
+const DEFAULT_RELEASE_PROFILE: &str = "release-lto";
+const BUILD_TARGET: &str = "wasm32-unknown-unknown";
+
 /// Builds the SmartModule in the current working directory into a WASM file
 #[derive(Debug, Parser)]
-pub struct BuildOpt;
+pub struct BuildOpt {
+    /// Build release profile
+    #[clap(long, default_value = DEFAULT_RELEASE_PROFILE)]
+    release: String,
+}
 
 impl BuildOpt {
     pub(crate) fn process(&self) -> Result<()> {
@@ -17,9 +25,9 @@ impl BuildOpt {
         cmd.current_dir(&cwd)
             .arg("build")
             .arg("--profile")
-            .arg("release-lto")
+            .arg(self.release.as_str())
             .arg("--lib");
-        cmd.arg("--target").arg("wasm32-unknown-unknown");
+        cmd.arg("--target").arg(BUILD_TARGET);
 
         let status = cmd.status()?;
 
