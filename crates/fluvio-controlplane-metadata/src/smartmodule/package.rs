281eadf2b40b7b80a48a9913345c3131644b088c
Sehyo Chang <sehyo@fluvio.io>
2022-10-01 12:21:42 -0700

    add metadata

--- crates/fluvio-controlplane-metadata/src/smartmodule/package.rs
+++ crates/fluvio-controlplane-metadata/src/smartmodule/package.rs
@@ -2,11 +2,33 @@
 //! # SmartModule Package
 //!
 
-use std::io::Error as IoError;
+use std::{io::Error as IoError};
 
 use bytes::Buf;
-use fluvio_protocol::{Encoder, Decoder, Version};
 use semver::Version as SemVersion;
+
+use fluvio_protocol::{Encoder, Decoder, Version};
+
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Encoder, Decoder)]
+#[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SmartModuleMetadata {
+    pub package: SmartModulePackage,
+    pub params: Vec<SmartModuleParam>,
+}
+
+impl SmartModuleMetadata {
+    #[cfg(feature = "smartmodule")]
+    /// parse the metadata file and return the metadata
+    pub fn from_toml<T: AsRef<std::path::Path>>(path: T) -> std::io::Result<Self> {
+        use std::fs::read_to_string;
+
+        let path_ref = path.as_ref();
+        let file_str: String = read_to_string(path_ref)?;
+        let metadata = toml::from_str(&file_str)?;
+        Ok(metadata)
+    }
+}
 
 
 /// SmartModule package definition
@@ -17,13 +39,12 @@ pub struct SmartModulePackage {
     pub name: String,
     pub group: String,
     pub version: FluvioSemVersion,
-    pub edition: FluvioSemVersion,
-    pub description: String,
-    #[serde(default)]
-    pub authors: Vec<String>,
-    pub license: String,
-    pub repository: String,
+    pub apiVersion: FluvioSemVersion,
+    pub description: Option<String>,
+    pub license: Option<String>,
+    pub repository: Option<String>,
 }
+
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
@@ -62,4 +83,40 @@ impl Decoder for FluvioSemVersion {
 
 
 
+#[derive(Debug, Clone, PartialEq, Eq, Encoder, Default, Decoder)]
+#[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SmartModuleParam {
+    pub name: String,
+    pub description: Option<String>,
+    pub required: bool,
+}
+
+
+    
+
+#[cfg(test)]
+mod test {
+
+    use semver::Version;
+
+    #[test]
+    fn read_metadata_toml() {
+        let metadata = super::SmartModuleMetadata::from_toml("tests/regex.toml")
+            .expect("failed to parse metadata");
+        assert_eq!(metadata.package.name, "regex");
+        assert_eq!(metadata.package.version, Version::parse("0.1.0").unwrap());
+        assert_eq!(metadata.package.description, "Regex SmartModule");
+        assert_eq!(metadata.package.authors.len(), 1);
+        assert_eq!(metadata.package.authors[0], "Infinyon");
+        assert_eq!(metadata.package.package_version, "0.1");
+        assert_eq!(metadata.package.license, "Apache-2.0");
+        assert_eq!(
+            metadata.package.repository,
+            "https://github.com/infinyon/fluvio"
+        );
+        let init_param = metadata.init;
+        assert_eq!(init_param.len(), 1);
+        assert_eq!(init_param["regex"].input, super::InitType::String);
+    }
+}
 
