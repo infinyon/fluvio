f5e3eb91b10620184b8efd915eaa709e8d5b898b
T.J. Telan <t.telan@gmail.com>
2022-10-05 14:45:44 -0700

    Simplify cd_dev check

--- makefiles/release.mk
+++ makefiles/release.mk
@@ -189,20 +189,16 @@ update-public-installer-script-s3:
 	$(DRY_RUN_ECHO) aws s3 cp ./install.sh s3://packages.fluvio.io/v1/install.sh --acl public-read
 
 latest-cd-dev-status:
-	rm --verbose --force /tmp/cd_dev_latest.txt
-#	gh api /repos/infinyon/fluvio/actions/workflows/cd_dev.yaml/runs | jq .workflow_runs[0] > /tmp/cd_dev_latest.txt
-	gh api /repos/{owner}/{repo}/actions/workflows/cd_dev.yaml/runs | jq .workflow_runs[0] | tee /tmp/cd_dev_latest.txt
-	echo "Latest CD_Dev run: $(shell touch /tmp/cd_dev_latest.txt; cat /tmp/cd_dev_latest.txt | jq .html_url | tr -d '"')"
-	echo Conclusion: $(shell touch /tmp/cd_dev_latest.txt; cat /tmp/cd_dev_latest.txt | jq .conclusion)
-ifeq ($(shell touch /tmp/cd_dev_latest.txt; cat /tmp/cd_dev_latest.txt | jq .conclusion | tr -d '"'), success)
-	echo $(shell echo Success branch; cat /tmp/cd_dev_latest.txt)
-	echo $(shell echo ✅ Most recent CD_Dev run passed)
-	exit 0;
-else
-	echo $(shell echo Failed branch; cat /tmp/cd_dev_latest.txt)
-	echo $(shell echo ❌ Most recent CD_Dev run failed)
-	exit 1;
-endif
+	gh api /repos/{owner}/{repo}/actions/workflows/cd_dev.yaml/runs | jq .workflow_runs[0] > /tmp/cd_dev_latest.txt
+	@echo "Latest CD_Dev run: $$( cat /tmp/cd_dev_latest.txt | jq .html_url | tr -d '"')"
+
+	@if [ $$(cat /tmp/cd_dev_latest.txt | jq .conclusion | tr -d '"') = success ]; then \
+		echo ✅ Most recent CD_Dev run passed; \
+		exit 0; \
+	else \
+		echo ❌ Most recent CD_Dev run failed; \
+		exit 1; \
+	fi
 
 build-release-notes:
 	rm --verbose --force /tmp/release_notes
