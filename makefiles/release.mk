7dbad8d620a68bf2cfea29f63c3732d3cc8ef9b5
T.J. Telan <t.telan@gmail.com>
2022-10-04 20:39:08 -0700

    Attempting to call publish workflow from ci

--- makefiles/release.mk
+++ makefiles/release.mk
@@ -1,5 +1,5 @@
 # Use the binary name produced by cargo
-PUBLISH_BINARIES=fluvio-test
+PUBLISH_BINARIES=fluvio fluvio-run fluvio-channel
 #PUBLISH_BINARIES=fluvio fluvio-run fluvio-channel fluvio-test smdk
 
 # CI has to set RELEASE=true to run commands that update public
@@ -53,6 +53,9 @@ TARGET?=
 PACKAGE?=
 ARTIFACT?=
 
+# Requires GH_TOKEN set
+DOWNLOAD_FLUVIO_RELEASE_CMD = gh release download $(GH_RELEASE_TAG) -R infinyon/fluvio --skip-existing
+
 ## TODO: Remember to remove `echo` from calls before merging
 
 #### Testing only
@@ -79,7 +82,6 @@ fix-latest-channel:
 # Login to Docker Hub
 docker-hub-login:
 	$(DRY_RUN_ECHO) docker login --username=$(DOCKER_USERNAME) --password=$(DOCKER_PASSWORD)
-#	docker login --username=$(DOCKER_USERNAME) --password=$(DOCKER_PASSWORD)
 
 docker-hub-check-image-exists:
 	$(info Checking on if infinyon/fluvio:$(DOCKER_IMAGE_TAG) exists in Docker Hub);
@@ -90,7 +92,6 @@ docker-hub-check-image-exists:
 	$(shell echo false > /tmp/fluvio_image_exists);
 	$(error Image tag does not exist);
 	endif
-
 
 # Get Fluvio VERSION from Github, provided a given git SHA
 docker-create-manifest: docker-hub-login
@@ -104,24 +105,14 @@ docker-push-manifest: docker-create-manifest-dev
 # Create latest development Fluvio image
 docker-create-manifest-dev: DOCKER_IMAGE_TAG=latest
 docker-create-manifest-dev: docker-hub-login
-#	$(DRY_RUN_ECHO) docker manifest create "docker.io/infinyon/fluvio:latest" \
-#		"docker.io/infinyon/fluvio:$(DEV_VERSION_TAG)-amd64" \
-#		"docker.io/infinyon/fluvio:$(DEV_VERSION_TAG)-arm64v8"
 
 # Push docker manifest
 docker-push-manifest-dev: DOCKER_IMAGE_TAG=$(DEV_VERSION_TAG)
 docker-push-manifest-dev: docker-create-manifest-dev
-#	$(DRY_RUN_ECHO) docker manifest push "docker.io/infinyon/fluvio:$(DEV_VERSION_TAG)"
-
-
-# Expects GH_TOKEN env var to have token
-#gh-cli-login-token:
-#	gh auth login --with-token
 
 # Uses $(VERSION)
 curl-install-fluvio:
 	curl -fsS https://packages.fluvio.io/v1/install.sh | bash
-
 
 install-fluvio-stable: VERSION=stable
 install-fluvio-stable: curl-install-fluvio
@@ -129,19 +120,10 @@ install-fluvio-stable: curl-install-fluvio
 install-fluvio-latest: VERSION=latest
 install-fluvio-latest: curl-install-fluvio
 
-# Tag images
-
-
-# Target that logs in to GH CLI. Assuming a token is provided, unless explicitly skipped
-
 # Target that install fluvio-package
 install-fluvio-package:
 	$(FLUVIO_BIN) install fluvio-package
 
-# Target that downloads the GH dev release
-# Requires GH_TOKEN set
-DOWNLOAD_FLUVIO_RELEASE_CMD = gh release download $(GH_RELEASE_TAG) -R infinyon/fluvio --skip-existing
-# Obviously don't commit this
 download-fluvio-release:
 	$(call DOWNLOAD_FLUVIO_RELEASE_CMD)
 
@@ -178,12 +160,6 @@ unzip-gh-release-artifacts: download-fluvio-release
 		) \
 	)
 
-#		$(shell \
-#			unzip $f -d $(basename $f); \
-#			cat $(basename $f)/.target
-#		) \
-#	)
-
 publish-artifacts: unzip-gh-release-artifacts
 	echo "package stuff"
 	$(foreach bin, $(wildcard *.zip), \
@@ -201,15 +177,7 @@ publish-artifacts: unzip-gh-release-artifacts
 publish-artifacts-dev: GH_RELEASE_TAG=dev
 publish-artifacts-dev: publish-artifacts
 
-#	for GH_ARTIFACT in *.zip; do \
-#		echo filename: $(basename $$GH_ARTIFACT); \
-#	done
-
-# Moved this from cargo-make. It was called "bump-fluvio-latest" and only run in CI
-
 # Version convention is different here. Notice the `+`
-#bump-fluvio-latest: DEV_VERSION_TAG=$(VERSION)+$(GIT_COMMIT_SHA)
-
 bump-fluvio: PUBLIC_VERSION=$(subst -,+,$(VERSION))
 bump-fluvio:
 	@$(DRY_RUN_ECHO) $(FLUVIO_BIN) package publish bump $(CHANNEL_TAG) $(PUBLIC_VERSION)
@@ -219,30 +187,10 @@ bump-fluvio:
 
 bump-fluvio-latest: VERSION=$(subst -, +, $(DEV_VERSION_TAG))
 bump-fluvio-latest: install-fluvio-latest install-fluvio-package bump-fluvio
-#	$(DRY_RUN_ECHO) $(FLUVIO_BIN) package publish bump latest $(DEV_VERSION_TAG_PLUS)
-#	$(DRY_RUN_ECHO) $(foreach bin, $(PUBLISH_BINARIES), \
-#		$(shell $(DRY_RUN_ECHO) $(FLUVIO_BIN) package tag $(bin):$(DEV_VERSION_TAG_PLUS) --allow-missing-targets --tag=latest --force) \
-#	)
-#	$(FLUVIO_BIN) package publish tag  fluvio:$(TAG) --tag=latest --force
-#	$(FLUVIO_BIN) package publish tag  fluvio-run:$(TAG) --allow-missing-targets --tag=latest --force
-#	$(FLUVIO_BIN) package publish tag  fluvio-channel:$(TAG) --allow-missing-targets --tag=latest --force
-#	$(FLUVIO_BIN) package publish tag  fluvio-test:$(TAG) --allow-missing-targets --tag=latest --force
-#	$(FLUVIO_BIN) package publish tag  smdk:$(TAG) --allow-missing-targets --tag=latest --force
-#${HOME}/.fluvio/extensions/fluvio-package ${FLUVIO_PUBLISH_TEST} \
-#    bump latest "$(cat VERSION)+$(git rev-parse HEAD)"
-#
-#${HOME}/.fluvio/extensions/fluvio-package ${FLUVIO_PUBLISH_TEST} \
-#    tag "fluvio:$(cat VERSION)+$(git rev-parse HEAD)" --tag=latest --force
-#${HOME}/.fluvio/extensions/fluvio-package ${FLUVIO_PUBLISH_TEST} \
-#    tag "fluvio-run:$(cat VERSION)+$(git rev-parse HEAD)" --allow-missing-targets --tag=latest --force
-#${HOME}/.fluvio/extensions/fluvio-package ${FLUVIO_PUBLISH_TEST} \
-#    tag "fluvio-channel:$(cat VERSION)+$(git rev-parse HEAD)" --allow-missing-targets --tag=latest --force
 
 update-public-installer-script-s3:
 	$(DRY_RUN_ECHO) aws s3 cp ./install.sh s3://packages.fluvio.io/v1/install.sh --acl public-read
 
-
-# Obviously don't commit this
 latest-cd-dev-status:
 	gh api /repos/{owner}/{repo}/actions/workflows/cd_dev.yaml/runs | jq .workflow_runs[0] > /tmp/cd_dev_latest.txt
 	echo "Latest CD_Dev run: $(shell cat /tmp/cd_dev_latest.txt | jq .html_url | tr -d '"')"
