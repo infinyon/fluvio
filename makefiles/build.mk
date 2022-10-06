b2879de0177c7c4c00fae3f7eef7dc73643f4424
T.J. Telan <t.telan@gmail.com>
2022-10-06 15:30:16 -0700

    Update name inside Cargo.toml too

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -3,7 +3,7 @@ build-cli: install_rustup_target
 	$(CARGO_BUILDER) build --bin fluvio -p fluvio-cli $(RELEASE_FLAG) $(TARGET_FLAG) $(VERBOSE_FLAG) $(SMARTENGINE_FLAG)
 
 build-smdk: install_rustup_target
-	$(CARGO_BUILDER) build --bin smdk -p smart-module-development-kit $(RELEASE_FLAG) $(TARGET_FLAG) $(VERBOSE_FLAG) $(SMARTENGINE_FLAG)
+	$(CARGO_BUILDER) build --bin smdk -p smartmodule-development-kit $(RELEASE_FLAG) $(TARGET_FLAG) $(VERBOSE_FLAG) $(SMARTENGINE_FLAG)
 
 build-cli-minimal: install_rustup_target
 	# https://github.com/infinyon/fluvio/issues/1255
