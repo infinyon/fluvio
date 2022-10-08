d4169811d161a2059358f3f616629fd16ebff027
Alan Chen <argot@digikata.com>
2022-10-07 19:53:41 -0700

    smdk publish, look for smartmodule toml to init hub pkgfiles

--- crates/smartmodule-development-kit/src/publish.rs
+++ crates/smartmodule-development-kit/src/publish.rs
@@ -1,10 +1,13 @@
-use clap::Parser;
+
+use std::{path::Path};
+
 use anyhow::Result;
+use clap::Parser;
 
 use crate::set_hubid::get_hubaccess;
 
 use fluvio_hub_util as hubutil;
-use hubutil::HubAccess;
+use hubutil::{DEF_HUB_INIT_DIR, HubAccess, PackageMeta};
 
 use fluvio_future::task::run_block_on;
 
@@ -26,6 +29,11 @@ pub struct PublishOpt {
 impl PublishOpt {
     pub(crate) fn process(&self) -> Result<()> {
         let access = get_hubaccess()?;
+
+        let hubdir = Path::new(DEF_HUB_INIT_DIR);
+        if !hubdir.exists() {
+            init_package_template()?;
+        }
 
         match (self.pack, self.push) {
             (false, false) | (true, true) => {
@@ -61,6 +69,7 @@ impl PublishOpt {
 }
 
 pub fn package_assemble(pkgmeta: &str, access: &HubAccess) -> Result<String> {
+
     let pkgname = hubutil::package_assemble_and_sign(pkgmeta, access, None)?;
     println!("Package {} created", pkgname);
     Ok(pkgname)
@@ -72,4 +81,60 @@ pub fn package_push(pkgpath: &str, access: &HubAccess) -> Result<()> {
         std::process::exit(1);
     }
     Ok(())
+}
+pub fn init_package_template() -> Result<()> {
+    // fill out template w/ defaults
+    let pmetapath = hubutil::DEF_HUB_PKG_META;
+
+    let mut pm = PackageMeta {
+        group: "no-hubid".into(),
+        name: "not-found".into(),
+        version: "0.0.0".into(),
+        manifest: Vec::new(),
+        ..PackageMeta::default()
+    };
+    let sm_toml_file = find_smartmodule_toml()?;
+    pm.update_from_smartmodule_toml(&sm_toml_file)?;
+
+    println!("Creating package {}/{}", &pm.name, &pm.version);
+    hubutil::packagename_validate(&pm.name)?;
+
+    let wasmout = hubutil::packagename_transform(&pm.name)? + ".wasm";
+    let wasmpath = format!("target/wasm32-unknown-unknown/release/{wasmout}");
+    pm.manifest.push(wasmpath);
+
+    // create directoy name pkgname
+    let pkgdir = Path::new(hubutil::DEF_HUB_INIT_DIR);
+    if pkgdir.exists() {
+        return Err(anyhow::anyhow!("package hub directory exists already"));
+    }
+    std::fs::create_dir(pkgdir)?;
+    pm.write(&pmetapath)?;
+
+    println!(".. fill out info in {pmetapath}");
+    Ok(())
+}
+
+fn find_smartmodule_toml() -> Result<String> {
+    let prjdir = if let Ok(d) = std::fs::read_dir("./") {
+        d
+    } else {
+        return Err(anyhow::anyhow!("couldn't read in directory"));
+    };
+    let cargo_toml = Path::new("Cargo.toml");
+    for ent in prjdir {
+        let ent = ent?;
+        let fp = ent.path();
+        if fp == cargo_toml {
+            continue;
+        }
+        if let Some(ext) = fp.extension() {
+            if ext != "toml" {
+                continue;
+            }
+            let fpstr = fp.to_string_lossy().to_string();
+            return Ok(fpstr)
+        }
+    }
+    Err(anyhow::anyhow!("No smartmodule toml file found"))
 }
