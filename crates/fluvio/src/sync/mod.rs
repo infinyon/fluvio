3aaec6daf4bdde84fd53703713d3621e15d6ad04
Dave Beesley <dave.beesley@dart.software>
2022-10-12 06:36:46 -0700

    short circuit wait for metadata

--- crates/fluvio/src/sync/mod.rs
+++ crates/fluvio/src/sync/mod.rs
@@ -108,6 +108,12 @@ mod context {
             use tokio::select;
             use fluvio_future::timer::sleep;
 
+            // We can short circuit here if already present
+            let found = search(self.store().read().await);
+            if found.is_some() {
+                return Ok(found);
+            }
+
             let mut timer = sleep(Duration::from_millis(*MAX_WAIT_TIME));
             let mut listener = self.store.change_listener();
 
