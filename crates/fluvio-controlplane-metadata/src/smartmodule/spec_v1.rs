f9323b8ba82292146afb9556e726bd2d7bb6d576
Sehyo Chang <sehyo@fluvio.io>
2022-10-14 16:12:19 -0700

    bring back missing attributes in SmartModuleSpec

--- crates/fluvio-controlplane-metadata/src/smartmodule/spec_v1.rs
+++ crates/fluvio-controlplane-metadata/src/smartmodule/spec_v1.rs
@@ -7,8 +7,23 @@ use super::spec::SmartModuleWasm;
 pub struct SmartModuleSpecV1 {
     pub input_kind: SmartModuleInputKind,
     pub output_kind: SmartModuleOutputKind,
+    pub source_code: Option<SmartModuleSourceCode>,
     pub wasm: SmartModuleWasm,
     pub parameters: Option<Vec<SmartModuleParameter>>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Encoder, Decoder)]
+#[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SmartModuleSourceCode {
+    language: SmartModuleSourceCodeLanguage,
+    payload: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Encoder, Decoder, Default)]
+#[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum SmartModuleSourceCodeLanguage {
+    #[default]
+    Rust,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Encoder, Decoder, Default)]
