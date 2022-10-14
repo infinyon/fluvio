02265e2bfbc78023237632cfe01af8ab682d42e9
Sehyo Chang <sehyo@fluvio.io>
2022-10-13 22:04:46 -0700

    update

---
+++ crates/fluvio-controlplane-metadata/src/smartmodule/spec_v1.rs
@@ -0,0 +1,41 @@
+use fluvio_protocol::{Encoder, Decoder};
+
+use super::spec::SmartModuleWasm;
+
+#[derive(Debug, Default, Clone, Eq, PartialEq, Encoder, Decoder)]
+#[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SmartModuleSpecV1 {
+    pub input_kind: SmartModuleInputKind,
+    pub output_kind: SmartModuleOutputKind,
+    pub wasm: SmartModuleWasm,
+    pub parameters: Option<Vec<SmartModuleParameter>>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Encoder, Decoder, Default)]
+#[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum SmartModuleInputKind {
+    #[default]
+    Stream,
+    External,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Encoder, Decoder, Default)]
+#[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum SmartModuleOutputKind {
+    #[default]
+    Stream,
+    External,
+    Table,
+}
+
+impl std::fmt::Display for SmartModuleSpecV1 {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "SmartModuleSpec")
+    }
+}
+
+#[derive(Debug, Clone, Default, Eq, PartialEq, Encoder, Decoder)]
+#[cfg_attr(feature = "use_serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SmartModuleParameter {
+    name: String,
+}
