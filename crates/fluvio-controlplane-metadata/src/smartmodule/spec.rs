a4ee57d0ab40b24eb2a163a3f3f93e75f491e31f
Esteban Borai <estebanborai@gmail.com>
2022-10-22 02:33:00 -0300

    chore: unit testing

--- crates/fluvio-controlplane-metadata/src/smartmodule/spec.rs
+++ crates/fluvio-controlplane-metadata/src/smartmodule/spec.rs
@@ -108,29 +108,29 @@ pub struct SmartModuleWasmSummary {
 
 pub struct WasmBytes(Vec<u8>);
 
+impl From<Vec<u8>> for WasmBytes {
+    fn from(bytes: Vec<u8>) -> Self {
+        Self(bytes)
+    }
+}
+
 impl Encoder for WasmBytes {
-    fn write_size(&self, version: Version) -> usize {
-        self.0
-            .iter()
-            .fold(4, |sum, val| sum + val.write_size(version))
+    fn write_size(&self, _version: Version) -> usize {
+        1
     }
 
     fn encode<T>(&self, dest: &mut T, _version: Version) -> Result<(), IoError>
     where
         T: BufMut,
     {
-        if dest.remaining_mut() < 4 {
+        if dest.remaining_mut() < 1 {
             return Err(IoError::new(
                 ErrorKind::UnexpectedEof,
                 "Not enough capacity for WasmBytes",
             ));
         }
 
-        dest.put_u32(self.0.len() as u32);
-
-        for v in &self.0 {
-            dest.put_u8(*v);
-        }
+        dest.put_slice(self.0.as_slice());
 
         Ok(())
     }
@@ -209,5 +209,28 @@ mod base64 {
     pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<u8>, D::Error> {
         let base64 = String::deserialize(d)?;
         base64::decode(base64.as_bytes()).map_err(serde::de::Error::custom)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fluvio_protocol::Encoder;
+
+    use crate::smartmodule::WasmBytes;
+
+    #[test]
+    fn test_encode_wasmbytes() {
+        let mut dest = Vec::default();
+        let value: WasmBytes = WasmBytes::from(vec![12, 128, 255, 78, 9]);
+        let result = value.encode(&mut dest, 0);
+
+        assert!(result.is_ok());
+        assert_eq!(dest.len(), 5);
+        assert_eq!(dest[0], 0x0C);
+        assert_eq!(dest[1], 0x80);
+        assert_eq!(dest[2], 0xFF);
+        assert_eq!(dest[3], 0x4E);
+        assert_eq!(dest[4], 0x09);
+        assert_eq!(value.write_size(0), 1);
     }
 }
