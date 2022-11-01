b5e52ce34d276320efd10c109d2eab4b2ed02e13
Esteban Borai <estebanborai@gmail.com>
2022-11-01 23:20:24 +0000

    feat: setup benchmark action (#2764)
    Setups Benchmarks as part of the CI workflow using the [benchmark-action][1].
    Benchmarks will be executed after running tests with the `local_cluster_test`
    action.
    
    Previous benchmarks are stored using `actions/cache` action with the goal of
    comparing them on every change to measure improvements on performance.
    
    [1]: https://github.com/benchmark-action/github-action-benchmark

---
+++ crates/fluvio-protocol/benches/bench.rs
@@ -0,0 +1,47 @@
+#![feature(test)]
+
+extern crate test;
+
+#[cfg(test)]
+mod bench {
+    use std::io::Cursor;
+
+    use fluvio_protocol::{Decoder, Encoder};
+    use test::{Bencher, black_box};
+
+    #[bench]
+    fn bench_encode_vecu8(b: &mut Bencher) {
+        let value: Vec<u8> = vec![0x10, 0x11, 0x12, 0x10, 0x11, 0x12, 0x10, 0x11];
+
+        #[allow(unused_must_use)]
+        b.iter(|| {
+            for _i in 1..100 {
+                let mut dest = vec![];
+
+                black_box(|| {
+                    value.encode(&mut dest, 0).unwrap();
+                });
+            }
+        });
+    }
+
+    #[bench]
+    fn bench_decode_vecu8(b: &mut Bencher) {
+        let encoded_expect: [u8; 14] = [
+            0x00, 0x00, 0x00, 0x0A, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
+        ];
+
+        #[allow(unused_must_use)]
+        b.iter(|| {
+            for _i in 1..100 {
+                let mut decoded_vecu8: Vec<u8> = vec![];
+
+                black_box(|| {
+                    decoded_vecu8
+                        .decode(&mut Cursor::new(&encoded_expect), 0)
+                        .unwrap();
+                });
+            }
+        });
+    }
+}
