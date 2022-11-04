b8f77a585b4ac4b72ab3523883ffbe6dedc2da59
Esteban Borai <estebanborai@gmail.com>
2022-11-03 22:15:56 -0300

    feat: update benchmarks using criterion

--- crates/fluvio-protocol/benches/bench.rs
+++ crates/fluvio-protocol/benches/bench.rs
@@ -1,35 +1,74 @@
+use std::fs::read;
 use std::io::Cursor;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use fluvio_protocol::{Decoder, Encoder};
+use fluvio_protocol::{Decoder, Encoder, ByteBuf};
+
+const EXAMPLE_WASM_FILE: &str = "fixtures/smartmodule.wasm";
 
 fn bench_encode_vecu8(c: &mut Criterion) {
-    let value: Vec<u8> = vec![0x10, 0x11, 0x12, 0x10, 0x11, 0x12, 0x10, 0x11];
+    let bytes = read(EXAMPLE_WASM_FILE).unwrap();
     let mut dest = vec![];
 
-    c.bench_function("encode vecu8", |b| {
+    c.bench_function("vecu8 encoding", |b| {
         b.iter(|| {
-            value.encode(&mut dest, 0).unwrap();
+            bytes.encode(&mut dest, 0).unwrap();
         })
     });
 }
 
 fn bench_decode_vecu8(c: &mut Criterion) {
-    let encoded_expect: [u8; 14] = [
-        0x00, 0x00, 0x00, 0x0A, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
-    ];
+    let bytes = read(EXAMPLE_WASM_FILE).unwrap();
+    let mut encoded = vec![];
 
-    c.bench_function("decode vecu8", |b| {
+    bytes.encode(&mut encoded, 0).unwrap();
+
+    c.bench_function("vecu8 decoding", |b| {
         b.iter(|| {
             let mut decoded_vecu8: Vec<u8> = vec![];
+            let mut cursor = Cursor::new(&encoded);
 
-            decoded_vecu8
-                .decode(&mut Cursor::new(&encoded_expect), 0)
-                .unwrap();
+            decoded_vecu8.decode(&mut cursor, 0).unwrap();
         })
     });
 }
 
-criterion_group!(benches, bench_encode_vecu8, bench_decode_vecu8);
+fn bench_encode_bytebuf(c: &mut Criterion) {
+    let bytes = read(EXAMPLE_WASM_FILE).unwrap();
+    let bytebuf: ByteBuf = ByteBuf::from(bytes.clone());
+
+    c.bench_function("bytebuf encoding", |b| {
+        b.iter(|| {
+            let mut dest = vec![];
+
+            bytebuf.encode(&mut dest, 0).unwrap();
+        })
+    });
+}
+
+fn bench_decode_bytebuf(c: &mut Criterion) {
+    let mut encoded: Vec<u8> = vec![];
+    let raw: Vec<u8> = read(EXAMPLE_WASM_FILE).unwrap();
+    let bytebuf: ByteBuf = ByteBuf::from(raw.to_vec());
+
+    bytebuf.encode(&mut encoded, 0).unwrap();
+
+    c.bench_function("bytebuf decoding", |b| {
+        b.iter(|| {
+            let mut decoded_bytebuf: ByteBuf = ByteBuf::default();
+            let mut cursor = Cursor::new(&encoded);
+
+            decoded_bytebuf.decode(&mut cursor, 0).unwrap();
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    bench_encode_vecu8,
+    bench_decode_vecu8,
+    bench_encode_bytebuf,
+    bench_decode_bytebuf
+);
 criterion_main!(benches);
