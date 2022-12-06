2bb0fddac04bf431d8eae1273508d1c38879e04e
Sehyo Chang <sehyo@fluvio.io>
2022-12-05 23:09:12 -0800

    add fluvio connector pkg to deploy

--- crates/fluvio-connector-deployer/src/lib.rs
+++ crates/fluvio-connector-deployer/src/lib.rs
@@ -1,22 +1,43 @@
+use std::path::PathBuf;
+use std::process::Command;
 
+use anyhow::Result;
 use derive_builder::Builder;
 
+use fluvio_connector_package::config::ConnectorConfig;
+use fluvio_connector_package::metadata::ConnectorMetadata;
 
-pub enum Executable {
-    Binary,
-    Image(String),
-}
-
+#[derive(Clone)]
 pub struct Secret(String);
 
+#[derive(Clone)]
+pub enum DeploymentType {
+    Local,
+    K8,
+}
 
 /// Describe deployment configuration
 #[derive(Builder)]
 pub struct Deployment {
-     pub executable: dyn Executable,   // Executable to deploy, this can be binary, image, etc.
-	 pub secrets: Vec<Secret>,          // List of Secrets
-     pub config: ConnectorConfig,      // Configuration to pass along,	
-     pub pkg:  ConnectorPkg             // Connector pkg definition
+    pub executable: PathBuf,             // path to executable
+    pub secrets: Vec<Secret>,            // List of Secrets
+    pub config: ConnectorConfig,         // Configuration to pass along,
+    pub pkg: ConnectorMetadata,          // Connector pkg definition
+    pub deployment_type: DeploymentType, // deployment type
 }
 
+impl DeploymentBuilder {
+    pub fn deploy(self) -> Result<()> {
+        let deployment = self.build()?;
+        match deployment.deployment_type {
+            DeploymentType::Local => {
+                let mut cmd = Command::new(deployment.executable);
 
+                cmd.spawn()?;
+            }
+            DeploymentType::K8 => {}
+        }
+
+        Ok(())
+    }
+}
