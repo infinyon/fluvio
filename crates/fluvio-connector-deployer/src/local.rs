9066efbbda23a85b7576b5619599fc9b584f5eec
Alexander Galibey <alexander.galibey@infinyon.com>
2022-12-07 15:35:20 -0300

    send connectors logs to a file

--- crates/fluvio-connector-deployer/src/local.rs
+++ crates/fluvio-connector-deployer/src/local.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 use anyhow::{Result, anyhow};
 use tempfile::NamedTempFile;
@@ -8,13 +8,25 @@ pub(crate) fn deploy_local(deployment: &Deployment) -> Result<()> {
     let (_, config_path) = NamedTempFile::new()?.keep()?;
     deployment.config.write_to_file(&config_path)?;
 
+    let mut log_path = std::env::current_dir()?;
+    log_path.push(&deployment.config.name);
+    log_path.set_extension("log");
+    let log_file = std::fs::File::create(log_path.as_path())?;
+
     let mut cmd = Command::new(&deployment.executable);
+    cmd.stdin(Stdio::null());
+    cmd.stdout(log_file);
     cmd.arg("--config");
     cmd.arg(
         config_path
             .to_str()
             .ok_or_else(|| anyhow!("illegal path of temp config file"))?,
     );
-    cmd.spawn()?;
+    let child = cmd.spawn()?;
+    println!(
+        "Connector runs with process id: {}. Log file: {}",
+        child.id(),
+        log_path.to_string_lossy()
+    );
     Ok(())
 }
