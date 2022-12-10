c3676a6dd8ecd7550b7587bc190bfd69c4e3ffd9
Alexander Galibey <alexander.galibey@infinyon.com>
2022-12-10 03:52:03 +0000

    fix: measure outbound metrics after SmartEngine (#2865)
    Fixes #2864

--- crates/fluvio-spu/src/core/metrics.rs
+++ crates/fluvio-spu/src/core/metrics.rs
@@ -1,8 +1,11 @@
-use std::sync::{
-    atomic::{AtomicU64, Ordering},
+use std::{
+    sync::atomic::{AtomicU64, Ordering},
+    ops::AddAssign,
 };
 
+use fluvio_protocol::record::Batch;
 use fluvio_smartengine::metrics::SmartModuleChainMetrics;
+use fluvio_spu_schema::fetch::FilePartitionResponse;
 use serde::Serialize;
 
 #[derive(Default, Debug, Serialize)]
@@ -21,36 +24,6 @@ impl SpuMetrics {
         &self.smartmodule
     }
 }
-
-/*
-pub(crate) struct SpuMetricsTopicPartition<'a> {
-    metrics: &'a SpuMetrics,
-    _topic: &'a str,
-    _partition: i32,
-}
-
-impl<'a> SpuMetricsTopicPartition<'a> {
-    pub(crate) fn increase(&self,records: u64,bytes: u64)
-        self.metrics.records_read.fetch_add(value, Ordering::SeqCst);
-    }
-
-    pub(crate) fn add_bytes_read(&self, value: u64) {
-        self.metrics.bytes_read.fetch_add(value, Ordering::SeqCst);
-    }
-
-    pub(crate) fn increase_inbound_records(&self, value: u64) {
-        self.metrics
-            .records_write
-            .fetch_add(value, Ordering::SeqCst);
-    }
-
-    pub(crate) fn increase_inbound_bytes(&self, value: u64) {
-        self.metrics
-            .bytes_written
-            .fetch_add(value, Ordering::SeqCst);
-    }
-}
-*/
 
 #[derive(Default, Debug, Serialize)]
 pub(crate) struct Record {
@@ -71,6 +44,12 @@ pub(crate) struct Activity {
     client: Record,
 }
 
+#[derive(Default, Debug)]
+pub(crate) struct IncreaseValue {
+    records: u64,
+    bytes: u64,
+}
+
 impl Activity {
     pub(crate) fn increase(&self, connector: bool, records: u64, bytes: u64) {
         if connector {
@@ -78,5 +57,92 @@ impl Activity {
         } else {
             self.client.increase(records, bytes);
         }
+    }
+
+    pub(crate) fn increase_by_value(&self, connector: bool, value: IncreaseValue) {
+        let IncreaseValue { records, bytes } = value;
+        self.increase(connector, records, bytes)
+    }
+}
+
+impl IncreaseValue {
+    pub(crate) fn new(records: u64, bytes: u64) -> Self {
+        Self { records, bytes }
+    }
+}
+
+// Measuring of serialized data. `bytes` is length of file slice, `records` is an offset's change
+impl From<&FilePartitionResponse> for IncreaseValue {
+    fn from(resp: &FilePartitionResponse) -> Self {
+        Self::new(
+            (resp.high_watermark - resp.log_start_offset) as u64,
+            resp.records.len() as u64,
+        )
+    }
+}
+
+// Measuring of deserialized data. `bytes` is a sum of key size and value size of every record, `records` is records size
+impl From<&Batch> for IncreaseValue {
+    fn from(batch: &Batch) -> Self {
+        let records = batch.records().len();
+        let mut bytes = 0;
+        for record in batch.records() {
+            bytes.add_assign(record.key().map_or(0, |k| k.len()));
+            bytes.add_assign(record.value.len());
+        }
+        Self::new(records as u64, bytes as u64)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use fluvio::RecordKey;
+    use fluvio_future::file_slice::AsyncFileSlice;
+    use fluvio_protocol::record::Record;
+    use fluvio_protocol::record::RecordData;
+    use fluvio_spu_schema::file::FileRecordSet;
+
+    #[test]
+    fn test_increase_from_batch() {
+        //given
+        let record1_value = [1; 10];
+        let record2_value = [2; 11];
+        let record2_key = [3; 12];
+        let batch = Batch::from(vec![
+            Record::new(RecordData::from(record1_value)),
+            Record::new_key_value(
+                RecordKey::from(record2_key),
+                RecordData::from(record2_value),
+            ),
+        ]);
+        let activity = Activity::default();
+
+        //when
+        activity.increase_by_value(true, IncreaseValue::from(&batch));
+
+        //then
+        assert_eq!(activity.connector.records.load(Ordering::SeqCst), 2);
+        assert_eq!(activity.connector.bytes.load(Ordering::SeqCst), 33); // 10 + 11 + 12
+    }
+
+    #[test]
+    fn test_increase_from_file_partition_response() {
+        //given
+        let response = FilePartitionResponse {
+            high_watermark: 2,
+            log_start_offset: 1,
+            records: FileRecordSet::from(AsyncFileSlice::new(Default::default(), 0, 123)),
+            ..Default::default()
+        };
+        let activity = Activity::default();
+
+        //when
+        activity.increase_by_value(true, IncreaseValue::from(&response));
+
+        //then
+        assert_eq!(activity.connector.records.load(Ordering::SeqCst), 1);
+        assert_eq!(activity.connector.bytes.load(Ordering::SeqCst), 123);
     }
 }
