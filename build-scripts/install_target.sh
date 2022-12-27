ae6eca9ab8947467d4e4c43629c78bcff0a3cce1
Alexander Galibey <alexander.galibey@infinyon.com>
2022-12-27 19:36:11 +0000

    build: add cdk build and connector-derive tests to CI (#2904)
    Added `cdk` build and connector-derive tests to CI.
    
    Ignored warnings of `udeps` for `cdk` and `fluvio-connector-derive`

--- build-scripts/install_target.sh
+++ build-scripts/install_target.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$TARGET" = "armv7-unknown-linux-gnueabihf" ] || [ "$TARGET" = "arm-unknown-linux-gnueabihf" ]; then
     # Install cross if not installed
-    [[ -x "$(command -v cross)" ]] || cargo install cross
+    [[ -x "$(command -v cross)" ]] || cargo install cross --locked
     # must perform helm pkg since we dont have access to pkg
     make helm_pkg
 fi
