f9d0973a9426530832c493401a6dae8f18df8da3
David Beesley <david.beesley@pm.me>
2023-01-17 19:24:28 +0000

    feat: enable fuel (#2940)

--- crates/fluvio-smartengine/src/engine.rs
+++ crates/fluvio-smartengine/src/engine.rs
@@ -24,7 +24,9 @@ pub struct SmartEngine(Engine);
 #[allow(clippy::new_without_default)]
 impl SmartEngine {
     pub fn new() -> Self {
-        Self(Engine::default())
+        let mut config = wasmtime::Config::default();
+        config.consume_fuel(true);
+        Self(Engine::new(&config).expect("Config is static"))
     }
 
     pub(crate) fn new_state(&self) -> WasmState {
@@ -233,12 +235,9 @@ mod test {
 #[cfg(test)]
 mod chaining_test {
 
-    use std::{convert::TryFrom};
+    use std::convert::TryFrom;
 
-    use fluvio_smartmodule::{
-        dataplane::smartmodule::{SmartModuleInput},
-        Record,
-    };
+    use fluvio_smartmodule::{dataplane::smartmodule::SmartModuleInput, Record};
 
     use crate::{
         SmartEngine, SmartModuleChainBuilder, SmartModuleConfig, SmartModuleInitialData,
