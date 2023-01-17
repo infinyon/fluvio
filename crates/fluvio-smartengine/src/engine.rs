f41dbb721e3de87427e434377880bbbb2910af22
Dave Beesley <david.beesley@pm.me>
2023-01-16 16:20:02 -0800

    rename, records in

--- crates/fluvio-smartengine/src/engine.rs
+++ crates/fluvio-smartengine/src/engine.rs
@@ -118,18 +118,18 @@ impl SmartModuleChainInstance {
         metric.add_bytes_in(raw_len as u64);
 
         let base_offset = input.base_offset();
-
-        let mut total_records_in = number_of_records;
-        let mut total_records_out = 0;
+        let mut smartmodule_usage_in = number_of_records;
+        let mut smartmodule_usage_out = 0;
         if let Some((last, instances)) = self.instances.split_last_mut() {
+            metric.add_records_in(number_of_records);
             let mut next_input = input;
 
             for instance in instances {
                 // pass raw inputs to transform instance
                 // each raw input may result in multiple records
                 let output = instance.process(next_input, &mut self.store)?;
-                total_records_out += output.successes.len() as u64;
-                total_records_in += output.successes.len() as u64;
+                smartmodule_usage_out += output.successes.len() as u64;
+                smartmodule_usage_in += output.successes.len() as u64;
 
                 if output.error.is_some() {
                     // encountered error, we stop processing and return partial output
@@ -142,10 +142,10 @@ impl SmartModuleChainInstance {
 
             let output = last.process(next_input, &mut self.store)?;
             let records_out = output.successes.len();
-            total_records_out += output.successes.len() as u64;
+            smartmodule_usage_out += output.successes.len() as u64;
             metric.add_records_out(records_out as u64);
-            debug!(records_out, "sm records out last step");
-            metric.add_records_processed(max(total_records_in, total_records_out));
+            debug!(records_out, "sm records out");
+            metric.add_smartmodule_usage(max(smartmodule_usage_in, smartmodule_usage_out));
 
             Ok(output)
         } else {
--- crates/fluvio-smartengine/src/metrics.rs
+++ crates/fluvio-smartengine/src/metrics.rs
@@ -5,8 +5,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub struct SmartModuleChainMetrics {
     bytes_in: AtomicU64,
+    records_in: AtomicU64,
     records_out: AtomicU64,
-    records_processed: AtomicU64,
+    smartmodule_usage: AtomicU64,
     invocation_count: AtomicU64,
 }
 
@@ -16,12 +17,19 @@ impl SmartModuleChainMetrics {
         self.invocation_count.fetch_add(1, Ordering::SeqCst);
     }
 
+    pub fn add_records_in(&self, value: u64) {
+        self.records_in.fetch_add(value, Ordering::SeqCst);
+    }
+
     pub fn add_records_out(&self, value: u64) {
         self.records_out.fetch_add(value, Ordering::SeqCst);
     }
 
-    pub fn add_records_processed(&self, value: u64) {
-        self.records_processed.fetch_add(value, Ordering::SeqCst);
+    /// value = max(total_records_in, total_records_out) where
+    /// total_records_in = Number of records passed to each smartmodule in the chain.
+    /// total_records_out =Number of records returned from each smartmodule in the chain.
+    pub fn add_smartmodule_usage(&self, value: u64) {
+        self.smartmodule_usage.fetch_add(value, Ordering::SeqCst);
     }
 
     pub fn bytes_in(&self) -> u64 {
