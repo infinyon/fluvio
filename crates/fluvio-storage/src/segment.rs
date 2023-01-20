d84deb8eda27bf397e2634a2f17052bef94e5d5a
morenol <22335041+morenol@users.noreply.github.com>
2023-01-20 12:50:09 -0400

    Update segment.rs

--- crates/fluvio-storage/src/segment.rs
+++ crates/fluvio-storage/src/segment.rs
@@ -133,6 +133,7 @@ where
                 );
                 match max_offset_opt {
                     Some(max_offset) => {
+                        let max_offset = std::cmp::min(max_offset, self.get_end_offset());
                         // check if max offset same as segment end
                         if max_offset == self.get_end_offset() {
                             debug!("max offset is same as end offset, reading to end");
