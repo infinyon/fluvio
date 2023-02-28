f33ba100e8d262d67f177968d8ef909205c66786
Sebastian Imlay <sebastian.imlay@gmail.com>
2023-02-28 14:38:40 -0500

    Add foo key to custom config

--- connector/cargo_template/src/config.rs
+++ connector/cargo_template/src/config.rs
@@ -3,5 +3,6 @@ use fluvio_connector_common::connector;
 
 #[connector(config, name = "{{project-name}}")]
 #[derive(Debug)]
-pub(crate) struct {{project-name}}Config {
+pub(crate) struct CustomConfig {
+    foo: String,
 }
