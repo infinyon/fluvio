dfca97b69afa0a8a590025760a514f7f99bc2843
Sebastian Imlay <sebastian.imlay@gmail.com>
2023-02-28 15:03:13 -0500

    Fix up pr

--- connector/cargo_template/src/main.rs
+++ connector/cargo_template/src/main.rs
@@ -1,5 +1,5 @@
 mod config;
-use config::{{project-name}}Config;
+use config::CustomConfig;
 
 {% if connector-type == "source" %}
 use fluvio::{RecordKey, TopicProducer};
@@ -10,14 +10,29 @@ use fluvio_connector_common::{
 };
 
 #[connector(source)]
-async fn start(config: {{project-name}}Config, consumer: TopicProducer) -> Result<()> {
+async fn start(config: CustomConfig, producer: TopicProducer) -> Result<()> {
+    println!("Starting {{project-name}} source connector with with {config:?}");
+    for i in 1..1000 {
+        let value = format!("Hello, Fluvio - {i}");
+        producer.send(RecordKey::NULL, value).await?;
+        producer.flush().await?;
+        use std::{thread, time};
+
+        thread::sleep(time::Duration::from_millis(1000));
+    }
 	Ok(())
 }
+
 {% elsif connector-type == "sink" %}
 use fluvio_connector_common::{connector, consumer::ConsumerStream, tracing::trace, Result, Sink};
 
 #[connector(sink)]
-async fn start(config: {{project-name}}Config, mut stream: impl ConsumerStream) -> Result<()> {
+async fn start(config: CustomConfig, mut stream: impl ConsumerStream) -> Result<()> {
+    println!("Starting {{project-name}} sink connector with with {config:?}");
+    while let Some(Ok(record)) = stream.next().await {
+        let string = String::from_utf8_lossy(record.value());
+        println!("{string}");
+    }
 	Ok(())
 }
 {% endif %}
