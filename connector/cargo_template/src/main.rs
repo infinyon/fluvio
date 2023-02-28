c6d0e36d96a40797d2e87978274e409c51da392d
Sebastian Imlay <sebastian.imlay@gmail.com>
2023-02-28 16:47:01 -0500

    Updates from comments

--- connector/cargo_template/src/main.rs
+++ connector/cargo_template/src/main.rs
@@ -11,7 +11,7 @@ use fluvio_connector_common::{
 
 #[connector(source)]
 async fn start(config: CustomConfig, producer: TopicProducer) -> Result<()> {
-    println!("Starting {{project-name}} source connector with with {config:?}");
+    println!("Starting {{project-name}} source connector with {config:?}");
     for i in 1..1000 {
         let value = format!("Hello, Fluvio - {i}");
         producer.send(RecordKey::NULL, value).await?;
@@ -20,7 +20,7 @@ async fn start(config: CustomConfig, producer: TopicProducer) -> Result<()> {
 
         thread::sleep(time::Duration::from_millis(1000));
     }
-	Ok(())
+    Ok(())
 }
 
 {% elsif connector-type == "sink" %}
@@ -28,11 +28,11 @@ use fluvio_connector_common::{connector, consumer::ConsumerStream, tracing::trac
 
 #[connector(sink)]
 async fn start(config: CustomConfig, mut stream: impl ConsumerStream) -> Result<()> {
-    println!("Starting {{project-name}} sink connector with with {config:?}");
+    println!("Starting {{project-name}} sink connector with {config:?}");
     while let Some(Ok(record)) = stream.next().await {
         let string = String::from_utf8_lossy(record.value());
         println!("{string}");
     }
-	Ok(())
+    Ok(())
 }
 {% endif %}
--- crates/cdk/src/generate.rs
+++ crates/cdk/src/generate.rs
@@ -7,21 +7,21 @@ use cargo_generate::{GenerateArgs, TemplatePath, generate};
 use include_dir::{Dir, include_dir};
 use tempfile::TempDir;
 
-static CONNECTOR_MODULE_TEMPLATE: Dir<'static> =
+static CONNECTOR_TEMPLATE: Dir<'static> =
     include_dir!("$CARGO_MANIFEST_DIR/../../connector/cargo_template");
 
-/// Generate new SmartModule project
+/// Generate new SmartConnector project
 #[derive(Debug, Parser)]
 pub struct GenerateCmd {
-    /// SmartModule Project Name
+    /// SmartConnector Project Name
     name: Option<String>,
 
-    /// SmartModule Project Group Name.
+    /// SmartConnector Project Group Name.
     /// Default to Hub ID, if set. Overrides Hub ID if provided.
     #[clap(long, env = "CDK_PROJECT_GROUP", value_name = "GROUP")]
     project_group: Option<String>,
 
-    /// Local path to generate the SmartModule project.
+    /// Local path to generate the SmartConnector project.
     /// Default to directory with project name, created in current directory
     #[clap(long, env = "CDK_DESTINATION", value_name = "PATH")]
     destination: Option<PathBuf>,
@@ -29,15 +29,6 @@ pub struct GenerateCmd {
     /// Disable interactive prompt. Take all values from CLI flags. Fail if a value is missing.
     #[clap(long, action, hide_short_help = true)]
     silent: bool,
-
-    /// Visibility of SmartModule project to generate.
-    /// Skip prompt if value given.
-    #[clap(long, value_enum, value_name = "PUBLIC", env = "CDK_CONN_PUBLIC")]
-    conn_public: Option<bool>,
-
-    /// Set the remote URL for the hub
-    #[clap(long, env = "SMDK_HUB_REMOTE", hide_short_help = true)]
-    hub_remote: Option<String>,
 }
 
 impl GenerateCmd {
@@ -50,9 +41,7 @@ impl GenerateCmd {
         // cargo generate template source
         let temp_dir = TempDir::new()?;
         let path = temp_dir.path().to_str().map(|s| s.to_string());
-        CONNECTOR_MODULE_TEMPLATE
-            .extract(&temp_dir)
-            .map_err(Error::from)?;
+        CONNECTOR_TEMPLATE.extract(&temp_dir).map_err(Error::from)?;
         let template_path = TemplatePath {
             path,
             ..Default::default()
