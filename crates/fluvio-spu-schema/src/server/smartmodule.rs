5506d2af081d23545cd11c604b1d80ddac862842
Alexander Galibey <alexander.galibey@infinyon.com>
2023-03-01 12:50:26 -0500

    fix: backward compatibility for `SmartModuleKind`

--- crates/fluvio-spu-schema/src/server/smartmodule.rs
+++ crates/fluvio-spu-schema/src/server/smartmodule.rs
@@ -94,6 +94,15 @@ pub enum SmartModuleKind {
     },
     #[fluvio(min_version = ARRAY_MAP_WASM_API)]
     FilterMap,
+    #[fluvio(min_version = SMART_MODULE_API)]
+    #[fluvio(max_version = CHAIN_SMARTMODULE_API)]
+    Join(String),
+    #[fluvio(min_version = SMART_MODULE_API)]
+    #[fluvio(max_version = CHAIN_SMARTMODULE_API)]
+    JoinStream {
+        topic: String,
+        derivedstream: String,
+    },
     #[fluvio(min_version = GENERIC_SMARTMODULE_API)]
     Generic(SmartModuleContextData),
 }
@@ -106,6 +115,8 @@ impl std::fmt::Display for SmartModuleKind {
             SmartModuleKind::ArrayMap => "array_map",
             SmartModuleKind::Aggregate { .. } => "aggregate",
             SmartModuleKind::FilterMap => "filter_map",
+            SmartModuleKind::Join(..) => "join",
+            SmartModuleKind::JoinStream { .. } => "join_stream",
             SmartModuleKind::Generic(..) => "smartmodule",
         };
         out.write_str(name)
