9849d07d62fe743de6e74f78e7f95715a6c31d43
Esteban Borai <estebanborai@gmail.com>
2023-05-04 14:01:55 -0400

    feat: provide `BuildCmd` a target to build to

--- crates/cdk/src/build.rs
+++ crates/cdk/src/build.rs
@@ -16,17 +16,25 @@ pub struct BuildCmd {
     /// Extra arguments to be passed to cargo
     #[clap(raw = true)]
     extra_arguments: Vec<String>,
+
+    /// Provide target platform for the package. Optional.
+    /// By default the host's one is used.
+    #[clap(
+        long,
+        default_value_t = current_platform::CURRENT_PLATFORM.to_string()
+    )]
+    target: String,
 }
 
 impl BuildCmd {
     pub(crate) fn process(self) -> Result<()> {
         let opt = self.package.as_opt();
         let p = PackageInfo::from_options(&opt)?;
-
         let cargo = Cargo::build()
             .profile(opt.release)
             .lib(false)
             .package(p.package_name())
+            .target(&self.target)
             .extra_arguments(self.extra_arguments)
             .build()?;
 
