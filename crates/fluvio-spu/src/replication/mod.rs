eb8ff4ae03775f9879e188efc8598a4a06a7c80d
Sehyo Chang <sehyo@fluvio.io>
2023-05-07 22:02:20 -0700

    separate out replication to different crates

--- crates/fluvio-spu/src/replication/mod.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) mod follower;
-pub(crate) mod leader;
-
-#[cfg(test)]
-pub(crate) mod test;
---
+++ crates/fluvio-storage-replication/src/lib.rs
@@ -0,0 +1,5 @@
+pub(crate) mod follower;
+pub(crate) mod leader;
+
+#[cfg(test)]
+pub(crate) mod test;
