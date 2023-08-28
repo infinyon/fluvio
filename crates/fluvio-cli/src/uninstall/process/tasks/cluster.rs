fea2a832dbe041bb6cc5af84cca168817d24a072
Esteban Borai <estebanborai@gmail.com>
2023-08-28 16:50:21 -0400

    feat: fluvio cluster removal

--- crates/fluvio-cli/src/uninstall/process/tasks/cluster.rs
+++ crates/fluvio-cli/src/uninstall/process/tasks/cluster.rs
@@ -1,30 +1,34 @@
 use async_trait::async_trait;
 use anyhow::Result;
 
+use fluvio_cluster::cli::{ClusterCmd, CheckOpt};
+
 use crate::uninstall::process::{Task, UninstallProcess};
 
 pub struct ClusterUninstallTask;
 
 impl ClusterUninstallTask {
-  pub fn new() -> Self {
-    Self {}
-  }
+    pub fn new() -> Self {
+        Self {}
+    }
 }
 
 #[async_trait]
 impl Task for ClusterUninstallTask {
-  fn title(&self) -> &'static str {
-    "Remove Fluvio Clusters"
-  }
+    fn title(&self) -> &'static str {
+        "Remove Fluvio Clusters"
+    }
 
-  /// Checks wether a `Task` is done
-  async fn is_done(&self, process: &UninstallProcess) -> Result<bool> {
-    Ok(false)
-  }
+    async fn is_done(&self, process: &UninstallProcess) -> Result<bool> {
+        let cluster_check_cmd = CheckOpt { fix: false };
 
-  /// Runs a `Task`
-  async fn run(&self, process: &UninstallProcess) -> Result<()> {
-    println!("Running cluster uninstall task");
-    Ok(())
-  }
+        cluster_check_cmd.process(process.version.clone()).await?;
+
+        Ok(true)
+    }
+
+    async fn run(&self, process: &UninstallProcess) -> Result<()> {
+        println!("Running cluster uninstall task");
+        Ok(())
+    }
 }
