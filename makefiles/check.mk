e46b342d40037d9095f76209b0a21f9c4483b7ef
Esteban Borai <estebanborai@gmail.com>
2023-08-31 19:38:32 -0400

    revert: include cli in docker image (#3483)
    This reverts commit fa4e81fa6178e4a263ac269d9e85fdd6f1bd445f.

--- makefiles/check.mk
+++ makefiles/check.mk
@@ -71,4 +71,3 @@ run-client-doc-test: install_rustup_target
 
 fluvio_run_bin: install_rustup_target
 	cargo build --bin fluvio-run -p fluvio-run $(RELEASE_FLAG) --target $(TARGET) $(DEBUG_SMARTMODULE_FLAG)
-	cargo build --bin fluvio -p fluvio-cli $(RELEASE_FLAG) --target $(TARGET) $(DEBUG_SMARTMODULE_FLAG)
