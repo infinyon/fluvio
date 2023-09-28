6fd6c58ff099b02fd72adcc42bcd224650e44971
Esteban Borai <estebanborai@gmail.com>
2023-09-28 17:31:41 -0300

    fix: registry url

--- crates/fvm/src/common.rs
+++ crates/fvm/src/common.rs
@@ -14,7 +14,7 @@ pub const FVM_BINARY_NAME: &str = "fvm";
 pub const FVM_PACKAGES_SET_DIR: &str = "pkgset";
 
 /// The URL of the Infinyon Hub's FVM API
-pub const INFINYON_HUB_URL: &str = "https://hub-dev.infinyon.cloud";
+pub const INFINYON_HUB_URL: &str = "https://hub.infinyon.cloud";
 
 /// The URI of the Infinyon Hub's FVM API Package Set Endpoint
 pub const INFINYON_HUB_FVM_PKGSET_API_URI: &str = "/hub/v1/fvm/pkgset";
