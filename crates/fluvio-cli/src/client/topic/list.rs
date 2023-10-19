22d86e8a2fc75a99db8fb11b2df43ab996c29148
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2023-10-18 21:05:25 -0700

    (cli): 'LAST MODIFIED' to topic list output & 'STATUS' presentation fix

--- crates/fluvio-cli/src/client/topic/list.rs
+++ crates/fluvio-cli/src/client/topic/list.rs
@@ -32,41 +32,52 @@ impl ListTopicsOpt {
         let output_type = self.output.format;
         debug!("list topics {:#?} ", output_type);
         let admin = fluvio.admin().await;
-
         let topics = admin.all::<TopicSpec>().await?;
-        display::format_response_output(out, topics, output_type)?;
+        display::format_response_output(out, topics, output_type, fluvio)?;
         Ok(())
     }
 }
 
 mod display {
+    use std::time::{Duration, UNIX_EPOCH};
 
-    use std::time::Duration;
-
-    use humantime::{format_duration};
     use comfy_table::{Row, Cell, CellAlignment};
+    use humantime::{format_duration, format_rfc3339_millis};
     use serde::Serialize;
 
-    use fluvio::metadata::objects::Metadata;
-    use fluvio::metadata::topic::TopicSpec;
+    use fluvio::{
+        Fluvio,
+        metadata::{objects::Metadata, topic::TopicSpec},
+    };
+    use fluvio_future::task::run_block_on;
 
     use crate::common::output::{OutputType, TableOutputHandler, Terminal, OutputError};
     use crate::common::t_println;
+    use super::utils::latest_record_timestamp;
 
     #[derive(Serialize)]
-    struct ListTopics(Vec<Metadata<TopicSpec>>);
+    struct ListTopics<'a> {
+        inner: Vec<Metadata<TopicSpec>>,
+
+        #[serde(skip_serializing)]
+        fluvio: &'a Fluvio,
+    }
 
     /// Process server based on output type
     pub fn format_response_output<O>(
         out: std::sync::Arc<O>,
         list_topics: Vec<Metadata<TopicSpec>>,
         output_type: OutputType,
+        fluvio: &Fluvio,
     ) -> Result<(), OutputError>
     where
         O: Terminal,
     {
         if !list_topics.is_empty() {
-            let table_list = ListTopics(list_topics);
+            let table_list = ListTopics {
+                inner: list_topics,
+                fluvio,
+            };
             out.render_list(&table_list, output_type)
         } else {
             t_println!(out, "No topics found");
@@ -77,7 +88,7 @@ mod display {
     // -----------------------------------
     // Output Handlers
     // -----------------------------------
-    impl TableOutputHandler for ListTopics {
+    impl TableOutputHandler for ListTopics<'_> {
         /// table header implementation
         fn header(&self) -> Row {
             Row::from([
@@ -89,6 +100,7 @@ mod display {
                 "COMPRESSION",
                 "DEDUPLICATION",
                 "STATUS",
+                "LAST MODIFIED",
                 "REASON",
             ])
         }
@@ -100,10 +112,24 @@ mod display {
 
         /// table content implementation
         fn content(&self) -> Vec<Row> {
-            self.0
+            self.inner
                 .iter()
                 .map(|metadata| -> Row {
                     let topic = &metadata.spec;
+
+                    let topic_name = metadata.name.to_string();
+
+                    let modified_at = run_block_on(async move {
+                        latest_record_timestamp(topic_name, self.fluvio)
+                            .await
+                            .map(|ts| {
+                                let ts_ms = UNIX_EPOCH
+                                    + u64::try_from(ts)
+                                        .map(Duration::from_millis)
+                                        .unwrap_or_default();
+                                format!("{}", format_rfc3339_millis(ts_ms))
+                            })
+                    });
 
                     Row::from([
                         Cell::new(metadata.name.to_string()),
@@ -120,11 +146,45 @@ mod display {
                                 .map(|d| d.filter.transform.uses.as_str())
                                 .unwrap_or("none"),
                         ),
-                        Cell::new(metadata.status.resolution.to_string()),
+                        Cell::new(metadata.status.resolution.resolution_label()),
+                        Cell::new(modified_at.unwrap_or_else(String::new)),
                         Cell::new(metadata.status.reason.to_string()),
                     ])
                 })
                 .collect()
+        }
+    }
+}
+
+mod utils {
+    use fluvio::{
+        consumer::{ConsumerConfigBuilder, PartitionSelectionStrategy},
+        Fluvio, Offset,
+    };
+    use fluvio_types::Timestamp;
+    use futures_util::stream::StreamExt;
+
+    /// A utility function to get the timestamp of the `Offset::from_end(1)` record across all
+    /// partitions for a given `topic_name`.
+    pub async fn latest_record_timestamp(topic_name: String, fluvio: &Fluvio) -> Option<Timestamp> {
+        let consumer = fluvio
+            .consumer(PartitionSelectionStrategy::All(topic_name))
+            .await
+            .ok()?;
+
+        let consumer_config = ConsumerConfigBuilder::default()
+            .disable_continuous(true)
+            .build()
+            .ok()?;
+
+        let stream_future = consumer.stream_with_config(Offset::from_end(1), consumer_config);
+
+        let mut stream = stream_future.await.ok()?;
+
+        if let Some(Ok(record)) = stream.next().await {
+            Some(record.timestamp())
+        } else {
+            None
         }
     }
 }
