52303b4bb713f44120ecc4a0b081d1f03c1a64ce
Matheus Consoli <matheus.consoli7@gmail.com>
2023-10-20 14:29:03 -0300

    Change `fluvio hub conn download` message to instruct about `cdk`

--- crates/fluvio-cli/src/client/hub/connector/mod.rs
+++ crates/fluvio-cli/src/client/hub/connector/mod.rs
@@ -21,7 +21,14 @@ impl ConnectorHubSubCmd {
     pub async fn process<O: Terminal + Debug + Send + Sync>(self, out: Arc<O>) -> Result<()> {
         match self {
             ConnectorHubSubCmd::List(opts) => opts.process(out).await,
-            ConnectorHubSubCmd::Download(opts) => opts.process(out).await,
+            ConnectorHubSubCmd::Download(_) => {
+                out.println("Connectors running on `InfinyOn Cloud` are automatically downloaded during `fluvio cloud connector create ...`.");
+                out.println("Connectors running locally require `cdk` to download and deploy:");
+                out.println("1. Install cdk: `fluvio install cdk`");
+                out.println("2. Download connector: `cdk hub download ...`");
+                out.println("3. Deploy connector: `cdk deploy start ...`");
+                Ok(())
+            }
         }
     }
 }
