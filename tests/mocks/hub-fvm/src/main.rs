fa6988e6b09aff00ca0e94ea0c9d0b88d4132006
Esteban Borai <estebanborai@gmail.com>
2023-11-02 16:33:15 -0300

    feat: mock impl with tide

--- tests/mocks/hub-fvm/src/main.rs
+++ tests/mocks/hub-fvm/src/main.rs
@@ -1,29 +1,18 @@
-use wiremock::{MockServer, Mock, ResponseTemplate};
-use wiremock::matchers::{method, path};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use tide::Request;
 
 #[async_std::main]
-async fn main() {
-    // Start a background HTTP server on a random local port
-    let mock_server = MockServer::start().await;
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
 
-    // Arrange the behaviour of the MockServer adding a Mock:
-    // when it receives a GET request on '/hello' it will respond with a 200.
-    Mock::given(method("GET"))
-        .and(path("/hello"))
-        .respond_with(ResponseTemplate::new(200))
-        // Mounting the mock on the mock server - it's now effective!
-        .mount(&mock_server)
-        .await;
+    app.at("/get").get(resolve);
 
-    // If we probe the MockServer using any HTTP client it behaves as expected.
-    let status = surf::get(format!("{}/hello", &mock_server.uri()))
-        .await
-        .unwrap()
-        .status();
+    app.listen("127.0.0.1:8080").await?;
 
-    // If the request doesn't match any `Mock` mounted on our `MockServer` a 404 is returned.
-    let status = surf::get(format!("{}/missing", &mock_server.uri()))
-        .await
-        .unwrap()
-        .status();
+    Ok(())
+}
+
+async fn resolve(mut req: Request<()>) -> tide::Result {
+    Ok(format!("Hello").into())
 }
