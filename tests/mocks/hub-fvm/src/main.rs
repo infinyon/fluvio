576ae4b020b7ae46fba4a7fbb64d84faf4066bf9
Esteban Borai <estebanborai@gmail.com>
2023-11-02 17:19:13 -0300

    feat: mocked response of artifacts

--- tests/mocks/hub-fvm/src/main.rs
+++ tests/mocks/hub-fvm/src/main.rs
@@ -1,18 +1,31 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::str::FromStr;
 
 use tide::Request;
+
+use fluvio_hub_util::fvm::Channel;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
 
-    app.at("/get").get(resolve);
+    app.at("/hub/v1/fvm/pkgset/:package/:version/:arch")
+        .get(get_pkgset);
 
-    app.listen("127.0.0.1:8080").await?;
+    app.listen("127.0.0.1:9000").await?;
 
     Ok(())
 }
 
-async fn resolve(mut req: Request<()>) -> tide::Result {
-    Ok(format!("Hello").into())
+async fn get_pkgset(mut req: Request<()>) -> tide::Result {
+    let arch = req.param("arch")?;
+    let package = req.param("package")?;
+    let version = req.param("version")?;
+    let channel = Channel::from_str(version)?;
+    let mocked_pkgset = make_pkgset_mock(arch, package, channel);
+    
+    todo!()
+}
+
+fn make_pkgset_mock(arch: &str, package: &str, channel: Channel) {
+    
 }
