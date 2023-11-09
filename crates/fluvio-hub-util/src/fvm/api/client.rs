5d11167a78350d39c481a3ad5a2489256412d590
Esteban Borai <estebanborai@gmail.com>
2023-11-09 18:19:06 -0300

    feat: update hub util fvm client to new endpoint structure

--- crates/fluvio-hub-util/src/fvm/api/client.rs
+++ crates/fluvio-hub-util/src/fvm/api/client.rs
@@ -19,13 +19,8 @@ impl Client {
     }
 
     /// Fetches a [`PackageSet`] from the Hub with the specific [`Channel`]
-    pub async fn fetch_package_set(
-        &self,
-        name: impl AsRef<str>,
-        channel: &Channel,
-        arch: &str,
-    ) -> Result<PackageSet> {
-        let url = self.make_fetch_package_set_url(name, channel, arch)?;
+    pub async fn fetch_package_set(&self, channel: &Channel, arch: &str) -> Result<PackageSet> {
+        let url = self.make_fetch_package_set_url(channel, arch)?;
         let mut res = surf::get(url)
             .await
             .map_err(|err| Error::msg(err.to_string()))?;
@@ -43,16 +38,10 @@ impl Client {
 
     /// Builds the URL to the Hub API for fetching a [`PackageSet`] using the
     /// [`Client`]'s `api_url`.
-    fn make_fetch_package_set_url(
-        &self,
-        name: impl AsRef<str>,
-        channel: &Channel,
-        arch: &str,
-    ) -> Result<Url> {
+    fn make_fetch_package_set_url(&self, channel: &Channel, arch: &str) -> Result<Url> {
         let url = format!(
-            "{}hub/v1/fvm/pkgset/{name}/{channel}/{arch}",
+            "{}hub/v1/fvm/pkgset/{channel}?arch={arch}",
             self.api_url,
-            name = name.as_ref(),
             channel = channel,
             arch = arch
         );
@@ -84,10 +73,13 @@ mod tests {
     fn builds_url_for_fetching_pkgsets() {
         let client = Client::new("https://hub.infinyon.cloud").unwrap();
         let url = client
-            .make_fetch_package_set_url("fluvio", &Channel::Stable, "arm-unknown-linux-gnueabihf")
+            .make_fetch_package_set_url(&Channel::Stable, "arm-unknown-linux-gnueabihf")
             .unwrap();
 
-        assert_eq!(url.as_str(), "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/fluvio/stable/arm-unknown-linux-gnueabihf");
+        assert_eq!(
+            url.as_str(),
+            "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/stable?arch=arm-unknown-linux-gnueabihf"
+        );
     }
 
     #[test]
@@ -95,12 +87,11 @@ mod tests {
         let client = Client::new("https://hub.infinyon.cloud").unwrap();
         let url = client
             .make_fetch_package_set_url(
-                "fluvio",
                 &Channel::Tag(Version::from_str("0.10.14-dev+123345abc").unwrap()),
                 "arm-unknown-linux-gnueabihf",
             )
             .unwrap();
 
-        assert_eq!(url.as_str(), "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/fluvio/0.10.14-dev+123345abc/arm-unknown-linux-gnueabihf");
+        assert_eq!(url.as_str(), "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/0.10.14-dev+123345abc?arch=arm-unknown-linux-gnueabihf");
     }
 }
