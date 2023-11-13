ef723c39d742fa4957061746d40d0854273915e6
Esteban Borai <estebanborai@gmail.com>
2023-11-13 14:23:48 -0300

    feat: publish PackageSet on Fluvio Publish

---
+++ actions/publish-pkgset.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+curl -v -X "POST" "https://hub.infinyon.cloud/hub/v1/fvm/pkgset" \
+     -H "Authorization: $BPKG_TOKEN" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     --data-binary @- << EOF
+{
+  "artifacts": [
+    {
+      "name": "fluvio",
+      "version": "$FLUVIO_VERSION"
+    },
+    {
+      "name": "fluvio-cloud",
+      "version": "$FLUVIO_CLOUD_VERSION"
+    },
+    {
+      "name": "fluvio-run",
+      "version": "$FLUVIO_VERSION"
+    },
+    {
+      "name": "cdk",
+      "version": "$FLUVIO_VERSION"
+    },
+    {
+      "name": "smdk",
+      "version": "$FLUVIO_VERSION"
+    }
+  ],
+  "pkgset": "$FLUVIO_VERSION"
+}
+EOF
