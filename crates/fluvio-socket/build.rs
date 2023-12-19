f117180c9f1962ebf9ba3b6db58f331116f08ad2
Luis Moreno <morenol@users.noreply.github.com>
2023-12-19 14:31:11 -0400

    build: don't require git to build fluvio

--- crates/fluvio-socket/build.rs
+++ crates/fluvio-socket/build.rs
@@ -1,5 +1,3 @@
-use std::process::Command;
-
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
 
@@ -10,14 +8,4 @@ fn main() {
         }
     }
     println!("cargo:rerun-if-changed=build.rs");
-
-    // Fetch current git hash to print version output
-    let git_version_output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .expect("should run 'git rev-parse HEAD' to get git hash");
-    let git_hash = String::from_utf8(git_version_output.stdout)
-        .expect("should read 'git' stdout to find hash");
-    // Assign the git hash to the compile-time GIT_HASH env variable (to use with env!())
-    println!("cargo:rustc-env=GIT_HASH={git_hash}");
 }
