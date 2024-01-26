56f8ff49c4edb6b21cd8f6d25bb0dd4e317534e4
Alan Chen <argot@digikata.com>
2024-01-26 08:47:51 -0800

    merge: more fixes

--- crates/fluvio-spu/src/replication/leader/mod.rs
+++ crates/fluvio-spu/src/replication/leader/mod.rs
@@ -9,12 +9,13 @@ mod spu;
 mod kv;
 
 pub use self::leaders_state::{ReplicaLeadersState, SharedReplicaLeadersState};
-pub use self::replica_state::{SharedFileLeaderState, LeaderReplicaState};
+pub use self::replica_state::{SharedFileLeaderState, SharedLeaderState, LeaderReplicaState};
 pub use self::connection::FollowerHandler;
 pub use self::api_key::LeaderPeerApiEnum;
 pub use self::peer_api::LeaderPeerRequest;
 pub use self::update_offsets::UpdateOffsetRequest;
 pub use self::update_offsets::ReplicaOffsetRequest;
 pub use self::kv::{LeaderKVStorage, LeaderReplicaLog};
+pub use self::actions::FollowerOffsetUpdate;
 
 pub use self::spu::*;
