00c39a11be3cc65e73cf0352d3b21b6aa3484ecb
Alan Chen <argot@digikata.com>
2024-02-21 15:03:58 -0800

    fix: `fluvio profile add` w/ no config file (#3874)
    Fixes #3816

--- crates/fluvio-cli/src/profile/add.rs
+++ crates/fluvio-cli/src/profile/add.rs
@@ -15,25 +15,23 @@ pub struct ManualAddOpt {
     /// Installation type of cluster, e.g. local, local-k8, k8
     installation_type: Option<InstallationType>,
 }
-// todo: p2 add tls config, p1 is default disabled for manual add
 
 impl ManualAddOpt {
     pub fn process(self) -> Result<()> {
-        match ConfigFile::load(None) {
-            Ok(mut config_file) => {
-                let def_tls = TlsPolicy::Disabled;
-                config_file.add_or_replace_profile(
-                    &self.profile_name,
-                    &self.cluster_address,
-                    &def_tls,
-                )?;
-                let config = config_file.mut_config().current_cluster_mut()?;
-                self.installation_type.unwrap_or_default().save_to(config)?;
-                config_file.save()?;
-                println!("Switched to profile {}", &self.profile_name);
+        let mut config_file = match ConfigFile::load(None) {
+            Ok(config_file) => config_file,
+            Err(_) => {
+                println!("Creating default fluvio config file");
+                ConfigFile::default_config()?
             }
-            Err(_) => println!("no profile can be found"),
-        }
+        };
+
+        let def_tls = TlsPolicy::Disabled;
+        config_file.add_or_replace_profile(&self.profile_name, &self.cluster_address, &def_tls)?;
+        let config = config_file.mut_config().current_cluster_mut()?;
+        self.installation_type.unwrap_or_default().save_to(config)?;
+        config_file.save()?;
+        println!("Switched to profile {}", &self.profile_name);
 
         Ok(())
     }
