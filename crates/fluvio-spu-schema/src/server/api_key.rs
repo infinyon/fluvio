6b0294447e7b2e6bed8fdf30df053e261799ad9b
Alexander Galibey <alexander.galibey@infinyon.com>
2024-03-26 15:53:07 +0700

    feat: added public SPU requests for offset mngt

--- crates/fluvio-spu-schema/src/server/api_key.rs
+++ crates/fluvio-spu-schema/src/server/api_key.rs
@@ -20,6 +20,9 @@ pub enum SpuServerApiKey {
     FetchOffsets = 1002,
     StreamFetch = 1003,
     UpdateOffsets = 1005,
+    UpdateConsumerOffset = 1006,
+    DeleteConsumerOffset = 1007,
+    FetchConsumerOffsets = 1008,
 }
 
 impl Default for SpuServerApiKey {
