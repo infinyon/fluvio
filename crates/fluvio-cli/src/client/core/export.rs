ca8845121723fc0e0a34f7b9f522d12db5a6eeea
Felipe Cardozo <fraifelipe@gmail.com>
2024-04-05 14:58:04 -0300

    fix: export should check if edge exist

--- crates/fluvio-cli/src/client/core/export.rs
+++ crates/fluvio-cli/src/client/core/export.rs
@@ -7,17 +7,23 @@ use fluvio_sc_schema::{
     remote::{Core, RemoteSpec, RemoteType},
 };
 use k8_types::K8Obj;
+use anyhow::anyhow;
+
+use super::get_admin;
 
 #[derive(Debug, Parser)]
 pub struct ExportOpt {
-    /// id of the remote cluster
-    remote_id: String,
+    /// id of the edge cluster to export
+    edge_id: String,
     /// name of the file where we should put the file
     #[arg(long, short = 'f')]
     file: Option<String>,
     /// override endpoint of the core cluster
     #[arg(long, short = 'e')]
     public_endpoint: Option<String>,
+    // id of the core cluster to share
+    #[arg(name = "c")]
+    core_id: Option<String>,
 }
 
 impl ExportOpt {
@@ -26,20 +32,32 @@ impl ExportOpt {
         out: Arc<T>,
         cluster_target: ClusterTarget,
     ) -> Result<()> {
-        let fluvio_config = cluster_target.load()?;
-
         let public_endpoint = if let Some(public_endpoint) = self.public_endpoint {
             public_endpoint
         } else {
+            let fluvio_config = cluster_target.clone().load()?;
             fluvio_config.endpoint
         };
 
-        let metadata_name = format!("edge-{}", self.remote_id);
+        let admin = get_admin(cluster_target).await?;
+        let all_remotes = admin.all::<RemoteSpec>().await?;
+        let _edge = all_remotes
+            .iter()
+            .find(|remote| match &remote.spec.remote_type {
+                RemoteType::Edge(edge) => edge.id == self.edge_id,
+                _ => false,
+            })
+            .ok_or_else(|| anyhow!("edge cluster not found"))?;
+
+        let core_id = self.core_id.clone().unwrap_or_else(|| "core".to_owned());
+
+        let metadata_name = format!("edge-{}", self.edge_id);
         let edge_metadata = vec![K8Obj::new(
             metadata_name,
             RemoteSpec {
                 remote_type: RemoteType::Core(Core {
-                    id: self.remote_id,
+                    id: core_id,
+                    edge_id: self.edge_id,
                     public_endpoint,
                 }),
             },
