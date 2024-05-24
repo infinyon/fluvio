3997a871ab167ef503236dd5a06ac5a3c87f2099
Esteban Borai <estebanborai@gmail.com>
2024-05-24 14:23:49 -0400

    feat(cdk): generate command params

--- crates/cdk/src/generate.rs
+++ crates/cdk/src/generate.rs
@@ -17,8 +17,14 @@ static CONNECTOR_TEMPLATE: Dir<'static> =
 /// Generate new SmartConnector project
 #[derive(Debug, Parser)]
 pub struct GenerateCmd {
-    /// SmartConnector Project Name
+    /// Connector Name
     name: Option<String>,
+
+    /// Connector developer group
+    group: Option<String>,
+
+    /// Connector description used as part of the project metadata
+    conn_description: Option<String>,
 
     /// Local path to generate the SmartConnector project.
     /// Default to directory with project name, created in current directory
@@ -28,6 +34,16 @@ pub struct GenerateCmd {
     /// Disable interactive prompt. Take all values from CLI flags. Fail if a value is missing.
     #[arg(long, hide_short_help = true)]
     silent: bool,
+
+    /// Type of Connector project to generate.
+    /// Skip prompt if value given.
+    #[arg(long, value_enum, value_name = "TYPE", env = "CDK_CONN_TYPE")]
+    conn_type: Option<ConnectorType>,
+
+    /// Visibility of Connector project to generate.
+    /// Skip prompt if value given.
+    #[arg(long, value_enum, value_name = "PUBLIC", env = "CDK_CONN_PUBLIC")]
+    conn_public: Option<bool>,
 }
 
 impl GenerateCmd {
@@ -46,8 +62,14 @@ impl GenerateCmd {
             ..Default::default()
         };
 
-        let fluvio_dependency_version_hash =
-            format!("fluvio-cargo-dependency-hash={}", env!("GIT_HASH"));
+        let mut maybe_user_input = CdkTemplateUserValues::new();
+
+        maybe_user_input
+            .with_name(self.name)
+            .with_group(self.group)
+            .with_description(self.conn_description)
+            .with_conn_type(self.conn_type)
+            .with_conn_public(self.conn_public);
 
         let args = GenerateArgs {
             name: self.name,
@@ -55,12 +77,110 @@ impl GenerateCmd {
             verbose: !self.silent,
             silent: self.silent,
             destination: self.destination,
-            define: vec![fluvio_dependency_version_hash],
+            define: maybe_user_input.to_cargo_generate(),
             ..Default::default()
         };
 
         let _gen_dir = generate(args).map_err(Error::from)?;
 
         Ok(())
+    }
+}
+
+#[derive(ValueEnum, Clone, Debug, Parser, PartialEq, Eq, EnumDisplay)]
+#[clap(rename_all = "kebab-case")]
+#[enum_display(case = "Kebab")]
+enum ConnectorType {
+    Sink,
+    Source,
+}
+
+enum CdkTemplateValue {
+    Name(String),
+    Group(String),
+    Description(String),
+    ConnFluvioDependencyHash(String),
+    ConnType(ConnectorType),
+    ConnPublic(bool),
+}
+
+impl Display for CdkTemplateValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CdkTemplateValue::Name(name) => write!(f, "project-name={}", name),
+            CdkTemplateValue::Group(group) => write!(f, "project-group={}", group),
+            CdkTemplateValue::Description(description) => write!(f, "project-description={}", description),
+            CdkTemplateValue::ConnFluvioDependencyHash(hash) => {
+                write!(f, "fluvio-cargo-dependency-hash={}", hash)
+            }
+            CdkTemplateValue::ConnType(conn_type) => write!(f, "connector-type={conn_type}"),
+            CdkTemplateValue::ConnPublic(conn_public) => {
+                write!(f, "connector-public={conn_public}")
+            }
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+struct CdkTemplateUserValues {
+    values: Vec<CdkTemplateValue>,
+}
+
+impl CdkTemplateUserValues {
+    fn new() -> Self {
+        // By default the fluvio dependency hash is the current git hash
+        // and its always passed as option to cargo generate
+        let values = vec![CdkTemplateValue::FluvioDependencyHash(
+            env!("GIT_HASH")
+        )];
+
+        Self::default()
+    }
+
+    fn to_vec(&self) -> Vec<SmdkTemplateValue> {
+        self.values.clone()
+    }
+
+    fn to_cargo_generate(&self) -> Vec<String> {
+        self.to_vec().iter().map(|v| v.to_string()).collect()
+    }
+
+    fn with_name(&mut self, value: Option<String>) {
+        if let Some(v) = value {
+            tracing::debug!("CDK Argument - project-name={}", v);
+            self.values.push(CdkTemplateValue::Name(v));
+        }
+    }
+
+    fn with_group(&mut self, value: Option<String>) {
+        if let Some(v) = value {
+            tracing::debug!("CDK Argument - project-group={}", v);
+            self.values.push(CdkTemplateValue::Group(v));
+        }
+    }
+
+    fn with_description(&mut self, value: Option<String>) {
+        if let Some(v) = value {
+            tracing::debug!("CDK Argument - project-description={}", v);
+            self.values.push(CdkTemplateValue::Description(v));
+        }
+    }
+
+    fn with_conn_type(&mut self, value: Option<ConnectorType>) {
+        if let Some(v) = value {
+            tracing::debug!("CDK Argument - connector-type={}", v);
+            self.values.push(CdkTemplateValue::ConnType(v));
+        }
+
+        self
+    }
+
+    fn with_conn_public(&mut self, value: Option<bool>) {
+        if let Some(v) = value {
+            tracing::debug!("CDK Argument - connector-public={}", v);
+            self.values.push(CdkTemplateValue::ConnPublic(v));
+        }
+
+        self
     }
 }
