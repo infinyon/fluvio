77100178cde87de57a46eb40a7c87647fb770b8d
Felipe Cardozo <fraifelipe@gmail.com>
2024-08-16 17:02:41 -0300

    feat: add force arg for cluster upgrade

--- crates/fluvio-cluster/src/cli/upgrade.rs
+++ crates/fluvio-cluster/src/cli/upgrade.rs
@@ -15,6 +15,9 @@ use super::start::StartOpt;
 
 #[derive(Debug, Parser)]
 pub struct UpgradeOpt {
+    /// Force upgrade without confirmation
+    #[clap(short, long)]
+    pub force: bool,
     #[clap(flatten)]
     pub start: StartOpt,
 }
@@ -30,32 +33,28 @@ impl UpgradeOpt {
         } else {
             self.start.installation_type.set(installation_type.clone());
         }
+
+        if !self.force {
+            let prompt = dialoguer::Confirm::new()
+                .with_prompt(format!(
+                    "Upgrade Local Fluvio cluster to version {}?",
+                    platform_version
+                ))
+                .interact()?;
+
+            if !prompt {
+                println!("Upgrade cancelled");
+                return Ok(());
+            }
+        }
+
         match installation_type {
             InstallationType::K8 => {
                 self.start.process(platform_version, true).await?;
             }
             InstallationType::Local | InstallationType::LocalK8 | InstallationType::ReadOnly => {
                 ShutdownOpt.process().await?;
-
-                let path = LOCAL_CONFIG_PATH
-                    .as_ref()
-                    .ok_or(anyhow!("Local config path not set"))?;
-
-                let mut local_config = LocalConfig::load_from(path)
-                    .map_err(|err| anyhow!("Failed to load local config: {err}"))?
-                    .evolve();
-
-                let config = local_config
-                    .platform_version(platform_version.clone())
-                    .launcher(DEFAULT_RUNNER_PATH.clone())
-                    .build()?;
-
-                config.save_to(path)?;
-
-                println!(
-                    "Successfully upgraded Local Fluvio cluster to {}",
-                    platform_version,
-                );
+                self.upgrade_local_cluster(platform_version)?;
             }
             InstallationType::Cloud => {
                 let profile = config.config().current_profile_name().unwrap_or("none");
@@ -64,6 +63,30 @@ impl UpgradeOpt {
             other => bail!("upgrade command is not supported for {other} installation type"),
         };
 
+        Ok(())
+    }
+
+    fn upgrade_local_cluster(&self, platform_version: Version) -> Result<()> {
+        let path = LOCAL_CONFIG_PATH
+            .as_ref()
+            .ok_or(anyhow!("Local config path not set"))?;
+
+        let mut local_config = LocalConfig::load_from(path)
+            .map_err(|err| anyhow!("Failed to load local config: {err}"))?
+            .evolve();
+
+        let config = local_config
+            .platform_version(platform_version.clone())
+            .launcher(DEFAULT_RUNNER_PATH.clone())
+            .build()?;
+
+        config.save_to(path)?;
+
+        println!(
+            "Successfully upgraded Local Fluvio cluster to {}",
+            platform_version,
+        );
+        println!("To start the cluster again, run: fluvio cluster resume");
         Ok(())
     }
 }
