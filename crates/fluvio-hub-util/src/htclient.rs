08fa2a83d0e1d016532c1331828e39b42494ba9d
Esteban Borai <estebanborai@gmail.com>
2024-08-30 19:42:08 -0400

    feat(hub): provide `wasm32-unknown-unknown` support for `hub-util` (#4151)
    * feat(hub): provide `wasm32-unknown-unknown` support for `hub-util`
    
    * feat: unimplemented for hub default access
    
    * feat: use `TryFrom<http::Request\<T\>\>`
    
    * fix: clippy
    
    * feat: use `post` instead of `get` for `make_action_token`
    
    * chore: pr review fixes

--- crates/fluvio-hub-util/src/htclient.rs
+++ crates/fluvio-hub-util/src/htclient.rs
@@ -3,8 +3,10 @@ pub use http::StatusCode;
 pub use http::{Request, Response};
 
 use anyhow::{anyhow, Result};
-use ureq::OrAnyStatus;
 use serde::de::DeserializeOwned;
+
+#[cfg(not(target_arch = "wasm32"))]
+use ureq::OrAnyStatus;
 
 pub async fn get_auth_json<J: serde::de::DeserializeOwned>(
     url: &str,
@@ -27,6 +29,7 @@ pub async fn get_auth_json<J: serde::de::DeserializeOwned>(
 }
 
 /// for simple get requests
+#[cfg(not(target_arch = "wasm32"))]
 pub async fn get(uri: impl AsRef<str>) -> Result<Response<Vec<u8>>> {
     use std::io::Read;
 
@@ -51,6 +54,43 @@ pub async fn get(uri: impl AsRef<str>) -> Result<Response<Vec<u8>>> {
     Ok(response)
 }
 
+#[cfg(target_arch = "wasm32")]
+pub async fn get(uri: impl AsRef<str>) -> Result<Response<Vec<u8>>> {
+    let request = reqwest::Client::new().get(uri.as_ref());
+    let response = request.send().await?;
+    let status = response.status();
+    let bytes = response.bytes().await?;
+    let bytes = bytes.to_vec();
+    let response = Response::builder().status(status).body(bytes)?;
+
+    Ok(response)
+}
+
+#[cfg(target_arch = "wasm32")]
+pub async fn send<T>(request: Request<T>) -> Result<Response<Vec<u8>>>
+where
+    T: Into<Vec<u8>> + std::fmt::Debug,
+    reqwest::Request: TryFrom<http::Request<T>>,
+{
+    let client = reqwest::Client::new();
+    let reqwest_req = reqwest::Request::try_from(request)
+        .map_err(|_| anyhow::anyhow!("Failed to build Request on conversion."))?;
+    let response = client
+        .execute(reqwest_req)
+        .await
+        .map_err(|err| anyhow::anyhow!("Failed to execute request: {:?}", err))?;
+    let status = response.status();
+    let bytes = response.bytes().await?;
+    let bytes = bytes.to_vec();
+    let response = Response::builder()
+        .status(status)
+        .body(bytes)
+        .map_err(|_| anyhow::anyhow!("Failed to build Response on conversion."))?;
+
+    Ok(response)
+}
+
+#[cfg(not(target_arch = "wasm32"))]
 pub async fn send<T>(request: Request<T>) -> Result<Response<Vec<u8>>>
 where
     T: Into<Vec<u8>> + std::fmt::Debug,
