c633afd6d00ba39d6e7a12c40c973c9358570054
Esteban Borai <estebanborai@gmail.com>
2024-09-23 13:36:05 -0300

    feat: build url params using `url::form_urlencoded` approach (#4189)

--- crates/fluvio-hub-util/src/fvm/api/client.rs
+++ crates/fluvio-hub-util/src/fvm/api/client.rs
@@ -1,10 +1,15 @@
 //! Hub FVM API Client
 
+use std::env::var;
+
 use anyhow::{Error, Result};
 use serde::{Deserialize, Serialize};
 use url::Url;
 
 use crate::fvm::{Channel, PackageSet, PackageSetRecord};
+
+const INFINYON_CI_CONTEXT: &str = "INFINYON_CI_CONTEXT";
+const CONTEXT_QUERY_PARAM: &str = "ctx";
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct ApiError {
@@ -58,23 +63,34 @@ impl Client {
     /// Builds the URL to the Hub API for fetching a [`PackageSet`] using the
     /// [`Client`]'s `api_url`.
     fn make_fetch_package_set_url(&self, channel: &Channel, arch: &str) -> Result<Url> {
-        let url = format!(
-            "{}hub/v1/fvm/pkgset/{channel}?arch={arch}",
+        let mut url = Url::parse(&format!(
+            "{}hub/v1/fvm/pkgset/{channel}",
             self.api_url,
             channel = channel,
-            arch = arch
-        );
+        ))?;
+        let mut params = url::form_urlencoded::Serializer::new(String::new());
 
-        Ok(Url::parse(&url)?)
+        params.append_pair("arch", arch);
+
+        if let Ok(ctx) = var(INFINYON_CI_CONTEXT) {
+            params.append_pair(CONTEXT_QUERY_PARAM, ctx.as_str());
+        }
+
+        url.set_query(Some(params.finish().as_str()));
+
+        Ok(url)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
+    use std::env::{set_var, remove_var};
 
     use url::Url;
     use semver::Version;
+
+    use crate::fvm::api::client::INFINYON_CI_CONTEXT;
 
     use super::{Client, Channel};
 
@@ -89,7 +105,9 @@ mod tests {
     }
 
     #[test]
-    fn builds_url_for_fetching_pkgsets() {
+    fn builds_urls_for_fetching_pkgsets() {
+        // Scenario: Using Stable Channel
+
         let client = Client::new("https://hub.infinyon.cloud").unwrap();
         let url = client
             .make_fetch_package_set_url(&Channel::Stable, "arm-unknown-linux-gnueabihf")
@@ -97,12 +115,12 @@ mod tests {
 
         assert_eq!(
             url.as_str(),
-            "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/stable?arch=arm-unknown-linux-gnueabihf"
+            "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/stable?arch=arm-unknown-linux-gnueabihf",
+            "failed on Scenario Using Stable Channel"
         );
-    }
 
-    #[test]
-    fn builds_url_for_fetching_pkgsets_on_version() {
+        // Scenario: Using Tag
+
         let client = Client::new("https://hub.infinyon.cloud").unwrap();
         let url = client
             .make_fetch_package_set_url(
@@ -111,6 +129,21 @@ mod tests {
             )
             .unwrap();
 
-        assert_eq!(url.as_str(), "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/0.10.14-dev+123345abc?arch=arm-unknown-linux-gnueabihf");
+        assert_eq!(url.as_str(), "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/0.10.14-dev+123345abc?arch=arm-unknown-linux-gnueabihf", "failed on Scenario Using Tag");
+
+        // Scenario: Using Context
+
+        set_var(INFINYON_CI_CONTEXT, "unit_testing");
+
+        let client = Client::new("https://hub.infinyon.cloud").unwrap();
+        let url = client
+            .make_fetch_package_set_url(
+                &Channel::Tag(Version::from_str("0.10.14-dev+123345abc").unwrap()),
+                "arm-unknown-linux-gnueabihf",
+            )
+            .unwrap();
+
+        assert_eq!(url.as_str(), "https://hub.infinyon.cloud/hub/v1/fvm/pkgset/0.10.14-dev+123345abc?arch=arm-unknown-linux-gnueabihf&ctx=unit_testing", "failed on Scenario Using Context");
+        remove_var(INFINYON_CI_CONTEXT);
     }
 }
