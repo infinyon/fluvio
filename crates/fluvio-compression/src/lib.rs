f1e3ccc5391fccbe454ac46ada0d113a53143d36
Esteban Borai <estebanborai@gmail.com>
2024-09-26 15:04:38 -0300

    feat: expose trait under feature

--- crates/fluvio-compression/src/lib.rs
+++ crates/fluvio-compression/src/lib.rs
@@ -22,6 +22,7 @@ pub trait CompressionExt {
     fn uncompress(&self, src: &[u8]) -> Result<Option<Vec<u8>>, CompressionError>;
 }
 
+#[cfg(feature = "compress")]
 impl CompressionExt for Compression {
     fn compress(&self, src: &[u8]) -> Result<Bytes, CompressionError> {
         match *self {
@@ -64,49 +65,3 @@ impl CompressionExt for Compression {
     }
 }
 
-// impl Compression {
-//     /// Compress the given data, returning the compressed data
-//     #[cfg(feature = "compress")]
-//     pub fn compress(&self, src: &[u8]) -> Result<Bytes, CompressionError> {
-//         match *self {
-//             Compression::None => Ok(Bytes::copy_from_slice(src)),
-//             #[cfg(feature = "gzip")]
-//             Compression::Gzip => gzip::compress(src),
-//             #[cfg(feature = "snap")]
-//             Compression::Snappy => snappy::compress(src),
-//             #[cfg(feature = "lz4")]
-//             Compression::Lz4 => lz4::compress(src),
-//             #[cfg(feature = "zstd")]
-//             Compression::Zstd => zstd::compress(src),
-//         }
-//     }
-
-//     /// Uncompresss the given data, returning the uncompressed data if any compression was applied, otherwise returns None
-//     #[allow(unused_variables)]
-//     #[cfg(feature = "compress")]
-//     pub fn uncompress(&self, src: &[u8]) -> Result<Option<Vec<u8>>, CompressionError> {
-//         match *self {
-//             Compression::None => Ok(None),
-//             #[cfg(feature = "gzip")]
-//             Compression::Gzip => {
-//                 let output = gzip::uncompress(src)?;
-//                 Ok(Some(output))
-//             }
-//             #[cfg(feature = "snap")]
-//             Compression::Snappy => {
-//                 let output = snappy::uncompress(src)?;
-//                 Ok(Some(output))
-//             }
-//             #[cfg(feature = "lz4")]
-//             Compression::Lz4 => {
-//                 let output = lz4::uncompress(src)?;
-//                 Ok(Some(output))
-//             }
-//             #[cfg(feature = "zstd")]
-//             Compression::Zstd => {
-//                 let output = zstd::uncompress(src)?;
-//                 Ok(Some(output))
-//             }
-//         }
-//     }
-// }
