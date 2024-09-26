6f1add75ea85124f31e980b65de4cc3c2a255cf9
Esteban Borai <estebanborai@gmail.com>
2024-09-26 19:28:31 -0300

    fix: use ref for compression enum

--- crates/fluvio-connector-common/src/producer.rs
+++ crates/fluvio-connector-common/src/producer.rs
@@ -18,7 +18,7 @@ pub async fn producer_from_config(config: &ConnectorConfig) -> Result<(Fluvio, T
         };
 
         // Compression
-        if let Some(compression) = producer_params.compression {
+        if let Some(compression) = &producer_params.compression {
             config_builder = config_builder.compression(compression)
         };
 
