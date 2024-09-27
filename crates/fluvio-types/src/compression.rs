e355bba7b0ce0e646b6f1639ab495cb03973a56a
Esteban Borai <estebanborai@gmail.com>
2024-09-26 21:40:16 -0300

    feat: use serde lowercase

--- crates/fluvio-types/src/compression.rs
+++ crates/fluvio-types/src/compression.rs
@@ -1,6 +1,7 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Debug, Deserialize, Eq, Serialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
 pub enum Compression {
     None,
     Gzip,
