231efc91f651adb6b52378bb0cd5d6e995f74181
Esteban Borai <estebanborai@gmail.com>
2024-10-03 12:56:57 -0300

    chore: tests for time rendering

--- crates/fluvio-extension-common/src/common/time.rs
+++ crates/fluvio-extension-common/src/common/time.rs
@@ -1,14 +1,17 @@
 use chrono::{DateTime, Utc};
 
+const SIMPLE_DATE_FORMAT: &str = "%Y/%m/%d";
+const EXACT_FORMAT: &str = "%Y-%m-%d %H:%M:%S %Z";
+
 pub fn format_duration(date: DateTime<Utc>, exact: bool) -> String {
     let duration = Utc::now().signed_duration_since(date);
 
     if exact {
-        return date.format("%Y-%m-%d %H:%M:%S %Z").to_string();
+        return date.format(EXACT_FORMAT).to_string();
     }
 
     if duration.num_weeks() >= 1 {
-        return date.format("%Y/%m/%d").to_string();
+        return date.format(SIMPLE_DATE_FORMAT).to_string();
     }
 
     if duration.num_days() >= 2 {
@@ -19,11 +22,11 @@ pub fn format_duration(date: DateTime<Utc>, exact: bool) -> String {
         return String::from("Yesterday");
     }
 
-    if duration.num_hours() >= 1 {
+    if duration.num_hours() >= 1 && duration.num_hours() < 24 {
         return format!("{} hours ago", duration.num_hours());
     }
 
-    if duration.num_minutes() >= 1 {
+    if duration.num_minutes() >= 1 && duration.num_minutes() < 59 {
         return format!("{} minutes ago", duration.num_minutes());
     }
 
@@ -31,5 +34,77 @@ pub fn format_duration(date: DateTime<Utc>, exact: bool) -> String {
         return String::from("Just now");
     }
 
-    date.format("%Y/%m/%d").to_string()
+    date.format(SIMPLE_DATE_FORMAT).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::{Days, Timelike, Utc};
+
+    use crate::time::{EXACT_FORMAT, SIMPLE_DATE_FORMAT};
+
+    use super::format_duration;
+
+    #[test]
+    fn determines_exact_date() {
+        let date = Utc::now();
+        let formatted = format_duration(date, true);
+
+        assert_eq!(formatted, date.format(EXACT_FORMAT).to_string());
+    }
+
+    #[test]
+    fn simple_date_format_for_exceeding_a_week() {
+        let date = Utc::now()
+            .checked_sub_days(Days::new(7))
+            .expect("failed to sub days");
+        let formatted = format_duration(date, false);
+
+        assert_eq!(formatted, date.format(SIMPLE_DATE_FORMAT).to_string());
+    }
+
+    #[test]
+    fn renders_days_passed() {
+        let date = Utc::now()
+            .checked_sub_days(Days::new(2))
+            .expect("failed to sub days");
+        let formatted = format_duration(date, false);
+
+        assert_eq!(formatted, "2 days ago");
+    }
+
+    #[test]
+    fn renders_yesterday() {
+        let date = Utc::now()
+            .checked_sub_days(Days::new(1))
+            .expect("failed to sub days");
+        let formatted = format_duration(date, false);
+
+        assert_eq!(formatted, "Yesterday");
+    }
+
+    #[test]
+    fn renders_hours() {
+        let next_hour = Utc::now().hour() - 8;
+        let date = Utc::now().with_hour(next_hour).expect("failed to set hour");
+        let formatted = format_duration(date, false);
+
+        assert_eq!(formatted, "8 hours ago");
+    }
+
+    #[test]
+    fn renders_minutes() {
+        let next_min = Utc::now().minute() - 30;
+        let date = Utc::now().with_minute(next_min).expect("failed to set min");
+        let formatted = format_duration(date, false);
+
+        assert_eq!(formatted, "30 minutes ago");
+    }
+
+    #[test]
+    fn renders_just_now() {
+        let formatted = format_duration(Utc::now(), false);
+
+        assert_eq!(formatted, "Just now");
+    }
 }
