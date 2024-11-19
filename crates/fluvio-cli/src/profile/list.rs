483d5d1c4d652463685f4ade3e6f2506c1614a13
Alan Chen <argot@digikata.com>
2024-11-19 14:59:09 -0800

    fix: sort 'fluvio profile list' output (#4263)

--- crates/fluvio-cli/src/profile/list.rs
+++ crates/fluvio-cli/src/profile/list.rs
@@ -55,14 +55,16 @@ impl TableOutputHandler for ListConfig<'_> {
     }
 
     fn content(&self) -> Vec<Row> {
-        self.0
-            .profile
+        let mut profile_names = self.0.profile.keys().collect::<Vec<_>>();
+        profile_names.sort();
+        profile_names
             .iter()
-            .map(|(profile_name, profile)| {
+            .filter_map(|profile_name| {
+                let profile = self.0.profile.get(profile_name.as_str())?;
                 let active = self
                     .0
                     .current_profile_name()
-                    .map(|it| it == profile_name)
+                    .map(|it| it == profile_name.as_str())
                     .map(|active| if active { "*" } else { "" })
                     .unwrap_or("");
 
@@ -78,8 +80,8 @@ impl TableOutputHandler for ListConfig<'_> {
                         )
                     })
                     .unwrap_or(("", "", "", String::new()));
-
-                Row::from([active, profile_name, cluster, addr, tls, &installation])
+                let row = Row::from([active, profile_name, cluster, addr, tls, &installation]);
+                Some(row)
             })
             .collect()
     }
