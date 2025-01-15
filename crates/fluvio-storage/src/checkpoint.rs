cec7c9617f2915a3853a35cea94e0a63d2b6f892
Sehyo Chang <sehyo@fluvio.io>
2025-01-14 16:53:15 -0800

    disable checkpoint

--- crates/fluvio-storage/src/checkpoint.rs
+++ crates/fluvio-storage/src/checkpoint.rs
@@ -151,7 +151,6 @@ where
         self.offset = pos;
         self.offset.write_to(&mut contents);
         self.file.write_all(&contents).await?;
-        self.file.sync_all().await?;
         Ok(())
     }
 }
