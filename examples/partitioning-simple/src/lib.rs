3897940852f1db445bcf03f11f3a5ae1fcbb1762
Sehyo Chang <sehyo@fluvio.io>
2025-02-04 10:53:54 -0800

    fmt

--- examples/partitioning-simple/src/lib.rs
+++ examples/partitioning-simple/src/lib.rs
@@ -6,10 +6,10 @@ use fluvio::{dataplane::types::PartitionId, Partitioner, PartitionerConfig};
 /// If length of key is greater than partition count, it will wrap around
 /// String is assumed to be utf-8 format
 /// To configure Fluvio producer
-/// 
+///
 /// ```
 /// use fluvio::{producer::Partitioner, producer::PartitionerConfig};
-/// 
+///
 /// let config = TopicProducerConfigBuilder::default()
 ///    .partitioner(Box::new(new AlphabetPartitioning()))
 ///   .build();
