754c73572a780aad8111193a29a7f7c2757e4f68
Alan Chen <alan@infinyon.com>
2025-04-15 12:41:33 -0700

    smartmodules: remove unimplemented tests

--- crates/fluvio-smartmodule/ui-tests/pass_array_map.rs
+++
@@ -1,8 +0,0 @@
-use fluvio_smartmodule::{smartmodule, Record, RecordData, Result};
-
-#[smartmodule(array_map)]
-pub fn my_array_map(_record: &Record) -> Result<Vec<(Option<RecordData>, RecordData)>> {
-    unimplemented!()
-}
-
-fn main() {}
