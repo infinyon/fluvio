cee5b6f6ec1bb7706fddf2b04b174824c19aff90
Nick Cardin <nick@cardin.email>
2025-04-16 21:14:28 -0400

    WIP

--- crates/fluvio-types/src/compression.rs
+++ crates/fluvio-types/src/compression.rs
@@ -1,6 +1,7 @@
+use schemars::JsonSchema;
 use serde::{Serialize, Deserialize};
 
-#[derive(Clone, Debug, Deserialize, Eq, Serialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Eq, Serialize, PartialEq, JsonSchema)]
 #[serde(rename_all = "lowercase")]
 pub enum Compression {
     None,
